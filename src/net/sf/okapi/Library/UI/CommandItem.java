ec3aa66ee25bda9993734ca973673d26a4e3e376
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-05-15 18:46:21 +0000

    Cleanup for new trunk
    git-svn-id: https://okapi.googlecode.com/svn/trunk@150 0cd2bb99-014b-0410-b875-5d0485b745ed

--- src/net/sf/okapi/Library/UI/CommandItem.java
+++
@@ -1,6 +0,0 @@
-package net.sf.okapi.Library.UI;
-
-class CommandItem {
-	String              label;
-	int                 accelerator;
-}
--- src/net/sf/okapi/Library/UI/EncodingItem.java
+++
@@ -1,8 +0,0 @@
-package net.sf.okapi.Library.UI;
-
-public class EncodingItem {
-
-	public String       name;
-	public int          codePage;
-	public String       ianaName;
-}
