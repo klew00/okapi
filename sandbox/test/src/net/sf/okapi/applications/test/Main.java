ef49bda549107e0870d903362c09aba4e221b740
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-05-19 17:04:01 +0000

    Move test app to sandbox
    git-svn-id: https://okapi.googlecode.com/svn/trunk@172 0cd2bb99-014b-0410-b875-5d0485b745ed

--- sandbox/test/src/net/sf/okapi/applications/test/Main.java
+++ sandbox/test/src/net/sf/okapi/applications/test/Main.java
@@ -1,23 +1,3 @@
-/*===========================================================================*/
-/* Copyright (C) 2008 Yves Savourel (at ENLASO Corporation                   */
-/*---------------------------------------------------------------------------*/
-/* This library is free software; you can redistribute it and/or modify it   */
-/* under the terms of the GNU Lesser General Public License as published by  */
-/* the Free Software Foundation; either version 2.1 of the License, or (at   */
-/* your option) any later version.                                           */
-/*                                                                           */
-/* This library is distributed in the hope that it will be useful, but       */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
-/* General Public License for more details.                                  */
-/*                                                                           */
-/* You should have received a copy of the GNU Lesser General Public License  */
-/* along with this library; if not, write to the Free Software Foundation,   */
-/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA               */
-/*                                                                           */
-/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
-/*===========================================================================*/
-
 package net.sf.okapi.applications.test;
 
 import java.io.File;
@@ -84,14 +64,14 @@ public class Main {
 		
 			IInputFilter inputFlt = new net.sf.okapi.filters.properties.InputFilter();
 			FileInputStream input = new FileInputStream("test.properties");
-			inputFlt.initialize(input, "utf-8", null, null);
+			inputFlt.initialize(input, "utf-16", null, null);
 			
 			IOutputFilter outputFlt = new net.sf.okapi.filters.properties.OutputFilter();
 			FileOutputStream output = new FileOutputStream("test.out.properties");
 			outputFlt.initialize(output, "us-ascii");
 			
 			inputFlt.setOutput(outputFlt);
-			inputFlt.convert();
+			inputFlt.process();
 		}
 		catch ( Exception e ) {
 			System.out.println(e.getLocalizedMessage());
@@ -102,7 +82,7 @@ public class Main {
 	public static void main (String[] args)
 		throws Exception
 	{
-		testContainer();
+//		testContainer();
 		testFilter();
 	}		
 		
