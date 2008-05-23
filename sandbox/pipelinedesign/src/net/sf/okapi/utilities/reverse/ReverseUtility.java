615ffe833a78f32bbcb1ed31356f3c151608a42a
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-05-23 06:00:45 +0000

    make Reverse utility a pipeline step
    git-svn-id: https://okapi.googlecode.com/svn/trunk@191 0cd2bb99-014b-0410-b875-5d0485b745ed

--- sandbox/pipelinedesign/src/net/sf/okapi/utilities/reverse/ReverseUtility.java
+++ sandbox/pipelinedesign/src/net/sf/okapi/utilities/reverse/ReverseUtility.java
@@ -1,16 +1,50 @@
 package net.sf.okapi.utilities.reverse;
 
-import net.sf.okapi.common.pipeline.ThrougputPipeBase;
+import net.sf.okapi.common.pipeline.PipelineException;
+import net.sf.okapi.common.pipeline.PipelineStep;
+import net.sf.okapi.common.pipeline.PipelineStepStatus;
 import net.sf.okapi.common.resource.IExtractionItem;
+import net.sf.okapi.common.resource.IResourceBuilder;
 
-public class ReverseUtility extends ThrougputPipeBase{
+public class ReverseUtility implements PipelineStep {
 
-    @Override
+    public PipelineStepStatus execute(IResourceBuilder resourceBuilder)
+			throws PipelineException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void finish(boolean success) throws PipelineException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getRevision() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void prepare() throws PipelineException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/*@Override
     public void endExtractionItem(IExtractionItem extractionItem) {
-        String reverse = new StringBuffer(extractionItem.getContent()).reverse().toString(); 
+        String reverse = new StringBuffer(extractionItem.getContent()).reverse().toString();
         extractionItem.setContent(reverse);
-        
+
         super.endExtractionItem(extractionItem);
-    }
-    
+    }*/
+
 }
