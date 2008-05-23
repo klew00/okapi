30e50394f82c11d201a3c28664a822783c707bcc
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-05-23 04:50:44 +0000

    For string based input
    git-svn-id: https://okapi.googlecode.com/svn/trunk@187 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ sandbox/pipelinedesign/src/net/sf/okapi/common/filters/StringBufferRawData.java
@@ -0,0 +1,28 @@
+package net.sf.okapi.common.filters;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StringBufferRawData implements RawData {
+
+	public InputStream getInputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean isReleased() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void release() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
