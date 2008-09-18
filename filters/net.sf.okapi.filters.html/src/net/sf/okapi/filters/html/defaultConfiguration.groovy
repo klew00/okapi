43f717eecde968a2e3ff8001d6a76849476bffad
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-09-18 19:44:05 +0000

    default rules taken from XLIFF Representation Guide for HTML
    git-svn-id: https://okapi.googlecode.com/svn/trunk@489 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.html/src/net/sf/okapi/filters/html/defaultConfiguration.groovy
+++ filters/net.sf.okapi.filters.html/src/net/sf/okapi/filters/html/defaultConfiguration.groovy
@@ -1,10 +1,50 @@
-// rule types that drive HTML parser behavior
-enum RULE_TYPE {INLINE, GROUP, EXCLUDE, INCLUDE, TEXTUNIT, PRESERVE_WHITESPACE, SCRIPT, SERVER}
+//Rule types that drive HTML parser behavior
+INLINE = 0  // inline element
+GROUP = 2 // group element
+ATTRIBUTE = 3 // the rule lists an attribute, not an element
+ATTRIBUTES_ONLY = 4 // only attribute is translatable or localizable 
+EXCLUDE = 5 // exclude this element and all children
+INCLUDE = 6 // exceptions to the exclude rules
+TEXTUNIT = 7 // make this element a textunit with skeleton before/after
+PRESERVE_WHITESPACE = 8 // turn on preserve whitespace.
+SCRIPT = 9 // Embedded scripting languatge - pass to another extractor
+SERVER = 10 // Embedded server language tags such as JSP, PHP, Mason etc.
+
+/********************************************************************************************* 
+Operators for attribute value compare
+
+Rules are of the form: 
+TO_EXTRACT_ATTRIBUTE:[IF_HAS_ATTRIBUTE, OPERATOR, VALUE]
+'content':['http-equiv', EQUALS, 'keywords']
+
+This rule would read:
+extract the value of 'content' if the value of 'http-equiv' equals 'keywords'
+
+Multiple attribute values may be included in a list:
+'content':['http-equiv', EQUALS, ['content-language', 'content-type']
+
+This rule would be read: 
+extract the value of 'content' if the value of 'http-equiv' equals 'content-language' or 'content-type' 
+
+ALWAYS means the attribute is always extracted.
+
+********************************************************************************************/
+EQUALS = 0
+NOT_EQUALS = 1 
+MATCH = 3 // regex match. Must match the entire attribute value
+ALWAYS = 4
+
+// attributes that occur on many elements
+title  {
+	ruleTypes = [ATTRIBUTE]
+	onlyTheseElements = []
+	allElementsExcept = ['base', 'basefront', 'head', 'html', 'meta', 'param', 'script', 'title']
+}
 
 // inline tags
 a {
 	ruleTypes = [INLINE]
-	translatableAttributes = ['title']
+	translatableAttributes = ['title':ALWAYS, 'accesskey':ALWAYS]
 }
 
 abbr {
@@ -20,11 +60,17 @@ acronym {
 } 
 
 applet {
-	ruleTypes = [INLINE, GROUP]
+	ruleTypes = [INLINE]
+	translatableAttributes = ['alt':ALWAYS]
 }
 
 acronym {
 	ruleTypes = [INLINE]
+} 
+
+area {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['accesskey':ALWAYS, 'area':ALWAYS]
 } 
 
 b {
@@ -49,6 +95,7 @@ br {
 
 button {
 	ruleTypes = [INLINE]
+	translatableAttributes = ['accesskey':ALWAYS, 'value':ALWAYS]
 } 
 
 cite {
@@ -89,16 +136,22 @@ iframe {
 
 img {
 	ruleTypes = [INLINE]
-	translatableAttributes = ['title', 'alt']
+	translatableAttributes = ['title':ALWAYS, 'alt':ALWAYS]
 }
 
 input {
 	ruleTypes = [INLINE]
+	translatableAttributes = ['accesskey':ALWAYS, 'alt':ALWAYS, 'value':ALWAYS]
 } 
 
 ins {
 	ruleTypes = [INLINE]
-} 
+}
+
+isindex {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['prompt':ALWAYS]
+}
 
 acronym {
 	ruleTypes = [INLINE]
@@ -110,9 +163,54 @@ kbd {
 
 label {
 	ruleTypes = [INLINE]
+	translatableAttributes = ['accesskey':ALWAYS]
 } 
 
+legend {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['accesskey':ALWAYS]
+}
+
+li {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['value':ALWAYS]
+}
+
 map {
+	ruleTypes = [INLINE]
+}
+
+meta {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['content':['http-equiv', EQUALS, 'keywords'], 'name':ALWAYS]
+	localizableAttributes = ['content':['http-equiv', EQUALS, ['content-language', 'content-type']]]
+}
+
+nobr {
+	ruleTypes = [INLINE]
+} 
+
+object {
+	ruleTypes = [INLINE]
+	translatableAttributes = ['standby':ALWAYS]
+} 
+
+option {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['label':ALWAYS, 'value':ALWAYS]
+}
+
+optgroup {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['label':ALWAYS]
+}
+
+param {
+	ruleTypes = [INLINE]
+	translatableAttributes = ['value':ALWAYS]
+} 
+
+q {
 	ruleTypes = [INLINE]
 } 
 
@@ -128,9 +226,17 @@ small {
 	ruleTypes = [INLINE]
 }
 
+select {
+	ruleTypes = [INLINE]
+} 
+
 span {
 	ruleTypes = [INLINE]
 }
+
+spacer {
+	ruleTypes = [INLINE]
+} 
 
 strike {
 	ruleTypes = [INLINE]
@@ -148,14 +254,54 @@ sup {
 	ruleTypes = [INLINE]
 }
 
+symbol {
+	ruleTypes = [INLINE]
+} 
+
+table {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['summary':ALWAYS]
+} 
+
+textarea {
+	ruleTypes = [INLINE]
+	translatableAttributes = ['accesskey':ALWAYS]
+} 
+
+tt {
+	ruleTypes = [INLINE]
+} 
+
+td {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['abbr':ALWAYS]
+} 
+
+th {
+	ruleTypes = [ATTRIBUTES_ONLY]
+	translatableAttributes = ['abbr':ALWAYS]
+} 
+
 u {
 	ruleTypes = [INLINE]
 }
+
+var {
+	ruleTypes = [INLINE]
+} 
+
+wbr {
+	ruleTypes = [INLINE]
+} 
 
 // Ruby inline tags
 ruby {
 	ruleTypes = [INLINE]
 }
+
+rb {
+	ruleTypes = [INLINE]
+} 
 
 rt {
 	ruleTypes = [INLINE]
