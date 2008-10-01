4e4b9d29d42934b50c0ff1f02702869bb8fb9bb6
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-10-01 22:53:39 +0000

    add copyright notice to files
    git-svn-id: https://okapi.googlecode.com/svn/trunk@543 0cd2bb99-014b-0410-b875-5d0485b745ed

--- common/net.sf.okapi.filters.common.tests/src/net/sf/okapi/common/pipeline2/tests/ConsumerProducer.java
+++ common/net.sf.okapi.filters.common.tests/src/net/sf/okapi/common/pipeline2/tests/ConsumerProducer.java
@@ -1,6 +1,23 @@
-/**
- * 
- */
+/*===========================================================================*/
+/* Copyright (C) 2008 Jim Hargrave                                           */
+/*---------------------------------------------------------------------------*/
+/* This library is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU Lesser General Public License as published by  */
+/* the Free Software Foundation; either version 2.1 of the License, or (at   */
+/* your option) any later version.                                           */
+/*                                                                           */
+/* This library is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
+/* General Public License for more details.                                  */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this library; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA              */
+/*                                                                           */
+/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
+/*===========================================================================*/
+
 package net.sf.okapi.common.pipeline2.tests;
 
 import java.util.concurrent.BlockingQueue;
@@ -41,7 +58,7 @@ public class ConsumerProducer extends BasePipelineStep implements IConsumer, IPr
 
 	public PipelineReturnValue process() throws InterruptedException {
 		IPipelineEvent event = consumerQueue.take();
-		producerQueue.put(event);
+		producerQueue.add(event);
 		if (event.getEventType() == PipelineEventType.FINISHED) {
 			return PipelineReturnValue.SUCCEDED;
 		}
