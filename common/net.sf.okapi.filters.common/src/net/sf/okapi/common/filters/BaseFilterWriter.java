9acb1d7ee1a863741b4af13d65d139503f060f2b
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-10-22 22:24:10 +0000

    stub for BaseFilterWriter - common writer for filters
    git-svn-id: https://okapi.googlecode.com/svn/trunk@658 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ common/net.sf.okapi.filters.common/src/net/sf/okapi/common/filters/BaseFilterWriter.java
@@ -0,0 +1,72 @@
+package net.sf.okapi.common.filters;
+
+import java.io.OutputStream;
+
+import net.sf.okapi.common.IParameters;
+
+public abstract class BaseFilterWriter implements IFilterWriter {
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#close()
+	 */
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#getName()
+	 */
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#getParameters()
+	 */
+	public IParameters getParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#handleEvent(net.sf.okapi.common.filters.FilterEvent)
+	 */
+	public void handleEvent(FilterEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#setOptions(java.lang.String, java.lang.String)
+	 */
+	public void setOptions(String language, String defaultEncoding) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#setOutput(java.lang.String)
+	 */
+	public void setOutput(String path) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#setOutput(java.io.OutputStream)
+	 */
+	public void setOutput(OutputStream output) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.sf.okapi.common.filters.IFilterWriter#setParameters(net.sf.okapi.common.IParameters)
+	 */
+	public void setParameters(IParameters params) {
+		// TODO Auto-generated method stub
+
+	}
+}
