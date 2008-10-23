10aa2cb1626b4085f6a2cf0225b0d6d302c09e40
fliden@translate.com <fliden@translate.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-10-23 18:06:41 +0000

    Added filter-based URI conversion utility/plugin for Rainbow [Editor, Utility, Parameters]
    git-svn-id: https://okapi.googlecode.com/svn/trunk@666 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ applications/rainbow/src/net/sf/okapi/applications/rainbow/utilities/uriconversion/Editor.java
@@ -0,0 +1,317 @@
+/*===========================================================================*/
+/* Copyright (C) 2008 Fredrik Liden                                          */
+/*---------------------------------------------------------------------------*/
+/* This library is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU Lesser General Public License as published by  */
+/* the Free Software Foundation; either version 2.1 of the License, or (at   */
+/* your option) any later version.                                           */
+/*                                                                           */
+/* This library is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
+/* General Public License for more details.                                  */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this library; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA               */
+/*                                                                           */
+/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
+/*===========================================================================*/
+
+package net.sf.okapi.applications.rainbow.utilities.uriconversion;
+
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.IParametersEditor;
+import net.sf.okapi.common.ui.Dialogs;
+import net.sf.okapi.common.ui.OKCancelPanel;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+
+public class Editor implements IParametersEditor {
+
+	
+	private Shell                 shell;
+	private boolean               result = false;
+	private OKCancelPanel         pnlActions;
+	private Parameters            params;
+	private Table                 table;
+	
+	private Button 				  chkUpdateAll;
+	private Button                chkUnescape;
+	private Button                chkEscape;
+	private Button                btnFirstOption;
+	private Button                btnSecondOption;
+	
+	
+	public boolean edit (IParameters params,
+		Object object)
+	{
+		boolean bRes = false;
+		try {
+			shell = null;
+			this.params = (Parameters)params;
+			shell = new Shell((Shell)object, SWT.CLOSE | SWT.TITLE | SWT.RESIZE | SWT.APPLICATION_MODAL);
+			create((Shell)object);
+			return showDialog();
+		}
+		catch ( Exception e ) {
+			Dialogs.showError(shell, e.getLocalizedMessage(), null);
+			bRes = false;
+		}
+		finally {
+			// Dispose of the shell, but not of the display
+			if ( shell != null ) shell.dispose();
+		}
+		return bRes;
+	}
+	
+	public IParameters createParameters () {
+		return new Parameters();
+	}
+	
+	private void create (Shell parent)
+	{
+		shell.setText("Search And Replace");
+		if ( parent != null ) shell.setImage(parent.getImage());
+
+		GridLayout layTmp = new GridLayout();
+		layTmp.marginBottom = 0;
+		layTmp.verticalSpacing = 0;
+		shell.setLayout(layTmp);
+		
+		TabFolder tfTmp = new TabFolder(shell, SWT.NONE);
+		tfTmp.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		Composite cmpTmp0 = new Composite(tfTmp, SWT.NONE);
+		cmpTmp0.setLayout(new GridLayout(4, false));
+		TabItem tiTmp = new TabItem(tfTmp, SWT.NONE);
+		tiTmp.setText("Options");
+		tiTmp.setControl(cmpTmp0);		
+		
+		
+	
+		chkUnescape = new Button (cmpTmp0, SWT.RADIO);
+		chkUnescape.setText ("Un-escape the URI escape sequence");
+		chkUnescape.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 4, 1));
+		chkUnescape.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				if(chkUnescape.getSelection()){
+					table.setEnabled(false);
+					chkUpdateAll.setEnabled(false);
+					btnFirstOption.setEnabled(false);
+					btnSecondOption.setEnabled(false);
+				}
+			}
+		});		
+	
+		chkEscape = new Button (cmpTmp0, SWT.RADIO);
+		chkEscape.setText ("Escape content to URI escape sequence");
+		chkEscape.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 4, 1));
+		chkEscape.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				if(chkEscape.getSelection()){
+					table.setEnabled(true);
+					chkUpdateAll.setEnabled(true);
+					btnFirstOption.setEnabled(true);
+					btnSecondOption.setEnabled(true);					
+				}
+			}
+		});		
+		
+
+		Label lblEmpty = new Label (cmpTmp0,SWT.LEFT);
+		lblEmpty.setText ("    ");
+		lblEmpty.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		
+		Label lblList = new Label (cmpTmp0,SWT.LEFT);
+		lblList.setText ("List of the characters to escape:");
+		lblList.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 3, 1));
+
+		lblEmpty = new Label (cmpTmp0,SWT.LEFT);
+		lblEmpty.setText ("    ");
+		lblEmpty.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		
+		table = new Table (cmpTmp0, SWT.CHECK | SWT.FULL_SELECTION | SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
+		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 1));
+
+		//--click updates button states--
+		table.addListener (SWT.Selection, new Listener () {
+			public void handleEvent (Event event) {
+				if ( event.detail!=SWT.CHECK ) {
+					
+				}
+			}
+		});		
+		
+		lblEmpty = new Label (cmpTmp0,SWT.LEFT);
+		lblEmpty.setText ("    ");
+		lblEmpty.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		
+		chkUpdateAll = new Button(cmpTmp0, SWT.CHECK);
+		chkUpdateAll.setText("Escape all extended characters");
+		chkUpdateAll.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 3, 1));
+		chkUpdateAll.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+
+		lblEmpty = new Label (cmpTmp0,SWT.LEFT);
+		lblEmpty.setText ("    ");
+		lblEmpty.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		
+		btnFirstOption = new Button(cmpTmp0, SWT.PUSH);
+		btnFirstOption.setText("All but URI-marks");
+		btnFirstOption.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		btnFirstOption.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+
+				String selectList = " `@#$^&+={}|[]\\:\";<>,?/";
+				for ( int i=0; i<table.getItemCount(); i++ ) {
+					TableItem ti = table.getItem(i);
+					if(selectList.contains(ti.getText(0))){
+						ti.setChecked(true);
+					}else{
+						ti.setChecked(false);
+					}
+				};
+			}
+		});		
+
+		btnSecondOption = new Button(cmpTmp0, SWT.PUSH);
+		btnSecondOption.setText("All but marks and reserved");
+		btnSecondOption.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
+		btnSecondOption.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+
+				String selectList = " `#^{}|[]\\\"<>";
+				for ( int i=0; i<table.getItemCount(); i++ ) {
+					TableItem ti = table.getItem(i);
+					if(selectList.contains(ti.getText(0))){
+						ti.setChecked(true);
+					}else{
+						ti.setChecked(false);
+					}
+				};
+			}
+		});	
+
+		//--- Dialog-level buttons
+
+		SelectionAdapter OKCancelActions = new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				result = false;
+				if ( e.widget.getData().equals("h") ) {
+					//TODO: Call help
+					return;
+				}
+				if ( e.widget.getData().equals("o") ){
+					if(!saveData()){
+						return;
+					}
+				}
+				shell.close();
+			};
+		};
+		pnlActions = new OKCancelPanel(shell, SWT.NONE, OKCancelActions, true);
+		pnlActions.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		shell.setDefaultButton(pnlActions.btOK);
+
+		shell.pack();
+		shell.setMinimumSize(shell.getSize());
+		shell.setSize(shell.getSize().x,shell.getSize().y+50);
+		Dialogs.centerWindow(shell, parent);
+		
+		setData();
+	}
+	
+	
+	private boolean showDialog () {
+		shell.open();
+		while ( !shell.isDisposed() ) {
+			if ( !shell.getDisplay().readAndDispatch() )
+				shell.getDisplay().sleep();
+		}
+		return result;
+	}
+
+
+	private void setData () {
+		
+		if(params.conversionType==0){
+			chkUnescape.setSelection(true);
+			chkEscape.setSelection(false);
+			
+			table.setEnabled(false);
+			chkUpdateAll.setEnabled(false);
+			btnFirstOption.setEnabled(false);
+			btnSecondOption.setEnabled(false);
+			
+		}else{
+			chkUnescape.setSelection(false);
+			chkEscape.setSelection(true);
+			
+			table.setEnabled(true);
+			chkUpdateAll.setEnabled(true);
+			btnFirstOption.setEnabled(true);
+			btnSecondOption.setEnabled(true);			
+		}
+		
+		chkUpdateAll.setSelection(params.updateAll);
+		
+		String allItems = " ~`!@#$^&*() +-={}|[]\\:\";'<>,.?/";
+		String selList = params.escapeList;
+		
+		int len = allItems.length();
+        for (int i = 0; i < len; i++) {
+        	TableItem ti = new TableItem (table, SWT.NONE);
+			ti.setText (""+allItems.charAt(i));
+			if(selList.contains(""+allItems.charAt(i))){
+				ti.setChecked(true);
+			}
+        } 		
+	}
+
+	private boolean saveData () {
+
+		params.reset();
+		
+		if(chkUnescape.getSelection()){
+			params.conversionType=0;
+		}else{
+			params.conversionType=1;
+		}
+		
+		params.updateAll = chkUpdateAll.getSelection();
+		
+		StringBuilder selectedItems = new StringBuilder();
+
+		for ( int i=0; i<table.getItemCount(); i++ ) {
+			TableItem ti = table.getItem(i);
+			if(ti.getChecked()){
+				selectedItems.append(ti.getText(0));
+			}
+		};
+		
+		// make sure the list is not empty
+		if (table.getItemCount()==0){
+			Dialogs.showError(shell, "You need to provide a search expression", null);
+			return false;
+		}
+		
+		result = true;
+		return result;
+	}
+}
---
+++ applications/rainbow/src/net/sf/okapi/applications/rainbow/utilities/uriconversion/Utility.java
@@ -0,0 +1,275 @@
+/*===========================================================================*/
+/* Copyright (C) 2008 Fredrik Liden                                          */
+/*---------------------------------------------------------------------------*/
+/* This library is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU Lesser General Public License as published by  */
+/* the Free Software Foundation; either version 2.1 of the License, or (at   */
+/* your option) any later version.                                           */
+/*                                                                           */
+/* This library is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
+/* General Public License for more details.                                  */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this library; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA               */
+/*                                                                           */
+/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
+/*===========================================================================*/
+
+package net.sf.okapi.applications.rainbow.utilities.uriconversion;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import javax.swing.event.EventListenerList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.sf.okapi.applications.rainbow.lib.FilterAccess;
+import net.sf.okapi.applications.rainbow.utilities.BaseUtility;
+import net.sf.okapi.applications.rainbow.utilities.CancelListener;
+import net.sf.okapi.applications.rainbow.utilities.IFilterDrivenUtility;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.Util;
+import net.sf.okapi.common.resource.TextContainer;
+import net.sf.okapi.common.resource.TextFragment;
+import net.sf.okapi.common.resource.TextUnit;
+
+public class Utility extends BaseUtility implements IFilterDrivenUtility { 
+	
+	private final Logger     logger = LoggerFactory.getLogger("net.sf.okapi.logging");
+
+	private Parameters            params;
+	private String                commonFolder;
+	private String                inputPath;
+	private String                outputPath;
+	private String			      inputEncoding;
+	private EventListenerList     listenerList = new EventListenerList();
+	
+	static final int UNESCAPE  = 0;
+	static final int ESCAPE    = 1;
+	
+	public Utility () {
+		params = new Parameters();
+	}
+	
+	public void resetLists () {
+		// Not used in this utility
+		// Not sure when to use this
+	}
+	
+	public String getID () {
+		return "oku_uriconversion";
+	}
+	
+	public void doProlog (String sourceLanguage, String targetLanguage){
+		commonFolder = null; // Reset
+	}
+	
+	public void doEpilog () {
+		// Not sure when to use this
+	}
+
+	public IParameters getParameters () {
+		return params;
+	}
+	
+	public String getInputRoot () {
+		return null;
+	}
+
+	public String getOutputRoot () {
+		return null;
+	}
+
+	public boolean hasParameters () {
+		return true;
+	}
+
+	public boolean needsRoots () {
+		return false;
+	}
+	
+	public boolean needsOutputFilter() {
+		return true;
+	}	
+
+	public void setParameters (IParameters paramsObject) {
+		params = (Parameters)paramsObject;
+	}
+	
+	public void setRoots (String inputRoot, String outputRoot){
+		// Not used in this utility.
+	}
+	
+	public boolean isFilterDriven () {
+		return true;
+	}
+
+	public String getFolderAfterProcess () {
+		return commonFolder;
+	}
+	
+	public int getInputCount() {
+		return 1;
+	}
+	
+	public void addInputData (String path, String encoding, String filterSettings){
+		// Not sure when to use this
+		inputPath = path;
+		inputEncoding = encoding;
+	}
+
+	public void addOutputData (String path, String encoding){
+		// Compute the longest common folder
+		commonFolder = Util.longestCommonDir(commonFolder,
+			Util.getDirectoryName(path), !Util.isOSCaseSensitive());
+		outputPath = path;
+		// Encoding stays the same as the input
+	}
+
+	public void setFilterAccess (FilterAccess filterAccess,
+		String paramsFolder)
+	{
+		// Not used
+	}
+
+	public void setContextUI (Object contextUI) {
+		// Not used
+	}
+	
+	public void addCancelListener (CancelListener listener) {
+		listenerList.add(CancelListener.class, listener);
+	}
+
+	public void removeCancelListener (CancelListener listener) {
+		listenerList.remove(CancelListener.class, listener);
+	}
+
+	/*private void fireCancelEvent (CancelEvent event) {
+		Object[] listeners = listenerList.getListenerList();
+		for ( int i=0; i<listeners.length; i+=2 ) {
+			if ( listeners[i] == CancelListener.class ) {
+				((CancelListener)listeners[i+1]).cancelOccurred(event);
+			}
+		}
+	}*/
+
+	public void endExtractionItem (TextUnit item) {
+		try {
+			processTU(item);
+			if ( item.hasChild() ) {
+				for ( TextUnit tu : item.childTextUnitIterator() ) {
+					processTU(tu);
+				}
+			}
+		}
+		finally {
+			super.endExtractionItem(item);
+		}		
+	}
+	
+	private void processTU (TextUnit tu) {
+		
+		String forceEscape = " * -,.";
+		
+		String tmp = null;
+		
+		StringBuilder sb = new StringBuilder();
+		StringBuilder sbTemp = new StringBuilder();
+		
+		// Skip non-translatable
+		if ( !tu.isTranslatable()) 
+			return;
+
+		// Else: do the requested modifications
+		// Make sure we have a target where to set data
+		if ( !tu.hasTarget() ) {
+			tu.setTargetContent(tu.getSourceContent());
+		}
+
+		try {
+			String result = tu.getTargetContent().getCodedText();
+			
+			if ( params.conversionType == UNESCAPE ){
+				//--do unescape
+
+				//--loop all characters--
+				for ( int i=0; i<result.length(); i++ ) {
+				
+					switch ( result.charAt(i) ) {
+					case TextFragment.MARKER_OPENING:
+					case TextFragment.MARKER_CLOSING:
+					case TextFragment.MARKER_ISOLATED:
+					case TextFragment.MARKER_SEGMENT:
+						
+						sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+						sb.append(result.charAt(i));
+						i++;
+						sb.append(result.charAt(i));
+						sbTemp = new StringBuilder();
+						break;
+					case '+':
+
+						sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+						sb.append(result.charAt(i));
+						sbTemp = new StringBuilder();
+						break;
+						
+					default:
+						sbTemp.append(result.charAt(i));
+						break;
+					}
+				}				
+				
+				sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+
+			}else{
+				//--do escape
+			
+				//--loop all characters--
+				for ( int i=0; i<result.length(); i++ ) {
+			
+					String subStr = result.substring(i, i+1);
+					
+					//--process if extended character if selected--
+					if (( result.charAt(i) > '\u007f' ) && (params.updateAll )){
+					
+						switch ( result.charAt(i) ) {
+						case TextFragment.MARKER_OPENING:
+						case TextFragment.MARKER_CLOSING:
+						case TextFragment.MARKER_ISOLATED:
+						case TextFragment.MARKER_SEGMENT:
+							sb.append(result.charAt(i));
+							i++;
+							sb.append(result.charAt(i));
+							break;
+						default:
+							sb.append(URLEncoder.encode(subStr,"UTF-8"));
+							break;
+						}
+					}else if (params.escapeList.contains(subStr)){
+						
+						if(forceEscape.contains(subStr)){
+							byte bytes[] = subStr.getBytes();
+							sb.append("%"+Integer.toHexString(bytes[0]));
+						}else{
+							sb.append(URLEncoder.encode(subStr,"UTF-8"));							
+						}
+
+					}else{
+						sb.append(result.charAt(i));
+					}
+				}	
+			}
+			
+			TextContainer cnt = tu.getTargetContent();
+			cnt.setCodedText(sb.toString(), tu.getSourceContent().getCodes(), false);
+		}
+		catch ( Exception e ) {
+			System.out.println("Error");
+			logger.warn("Error when updating content: '"+tmp+"'", e);
+		}
+	}	
+	
+}
