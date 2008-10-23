f68cea8dd3a7b0770af2682cf53bd032d6d3bee5
asgeirf <asgeirf@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-10-23 12:34:07 +0000

    fix compilation error
    git-svn-id: https://okapi.googlecode.com/svn/trunk@665 0cd2bb99-014b-0410-b875-5d0485b745ed

--- sandbox/annotationmodel/src/com/googlecode/okapi/filter/odf/OdfZipDocumentParser.java
+++ sandbox/annotationmodel/src/com/googlecode/okapi/filter/odf/OdfZipDocumentParser.java
@@ -1,9 +1,14 @@
 package com.googlecode.okapi.filter.odf;
 
+import java.io.File;
+import java.io.IOException;
+
 import com.googlecode.okapi.filter.zip.ZipDocumentParser;
+import com.googlecode.okapi.resource.DocumentManager;
 
 public class OdfZipDocumentParser extends ZipDocumentParser{
-	public OdfZipDocumentParser() {
+
+	public OdfZipDocumentParser(DocumentManager documentManager, String inputFile) throws IOException {
 		super(documentManager, inputFile);
 	}
 }
