ea911618d55a87c85ac60a92d39c0487ff358c5c
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-10-29 21:55:27 +0000

    add serializable constant to remove warning
    git-svn-id: https://okapi.googlecode.com/svn/trunk@709 0cd2bb99-014b-0410-b875-5d0485b745ed

--- common/net.sf.okapi.filters.common/src/net/sf/okapi/common/threadedpipeline/RuntimeInterruptedException.java
+++ common/net.sf.okapi.filters.common/src/net/sf/okapi/common/threadedpipeline/RuntimeInterruptedException.java
@@ -1,6 +1,7 @@
 package net.sf.okapi.common.threadedpipeline;
 
-public class RuntimeInterruptedException extends RuntimeException {
+public class RuntimeInterruptedException extends RuntimeException {	
+	private static final long serialVersionUID = 8175096454212744354L;
 
 	public RuntimeInterruptedException(InterruptedException e) {
 		super(e);
