78c6d1fe63d77328b046df4abdef709700e246ea
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-11-06 21:15:54 +0000

    Updated ISkeleton for element/attribute handling.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@738 0cd2bb99-014b-0410-b875-5d0485b745ed

--- sandbox/apptest/src/net/sf/okapi/apptest/common/ISkeleton.java
+++ sandbox/apptest/src/net/sf/okapi/apptest/common/ISkeleton.java
@@ -1,5 +1,19 @@
 package net.sf.okapi.apptest.common;
 
+import net.sf.okapi.apptest.resource.GenericSkeletonPart;
+
 public interface ISkeleton {
+
+	public void add (GenericSkeletonPart part);
+
+	public void add (String data);
+
+	public void addStartElement (String name);
+	
+	public void addEndElement (String name);
+	
+	public void addAttribute (String name, String value);
+	
+	public void flush ();
 
 }
--- sandbox/apptest/src/net/sf/okapi/apptest/resource/GenericSkeleton.java
+++ sandbox/apptest/src/net/sf/okapi/apptest/resource/GenericSkeleton.java
@@ -8,6 +8,7 @@ import net.sf.okapi.apptest.common.ISkeleton;
 public class GenericSkeleton implements ISkeleton {
 
 	private ArrayList<GenericSkeletonPart> list;
+	private boolean inStartElement = false;
 	
 	public GenericSkeleton () {
 		list = new ArrayList<GenericSkeletonPart>();
@@ -19,19 +20,47 @@ public class GenericSkeleton implements ISkeleton {
 	}
 
 	public void add (GenericSkeletonPart part) {
+		flush();
 		list.add(part);
 	}
-	
+
 	public void add (String data) {
-		list.add(new GenericSkeletonPart(data));
-	}
-	
-	public void append (String data) {
-		if ( list.size() == 0 ) add(data);
+		flush();
+		if ( list.size() == 0 ) list.add(new GenericSkeletonPart(data));
 		else list.get(list.size()-1).append(data);
 	}
 	
 	public List<GenericSkeletonPart> getParts () {
 		return list;
 	}
+
+	public void addAttribute (String name, String value) {
+		if ( !inStartElement ) {
+			throw new RuntimeException("Call to addAttribute done outside a start element tag.");
+		}
+		addWithoutCheck(String.format(" %s=\"%s\"", name, value));
+	}
+
+	public void addEndElement(String name) {
+		flush();		
+		addWithoutCheck(String.format("</%s>", name));		
+	}
+
+	public void addStartElement(String name) {
+		flush();
+		addWithoutCheck(String.format("<%s", name));		
+	}
+	
+	public void flush () {
+		if ( inStartElement ) {
+			addWithoutCheck(">");
+			inStartElement = false;
+		}
+	}
+
+	private void addWithoutCheck (String data) {
+		if ( list.size() == 0 ) list.add(new GenericSkeletonPart(data));
+		else list.get(list.size()-1).append(data);
+	}
+	
 }
