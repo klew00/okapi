3386dd2e62dc0831ca712f214a0ebdcd8c424aab
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-11-14 22:55:03 +0000

    Started to refactor Regex Filter for IFilter.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@759 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ filters/net.sf.okapi.filters.regex/src/net/sf/okapi/filters/regex/RegexFilter.java
@@ -0,0 +1,465 @@
+/*===========================================================================*/
+/* Copyright (C) 2008 by the Okapi Framework contributors                    */
+/*---------------------------------------------------------------------------*/
+/* This library is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU Lesser General Public License as published by  */
+/* the Free Software Foundation; either version 2.1 of the License, or (at   */
+/* your option) any later version.                                           */
+/*                                                                           */
+/* This library is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
+/* General Public License for more details.                                  */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this library; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA               */
+/*                                                                           */
+/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
+/*===========================================================================*/
+
+package net.sf.okapi.filters.regex;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.sf.okapi.common.BOMAwareInputStream;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.filters.FilterEvent;
+import net.sf.okapi.common.filters.IFilter;
+import net.sf.okapi.common.filters.IOldParser.ParserTokenType;
+import net.sf.okapi.common.resource.Document;
+import net.sf.okapi.common.resource.Group;
+import net.sf.okapi.common.resource.IContainable;
+import net.sf.okapi.common.resource.IResource;
+import net.sf.okapi.common.resource.SkeletonUnit;
+import net.sf.okapi.common.resource.TextUnit;
+
+public class RegexFilter implements IFilter {
+
+	private boolean canceled;
+	private IResource currentRes;
+	private Parameters params;
+	private String encoding;
+	private Document docRes;
+	private BufferedReader reader;
+	private String inputText;
+	private Stack<Group> groupStack;
+	private int sklID;
+	private int groupID;
+	private int itemID;
+	private LinkedList<IContainable> resultQueue;
+	private int startSearch;
+	private int startSkl;
+	private int nextAction;
+	
+	public RegexFilter () {
+		params = new Parameters();
+	}
+
+	public void cancel () {
+		canceled = true;
+	}
+
+	public void close () {
+		try {
+			inputText = null;
+			if ( reader != null ) {
+				reader.close();
+				reader = null;
+			}
+		}
+		catch ( IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String getName () {
+		return "okf_properties";
+	}
+
+	public IParameters getParameters () {
+		return params;
+	}
+
+	public IResource getResource () {
+		return currentRes;
+	}
+
+	public boolean hasNext () {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public FilterEvent next () {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void open (InputStream input) {
+		try {
+			// Close any previously non-closed file
+			close();
+			canceled = false;
+			
+			// Open the input reader from the provided stream
+			BOMAwareInputStream bis = new BOMAwareInputStream(input, encoding);
+			reader = new BufferedReader(new InputStreamReader(bis, bis.detectEncoding()));
+
+			// Read the whole file into one string
+			//TODO: detect the original line-break type
+			//TODO: Optimize this with a better 'readToEnd()'
+			StringBuilder tmp = new StringBuilder();
+			String buffer;
+			while ( (buffer = reader.readLine()) != null ) {
+				if ( tmp.length() > 0 ) tmp.append("\n");
+				tmp.append(buffer);
+			}
+			
+			close(); // We can free the file handle now
+			// Make sure to close before inputText is set
+			// so it does not get reset to null
+			inputText = tmp.toString();
+			resultQueue = new LinkedList<IContainable>();
+			groupStack = new Stack<Group>();
+			startSearch = 0;
+			startSkl = 0;
+			itemID = 0;
+			sklID = 0;
+			groupID = 0;
+			nextAction = -1;
+
+			// Prepare the filter rules
+			params.compileRules();
+		}
+		catch ( UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+		catch ( IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void open (URL inputUrl) {
+		try { //TODO: Make sure this is actually working (encoding?, etc.)
+			// TODO: docRes should be always set with all opens... need better way
+			docRes = new Document();
+			docRes.setName(inputUrl.getPath());
+			open(inputUrl.openStream());
+		}
+		catch ( IOException e ) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void open (CharSequence inputText) {
+		//TODO: Check for better solution, going from char to byte to read char is just not good
+		open(new ByteArrayInputStream(inputText.toString().getBytes())); 
+	}
+
+	public void setOptions (String language,
+		String defaultEncoding)
+	{
+		encoding = defaultEncoding;
+	}
+
+	public void setParameters (IParameters params) {
+		this.params = (Parameters)params;
+	}
+
+	private ParserTokenType processMatch (Rule rule,
+		MatchResult startResult,
+		MatchResult endResult)
+	{
+		SkeletonUnit skl;
+		switch ( rule.ruleType ) {
+		case Rule.RULETYPE_NOTRANS:
+		case Rule.RULETYPE_COMMENT:
+			// Skeleton data include the content
+			skl = new SkeletonUnit();
+			skl.setData(inputText.substring(startSkl, endResult.start()));
+			skl.setID(String.format("%d", ++sklID));
+			currentRes = skl;
+			// Update starts for next read
+			startSearch = endResult.end();
+			startSkl = endResult.start();
+			// If comment: process the comment for directives
+			if ( rule.ruleType == Rule.RULETYPE_COMMENT ) {
+				params.locDir.process(skl.toString());
+			}
+			// Then just return one skeleton event
+			return ParserTokenType.SKELETON;
+			
+		case Rule.RULETYPE_OPENGROUP:
+		case Rule.RULETYPE_CLOSEGROUP:
+			// Skeleton data include the content
+			skl = new SkeletonUnit();
+			skl.setData(inputText.substring(startSkl, endResult.start()));
+			skl.setID(String.format("%d", ++sklID));
+			currentRes = skl;
+			// Update starts for next read
+			startSearch = endResult.end();
+			startSkl = endResult.start();
+			//TODO: return group event, and deal with skeleton
+			if ( rule.ruleType == Rule.RULETYPE_OPENGROUP ) {
+				Group groupRes = new Group();
+				groupRes.setID(String.valueOf(++groupID));
+				if ( rule.nameStart.length() > 0 ) {
+					String name = getMatch(startResult.group(), rule.nameStart, rule.nameEnd);
+					if ( name != null ) {
+						if ( rule.nameFormat.length() > 0 ) {
+							String tmp = rule.nameFormat.replace("<parentName>",
+								(groupStack.size()>0 ? groupStack.peek().getName() : "" ));
+							groupRes.setName(tmp.replace("<self>", name));
+						}
+						else groupRes.setName(name);
+					}
+				}
+				groupStack.push(groupRes);
+			}
+			else { // Rule.RULETYPE_CLOSEGROUP
+				groupStack.pop();
+			}
+			return ParserTokenType.SKELETON;
+		}
+		
+		// Otherwise: process the content
+
+		// Set skeleton data if needed
+		if ( startResult.end() > startSkl ) {
+			addSkeletonToQueue(inputText.substring(startSkl, startResult.end()), false);
+		}
+		
+		// Set start positions for next read
+		startSearch = endResult.end();
+		startSkl = endResult.start();
+
+		// Check localization directives
+		if ( !params.locDir.isLocalizable(true) ) {
+			// If not to be localized: make it a skeleton unit
+			addSkeletonToQueue(inputText.substring(startResult.end(),
+				endResult.start()), false);
+			// And return
+			return nextEvent();
+		}
+
+		//--- Else: We extract
+
+		// Any resname we can use?
+		String name = getMatch(startResult.group(), rule.nameStart,
+			rule.nameEnd);
+
+		// Process the data, this will create a queue of events if needed
+		if ( rule.ruleType == Rule.RULETYPE_CONTENT ) {
+			processContent(rule, name, inputText.substring(
+				startResult.end(), endResult.start()));
+		}
+		else if ( rule.ruleType == Rule.RULETYPE_STRING ) {
+			processStrings(rule, name, inputText.substring(
+				startResult.end(), endResult.start()));
+		}
+		return nextEvent();
+	}
+
+	private String getMatch (String text,
+		String start,
+		String end)
+	{
+		if (( start == null ) || ( start.length() == 0 )) return null;
+		if (( end == null ) || ( end.length() == 0 )) return null;
+		
+		Pattern p = Pattern.compile(start, params.regexOptions);
+		Matcher m1 = p.matcher(text);
+		if ( m1.find() ) {
+			p = Pattern.compile(end, params.regexOptions);
+			Matcher m2 = p.matcher(text);
+			if ( m2.find(m1.end()) ) {
+				return text.substring(m1.end(), m2.start());
+			}
+		}
+		return null;
+	}
+	
+	private void processContent (Rule rule,
+		String name,
+		String data)
+	{
+		TextUnit item = new TextUnit(String.valueOf(++itemID), data);
+		item.setPreserveWhitespaces(rule.preserveWS);
+
+		if ( rule.useCodeFinder ) {
+			rule.codeFinder.process(item.getSourceContent());
+		}
+
+		if ( name != null ) {
+			if ( rule.nameFormat.length() > 0 ) {
+				String tmp = rule.nameFormat.replace("<parentName>",
+					(groupStack.size()>0 ? groupStack.peek().getName() : "" ));
+				item.setName(tmp.replace("<self>", name));
+			}
+			else item.setName(name);
+		}
+
+		resultQueue.add(item);
+	}
+	
+	private void processStrings (Rule rule,
+		String name,
+		String data)
+	{
+		int state = 0;
+		int i = -1;
+		int j = 0;
+		int start = -1;
+		int end = -1;
+		String mark = params.startString;
+		
+		while (( ++i < data.length() ) && ( end == -1 )) {
+			
+			// Deal with \\, \" and \' escapes
+			if ( state > 0 ) {
+				if ( params.useBSlashEscape ) {
+					while ( data.codePointAt(i) == '\\' ) {
+						if ( i+2 < data.length() ) i += 2; // Now point to next
+						else throw new RuntimeException("Escape syntax error in ["+data+"]");
+					}
+				}
+			}
+		
+			// Check characters
+			switch ( state ) {
+			case 0:
+				if ( data.codePointAt(i) == mark.codePointAt(j) ) {
+					if ( ++j == mark.length() ) {
+						// Start of string match found, set search info for end
+						start = i+1; // Start of the string content
+						state = 2;
+						mark = params.endString;
+						j = 0;
+					}
+					else state = 1;
+				}
+				break;
+				
+			case 1: // Look if we can finish a start match
+				if ( data.codePointAt(i) == mark.codePointAt(j) ) {
+					if ( ++j == mark.length() ) {
+						// Start of string match found, set search info for end
+						start = i+1; // Start of the string content
+						state = 2;
+						mark = params.endString;
+						j = 0;
+					}
+					// Else: keep moving
+				}
+				else { // Was not a match
+					state = 0;
+					i -= (j-1); // Go back just after the trigger
+					j = 0; // And reset the mark index
+				}
+				break;
+				
+			case 2: // Look for an end mark
+				if ( data.codePointAt(i) == mark.codePointAt(j) ) {
+					if ( ++j == mark.length() ) {
+						// End of string match found
+						// Set the end of the string position (will stop the loop too)
+						end = i-j+1;
+					}
+					else state = 3;
+				}
+				break;
+				
+			case 3: // Look if we can finish an end match
+				if ( data.codePointAt(i) == mark.codePointAt(j) ) {
+					if ( ++j == mark.length() ) {
+						// End of string match found
+						// Set the end of the string position (will stop the loop too)
+						end = i-j+1;
+					}
+					// Else: Keep moving
+				}
+				else { // Was not a match
+					state = 2;
+					i -= (j-1); // Go back just after the trigger
+					j = 0; // And reset the mark index
+				}
+				break;
+			}
+		}
+
+		// If we have found a string: process it
+		if ( end != -1 ) {
+			// Skeleton part before
+			if ( start > 0 ) {
+				addSkeletonToQueue(data.substring(0, start), false);
+			}
+			
+			// Item to extract
+			TextUnit item = new TextUnit(String.valueOf(++itemID),
+				data.substring(start, end));
+			item.setPreserveWhitespaces(rule.preserveWS);
+			
+			if ( rule.useCodeFinder ) {
+				rule.codeFinder.process(item.getSourceContent());
+			}
+
+			if ( name != null ) {
+				if ( rule.nameFormat.length() > 0 ) {
+					String tmp = rule.nameFormat.replace("<parentName>",
+						(groupStack.size()>0 ? groupStack.peek().getName() : "" ));
+					item.setName(tmp.replace("<self>", name));
+				}
+				else item.setName(name);
+			}
+			resultQueue.add(item);
+			
+			// Skeleton part after
+			if ( end < data.length() ) {
+				addSkeletonToQueue(data.substring(end), false);
+			}
+		}
+		else { // No string in this entry, all is skeleton
+			addSkeletonToQueue(data, true);
+		}
+	}
+	
+	private void addSkeletonToQueue (String data,
+		boolean forceNewEntry)
+	{
+		SkeletonUnit skl;
+		if ( !forceNewEntry && ( resultQueue.size() > 0 )) {
+			if ( resultQueue.getLast() instanceof SkeletonUnit ) {
+				// Append to the last queue entry if possible
+				skl = (SkeletonUnit)resultQueue.getLast();
+				skl.appendData(data);
+				return;
+			}
+		}
+		// Else: create a new skeleton entry
+		skl = new SkeletonUnit(String.valueOf(++sklID), data);
+		resultQueue.add(skl);
+	}
+
+	private ParserTokenType nextEvent () {
+		if ( resultQueue.size() == 0 ) return ParserTokenType.NONE;
+		if ( resultQueue.peek() instanceof SkeletonUnit ) {
+			currentRes = (SkeletonUnit)resultQueue.poll();
+			return ParserTokenType.SKELETON;
+		}
+		// Else: it's an item
+		currentRes = (TextUnit)resultQueue.poll();
+		return ParserTokenType.TRANSUNIT;
+	}
+
+}
