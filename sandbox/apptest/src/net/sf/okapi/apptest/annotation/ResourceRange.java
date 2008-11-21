4c8f6093963f765e8952a95c5ede23eb92cea6ee
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-11-21 00:13:32 +0000

    start annotation package - add ResourceRange and ResourcePointer classes which will be used by some annotations.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@771 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ sandbox/apptest/src/net/sf/okapi/apptest/annotation/ResourceRange.java
@@ -0,0 +1,21 @@
+package net.sf.okapi.apptest.annotation;
+
+public class ResourceRange {
+
+	private ResourcePointer start;
+	private ResourcePointer end;
+	
+	public ResourcePointer getStart() {
+		return start;
+	}
+	public void setStart(ResourcePointer start) {
+		this.start = start;
+	}
+	public ResourcePointer getEnd() {
+		return end;
+	}
+	public void setEnd(ResourcePointer end) {
+		this.end = end;
+	}
+	
+}
