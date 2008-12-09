19e73db2401b705f0534e5cec065688dc33843e9
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-12-09 00:12:55 +0000

    add constructor for simple events like FINISH and START which have no resources
    git-svn-id: https://okapi.googlecode.com/svn/trunk@837 0cd2bb99-014b-0410-b875-5d0485b745ed

--- common/net.sf.okapi.filters.common/src/net/sf/okapi/common/filters/FilterEvent.java
+++ common/net.sf.okapi.filters.common/src/net/sf/okapi/common/filters/FilterEvent.java
@@ -23,32 +23,35 @@ package net.sf.okapi.common.filters;
 import net.sf.okapi.common.resource.IResource;
 
 public class FilterEvent {
-	
+
 	private FilterEventType filterEventType;
 	private IResource resource;
-	
-	public FilterEvent (FilterEventType filterEventType,
-		IResource resource)
-	{
+
+	/**
+	 * Used for filter events that have no resources such as START and FINISH
+	 * @param filterEventType
+	 */
+	public FilterEvent(FilterEventType filterEventType) {
+		this.filterEventType = filterEventType;		
+	}
+
+	public FilterEvent(FilterEventType filterEventType, IResource resource) {
 		this.filterEventType = filterEventType;
 		this.resource = resource;
-	}	
-	
-	public FilterEvent (FilterEventType filterEventType,
-		IResource resource,
-		ISkeleton skeleton)
-	{
+	}
+
+	public FilterEvent(FilterEventType filterEventType, IResource resource, ISkeleton skeleton) {
 		this.filterEventType = filterEventType;
 		this.resource = resource;
 		this.resource.setSkeleton(skeleton);
-	}	
-	
-	public FilterEventType getEventType () {
+	}
+
+	public FilterEventType getEventType() {
 		return filterEventType;
 	}
-	
-	public IResource getResource () {
+
+	public IResource getResource() {
 		return resource;
 	}
-	
+
 }
