466e496d9efff218250b7107f62391feae68526e
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2008-12-17 23:55:31 +0000

    Remove any hopes of a parameterized skeleton in BaseFilter. Start adding methods to support more complex resource model.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@872 0cd2bb99-014b-0410-b875-5d0485b745ed

--- common/net.sf.okapi.filters.common/src/net/sf/okapi/common/filters/BaseFilter.java
+++ common/net.sf.okapi.filters.common/src/net/sf/okapi/common/filters/BaseFilter.java
@@ -22,74 +22,252 @@ package net.sf.okapi.common.filters;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
-import net.sf.okapi.common.annotation.IAnnotation;
+import net.sf.okapi.common.resource.Code;
+import net.sf.okapi.common.resource.DocumentPart;
+import net.sf.okapi.common.resource.Ending;
+import net.sf.okapi.common.resource.INameable;
 import net.sf.okapi.common.resource.IResource;
+import net.sf.okapi.common.resource.Property;
+import net.sf.okapi.common.resource.StartGroup;
+import net.sf.okapi.common.resource.TextFragment;
 import net.sf.okapi.common.resource.TextUnit;
-import net.sf.okapi.common.skeleton.SkeletonFactory;
+import net.sf.okapi.common.skeleton.GenericSkeleton;
 
-public abstract class BaseFilter<S extends ISkeleton> implements IFilter {	
-	S skeletonClass;
+public abstract class BaseFilter implements IFilter {
 	private TextUnit currentTextUnit;
-	private int groupId = 0;
+	private int startGroupId = 0;
+	private int endGroupId = 0;
 	private int textUnitId = 0;
+	private int subDocumentId = 0;
+	private int documentPartId = 0;
+	private Stack<StartGroup> groupStack;
+	private StartGroup currentStartGroup;
+	private GenericSkeleton currentSkeleton;
 	private List<FilterEvent> filterEvents;
-	
+	private List<FilterEvent> referencableFilterEvents;
+
 	public BaseFilter() {
-		filterEvents = new LinkedList<FilterEvent>();
-		skeletonClass = SkeletonFactory.createSkeleton(skeletonClass);
+		reset();
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Reset parser for new input.
+	 */
+	protected void reset() {
+		startGroupId = 0;
+		endGroupId = 0;
+		textUnitId = 0;
+		subDocumentId = 0;
+		documentPartId = 0;
+
+		referencableFilterEvents = new LinkedList<FilterEvent>();
+		filterEvents = new LinkedList<FilterEvent>();
+		groupStack = new Stack<StartGroup>();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see net.sf.okapi.common.filters.IFilter#getResource()
 	 */
 	public IResource getResource() {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see net.sf.okapi.common.filters.IFilter#hasNext()
 	 */
 	public boolean hasNext() {
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see net.sf.okapi.common.filters.IFilter#next()
 	 */
 	public FilterEvent next() {
 		return null;
-	}	
+	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.okapi.common.filters.IFilter#setOptions(java.lang.String, java.lang.String, boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.sf.okapi.common.filters.IFilter#setOptions(java.lang.String,
+	 * java.lang.String, boolean)
 	 */
 	public void setOptions(String sourceLanguage, String defaultEncoding, boolean generateSkeleton) {
 	}
 
-	/* (non-Javadoc)
-	 * @see net.sf.okapi.common.filters.IFilter#setOptions(java.lang.String, java.lang.String, java.lang.String, boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see net.sf.okapi.common.filters.IFilter#setOptions(java.lang.String,
+	 * java.lang.String, java.lang.String, boolean)
 	 */
 	public void setOptions(String sourceLanguage, String targetLanguage, String defaultEncoding,
 			boolean generateSkeleton) {
 	}
-	
+
+	private void addPropertiesToResource(INameable resource, List<Property> properties) {
+		if (properties != null) {
+			for (Property property : properties) {
+				resource.setProperty(property);
+			}
+		}
+	}
+
 	protected void start() {
 		FilterEvent event = new FilterEvent(FilterEventType.START);
 		filterEvents.add(event);
 	}
-	
+
 	protected void finish() {
 		FilterEvent event = new FilterEvent(FilterEventType.FINISHED);
 		filterEvents.add(event);
 	}
-	
+
 	protected void startTextUnit() {
+		startTextUnit(null, null);
+	}
+
+	protected void endTextUnit() {
+		endTextUnit(null, null);
+	}
+
+	protected void startTextUnit(String startMarker) {
+		startTextUnit(startMarker, null);
+	}
+
+	protected void endTextUnit(String endMarker) {
+		endTextUnit(endMarker, null);
+	}
+
+	protected void startTextUnit(String startMarker, List<Property> properties) {
 		currentTextUnit = new TextUnit(String.format("s%d", ++textUnitId));
+
+		addPropertiesToResource(currentTextUnit, properties);
+
+		if (startMarker != null) {
+			currentSkeleton = new GenericSkeleton(startMarker);
+			currentSkeleton.addRef(currentTextUnit);
+		}
+	}
+
+	protected void endTextUnit(String endMarker, List<Property> properties) {
+		if (endMarker != null) {
+			currentSkeleton.add(endMarker);
+		}
+
+		addPropertiesToResource(currentTextUnit, properties);
+
+		filterEvents.add(new FilterEvent(FilterEventType.TEXT_UNIT, currentTextUnit, currentSkeleton));
+		currentSkeleton = null;
+		currentTextUnit = null;
+	}
+
+	protected void addToTextUnit(String text) {
+		currentTextUnit.getSource().append(text);
+	}	
+
+//	protected void addToTextUnit(Code code, List<Property> properties) {
+//		String data = code.getData();
+//		for (Property property : properties) {
+//			String value = property.getValue();
+//			String refMarker = TextFragment.makeRefMarker(currentTextUnit.getId());
+//			if (property.isReadOnly()) {
+//				refMarker = TextFragment.makeRefMarker();
+//			}
+//			data.replaceFirst("", refMarker);
+//		}
+//		code.setData(data);
+//		addToTextUnit(code);
+//		code.setHasReference(true);
+//	}
+	
+	protected void addToTextUnit(Code code) {
+		currentTextUnit.getSource().append(code);
 	}
 	
-	protected void endTextUnit() {
-		FilterEvent event = new FilterEvent(FilterEventType.TEXT_UNIT, currentTextUnit, );
-		filterEvents.add(event);
+	protected void startGroup(String startMarker) {
+		startGroup(startMarker, null);
+	}
+
+	protected void endGroup(String endMarker) {
+		endGroup(endMarker, null);
+	}
+
+	protected void startGroup(String startMarker, List<Property> properties) {
+		String parentId = String.format("s%d", subDocumentId);
+		if (groupStack.capacity() > 0) {
+			StartGroup g = groupStack.lastElement();
+			parentId = g.getId();
+		}
+
+		currentStartGroup = new StartGroup(parentId, String.format("s%d", ++startGroupId));
+
+		addPropertiesToResource(currentStartGroup, properties);
+
+		groupStack.push(currentStartGroup);
+		GenericSkeleton skel = new GenericSkeleton(startMarker);
+		if (currentTextUnit == null) {
+			filterEvents.add(new FilterEvent(FilterEventType.START_GROUP, currentStartGroup, skel));
+		} else {
+			// These groups are referencables from an existing TextUnit. We add
+			// these to a separate list.
+			referencableFilterEvents.add(new FilterEvent(FilterEventType.START_GROUP, currentStartGroup, skel));
+		}
+	}
+
+	protected void endGroup(String endMarker, List<Property> properties) {
+		GenericSkeleton skel = new GenericSkeleton(endMarker);
+		StartGroup startGroup = groupStack.pop();
+
+		addPropertiesToResource(startGroup, properties);
+
+		filterEvents.add(new FilterEvent(FilterEventType.END_GROUP, new Ending(String.format("s%d", ++endGroupId)),
+				skel));
+
+		currentStartGroup = null;
+	}
+
+	protected void addProperty(Property property, String language) {
+		DocumentPart dp = new DocumentPart(String.format("s%d", ++documentPartId), true);
+		dp.setTargetProperty(language, property);
+		referencableFilterEvents.add(new FilterEvent(FilterEventType.DOCUMENT_PART, dp));
+	}
+
+	protected void addProperties(List<Property> properties, String language) {
+		for (Property property : properties) {
+			addProperty(property, language);
+		}
+	}
+
+	protected void addProperty(Property property) {
+		DocumentPart dp = new DocumentPart(String.format("s%d", ++documentPartId), true);
+		dp.setProperty(property);
+		referencableFilterEvents.add(new FilterEvent(FilterEventType.DOCUMENT_PART, dp));
+	}
+
+	protected void addProperties(List<Property> properties) {
+		for (Property property : properties) {
+			addProperty(property);
+		}
+	}
+	
+	protected void addProperty(Property property, Code code) {
+		DocumentPart dp = new DocumentPart(String.format("s%d", ++documentPartId), true);
+		dp.setProperty(property);
+		referencableFilterEvents.add(new FilterEvent(FilterEventType.DOCUMENT_PART, dp));
+	}
+
+	protected void addProperties(List<Property> properties, Code code) {
+		for (Property property : properties) {
+			addProperty(property, code);
+		}
 	}
 }
