7087f76f7b34a2c0598a28b205b1967bc72478f5
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-01-09 21:15:33 +0000

    add a bit more complexity to jython test case
    git-svn-id: https://okapi.googlecode.com/svn/trunk@978 0cd2bb99-014b-0410-b875-5d0485b745ed

--- libraries/net.sf.okapi.lib.jython.tests/src/net/sf/okapi/lib/jython/tests/pipeline_test1.py
+++ libraries/net.sf.okapi.lib.jython.tests/src/net/sf/okapi/lib/jython/tests/pipeline_test1.py
@@ -1,11 +1,11 @@
 from net.sf.okapi.common.pipeline import IPipeline
 from net.sf.okapi.common.pipeline import BasePipelineStep
 from net.sf.okapi.common.filters import FilterEvent, FilterEventType
+from net.sf.okapi.common.resource import TextUnit
 from net.sf.okapi.common.simplepipeline import SimplePipeline
 
 
 class Producer(BasePipelineStep):
-    
     def preprocess(self):
         self.eventCount = 0
         
@@ -15,14 +15,15 @@ class Producer(BasePipelineStep):
     def handleEvent(self, event):        
         self.eventCount += 1
         if (self.eventCount >= 10):                        
-            return FilterEvent(FilterEventType.FINISHED, None)        
-        return FilterEvent(FilterEventType.TEXT_UNIT, None)
+            return FilterEvent(FilterEventType.FINISHED, None)                
+        id = str(self.eventCount)
+        return FilterEvent(FilterEventType.TEXT_UNIT, TextUnit(id, "Hello Event %s" % id))
     
 class Consumer(BasePipelineStep):
     def preprocess(self): pass
     def postprocess(self): pass 
     def handleEvent(self, event): 
-        print event
+        print "%s<===>%s" % (event.getEventType(), event.getResource())
 
 pipeline = SimplePipeline()
 pipeline.addStep(Producer())
