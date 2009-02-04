8b377f3a17bac1d0c000a985e28125ad459e0e22
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-02-04 22:39:28 +0000

    suite for html tests
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1072 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ filters/net.sf.okapi.filters.html.tests/src/net/sf/okapi/filters/html/tests/AllHtmlTests.java
@@ -0,0 +1,18 @@
+package net.sf.okapi.filters.html.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
+@RunWith(Suite.class)
+@SuiteClasses({HtmlEventTest.class,  HtmlSnippetsTest.class, HtmlFullFileTest.class, HtmlConfigurationTest.class})
+public class AllHtmlTests {
+
+  public static Test suite() {
+    return new JUnit4TestAdapter(AllHtmlTests.class);
+  }
+
+}
