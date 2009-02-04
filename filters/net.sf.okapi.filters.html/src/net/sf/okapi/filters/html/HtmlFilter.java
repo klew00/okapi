38db4563e590783fbedb83b3d435438fa202e248
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-02-04 23:25:01 +0000

    add logging support for slf4j-jdk14-1.5.0. Add newest copyright.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1077 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.html/src/net/sf/okapi/filters/html/HtmlFilter.java
+++ filters/net.sf.okapi.filters.html/src/net/sf/okapi/filters/html/HtmlFilter.java
@@ -1,26 +1,29 @@
-/*===========================================================================*/
-/* Copyright (C) 2008 Jim Hargrave                                           */
-/*---------------------------------------------------------------------------*/
-/* This library is free software; you can redistribute it and/or modify it   */
-/* under the terms of the GNU Lesser General Public License as published by  */
-/* the Free Software Foundation; either version 2.1 of the License, or (at   */
-/* your option) any later version.                                           */
-/*                                                                           */
-/* This library is distributed in the hope that it will be useful, but       */
-/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
-/* General Public License for more details.                                  */
-/*                                                                           */
-/* You should have received a copy of the GNU Lesser General Public License  */
-/* along with this library; if not, write to the Free Software Foundation,   */
-/* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA              */
-/*                                                                           */
-/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
-/*===========================================================================*/
+/*===========================================================================
+  Copyright (C) 2008-2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
 
 package net.sf.okapi.filters.html;
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.htmlparser.jericho.Attribute;
 import net.htmlparser.jericho.EndTag;
@@ -34,6 +37,9 @@ import net.sf.okapi.common.skeleton.GenericSkeleton;
 import net.sf.okapi.filters.markupfilter.BaseMarkupFilter;
 
 public class HtmlFilter extends BaseMarkupFilter {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HtmlFilter.class);
+	
 	public HtmlFilter() {
 		super();
 		setMimeType("text/html");
--- filters/net.sf.okapi.filters.markupfilter/src/net/sf/okapi/filters/markupfilter/BaseMarkupFilter.java
+++ filters/net.sf.okapi.filters.markupfilter/src/net/sf/okapi/filters/markupfilter/BaseMarkupFilter.java
@@ -1,3 +1,23 @@
+/*===========================================================================
+  Copyright (C) 2008-2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
 package net.sf.okapi.filters.markupfilter;
 
 import java.io.BufferedReader;
@@ -10,6 +30,9 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.htmlparser.jericho.Attribute;
 import net.htmlparser.jericho.EndTag;
@@ -29,6 +52,8 @@ import net.sf.okapi.filters.yaml.TaggedFilterConfiguration;
 import net.sf.okapi.filters.yaml.TaggedFilterConfiguration.RULE_TYPE;
 
 public abstract class BaseMarkupFilter extends BaseFilter {
+	private static final Logger logger = LoggerFactory.getLogger(BaseMarkupFilter.class);
+	
 	private Source document;
 	private ExtractionRuleState ruleState;
 	private Parameters parameters;
