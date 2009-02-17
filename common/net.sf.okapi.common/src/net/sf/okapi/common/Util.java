894ae48738dffd2b0c4ad3f38ee40a57a97d62a6
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-02-17 20:40:10 +0000

    forgot to remove unneeded imports
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1121 0cd2bb99-014b-0410-b875-5d0485b745ed

--- common/net.sf.okapi.common/src/net/sf/okapi/common/Util.java
+++ common/net.sf.okapi.common/src/net/sf/okapi/common/Util.java
@@ -32,14 +32,8 @@ import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
 import java.nio.charset.CharacterCodingException;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
-import java.nio.charset.CoderResult;
-import java.nio.charset.CodingErrorAction;
-import java.nio.charset.UnsupportedCharsetException;
 
 import org.w3c.dom.Node;
 
