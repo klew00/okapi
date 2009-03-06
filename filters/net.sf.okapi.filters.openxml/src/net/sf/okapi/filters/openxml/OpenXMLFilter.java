e8b47766bbbb5084338d07466b955b76757d7566
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-03-06 22:22:47 +0000

    Major library re-structuring: added common location, use of project libraries (with export). Only SWT uses a user-lib.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1231 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.openxml/src/net/sf/okapi/filters/openxml/OpenXMLFilter.java
+++ filters/net.sf.okapi.filters.openxml/src/net/sf/okapi/filters/openxml/OpenXMLFilter.java
@@ -55,6 +55,7 @@ import net.htmlparser.jericho.Tag;
 import net.sf.okapi.common.Event;
 import net.sf.okapi.common.EventType;
 import net.sf.okapi.common.encoder.HtmlEncoder;
+import net.sf.okapi.common.filters.IEncoder;
 import net.sf.okapi.common.filters.PropertyTextUnitPlaceholder;
 //import net.sf.okapi.common.filters.PropertyTextUnitPlaceholder.PlaceholderType;
 import net.sf.okapi.filters.markupfilter.BaseMarkupFilter;
@@ -856,7 +857,7 @@ public class OpenXMLFilter extends BaseMarkupFilter {
 			StartTag st = (StartTag) tag;
 			if (st.getAttributeValue("w:val") != null)
 			{
-				normalizedName = HtmlEncoder.NORMALIZED_LANGUAGE;
+				normalizedName = IEncoder.PROP_LANGUAGE;
 				return normalizedName;
 			}
 		}
@@ -865,7 +866,7 @@ public class OpenXMLFilter extends BaseMarkupFilter {
 			StartTag st = (StartTag) tag;
 			if (st.getAttributeValue("lang") != null)
 			{
-				normalizedName = HtmlEncoder.NORMALIZED_LANGUAGE;
+				normalizedName = IEncoder.PROP_LANGUAGE;
 				return normalizedName;
 			}
 		}
