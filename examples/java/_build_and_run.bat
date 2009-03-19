2233f3d94b4dfefdc0f71c27f5675a52254de1f9
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-03-19 03:43:46 +0000

    Updated java examples build.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1322 0cd2bb99-014b-0410-b875-5d0485b745ed

--- examples/java/_build_and_run.bat
+++ examples/java/_build_and_run.bat
@@ -1,8 +1,8 @@
 call ant
 pause
-java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s pseudo out_pseudo.html
-java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s upper out_uper.html
-java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s pseudo -s upper out_both.html
+java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s pseudo myFile.pseudo.html
+java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s upper myFile.upper.html
+java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.html -s pseudo -s upper myFile.both.html
 java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.odt -s pseudo
 java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.properties -s pseudo
 java -cp .;../../lib/okapi-lib.jar;Main.jar Main myFile.xml -s pseudo
