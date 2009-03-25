ed82ae79135cb0222a768384a09f7ed71658a02a
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-03-25 18:36:07 +0000

    Major Pipeline refactor:
    1. Remove IInitalStep and FileResourceIntialPipelineStepWrapper
    2. Move ThreadedPipeline to sandbox.
    3. Add IPipeline.hasNext()
    4. Change logic of Pipeline to handle all combinations of steps (FileResource to Event to FileResource conversion).
    5. Fix dependent tests and examples.
    
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1381 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ common/net.sf.okapi.common/src/net/sf/okapi/common/OkapiNotImplementedException.java
@@ -0,0 +1,21 @@
+package net.sf.okapi.common;
+
+public class OkapiNotImplementedException extends RuntimeException {
+
+	private static final long serialVersionUID = -1943082812163691869L;
+
+	public OkapiNotImplementedException() {
+	}
+
+	public OkapiNotImplementedException(String message) {
+		super(message);
+	}
+
+	public OkapiNotImplementedException(Throwable cause) {
+		super(cause);
+	}
+
+	public OkapiNotImplementedException(String message, Throwable cause) {
+		super(message, cause);	
+	}
+}
