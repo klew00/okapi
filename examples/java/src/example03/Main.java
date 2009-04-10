90b6dc6d8fb30acdead2759660e41348436efebf
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-04-10 16:11:56 +0000

    added main comment explaining the example
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1548 0cd2bb99-014b-0410-b875-5d0485b745ed

--- examples/java/src/example03/Main.java
+++ examples/java/src/example03/Main.java
@@ -16,8 +16,12 @@ import net.sf.okapi.common.pipeline.Pipeline;
 import net.sf.okapi.common.resource.InputResource;
 import net.sf.okapi.filters.xml.XMLFilter;
 
+/**
+ * Example showing an Okapi pipeline running XSLT transformations on a file. The
+ * transformed file is then run through the XML filter (broken down into Okapi
+ * Events) and then written using the filter Events writer.
+ */
 public class Main {
-
 
 	public static void main(String[] args) throws URISyntaxException, UnsupportedEncodingException {
 		IPipeline pipeline = new Pipeline();
@@ -33,12 +37,12 @@ public class Main {
 		in = Main.class.getResourceAsStream("remove_b_tags.xsl");
 		pipeline.addStep(new XsltTransformStep(in));
 
-		// filtering step - converts resource to events
-		IFilter filter = new XMLFilter();		
+		// filtering step - converts raw resource to events
+		IFilter filter = new XMLFilter();
 		IPipelineStep filterStep = new FilterPipelineStepAdaptor(filter);
 		pipeline.addStep(filterStep);
 
-		// writer step - converts events to a resource
+		// writer step - converts events to a raw resource
 		IFilterWriter writer = filter.createFilterWriter();
 		writer.setOptions("en", "UTF-8");
 		pipeline.addStep(new FilterWriterPipelineStepAdaptor(writer));
@@ -47,16 +51,16 @@ public class Main {
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		writer.setOutput(outStream);
 
-		// run the pipeline: 
+		// run the pipeline:
 		// (1) XSLT identity conversion step
 		// (2) XSLT replace b tag conversion step
 		// (3) XML filtering step creates IResource Events
 		// (4) Writer step takes Events and writes them out to outStream
-		pipeline.process(new InputResource(inputXml.toURI(), "UTF-8", "en"));	
+		pipeline.process(new InputResource(inputXml.toURI(), "UTF-8", "en"));
 
 		// destroy the pipeline and all steps - clean up resources
 		pipeline.destroy();
-		
+
 		// print out resulting XML file from the writer
 		System.out.println(new String(outStream.toByteArray(), "UTF-8"));
 
