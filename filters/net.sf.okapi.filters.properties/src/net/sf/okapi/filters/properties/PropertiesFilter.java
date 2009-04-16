a370deaa33829d2088166057212dd1cef1516891
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-04-16 23:42:10 +0000

    rename InputResource to RawDocument
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1637 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.properties/src/net/sf/okapi/filters/properties/PropertiesFilter.java
+++ filters/net.sf.okapi.filters.properties/src/net/sf/okapi/filters/properties/PropertiesFilter.java
@@ -44,7 +44,7 @@ import net.sf.okapi.common.filters.IFilter;
 import net.sf.okapi.common.filterwriter.GenericFilterWriter;
 import net.sf.okapi.common.filterwriter.IFilterWriter;
 import net.sf.okapi.common.resource.Ending;
-import net.sf.okapi.common.resource.InputResource;
+import net.sf.okapi.common.resource.RawDocument;
 import net.sf.okapi.common.resource.Property;
 import net.sf.okapi.common.resource.StartDocument;
 import net.sf.okapi.common.resource.TextUnit;
@@ -159,11 +159,11 @@ public class PropertiesFilter implements IFilter {
 		return new Event(EventType.END_DOCUMENT, ending);
 	}
 
-	public void open (InputResource input) {
+	public void open (RawDocument input) {
 		open(input, true);
 	}
 	
-	public void open (InputResource input,
+	public void open (RawDocument input,
 		boolean generateSkeleton)
 	{
 		setOptions(input.getSourceLanguage(), input.getTargetLanguage(),
@@ -178,7 +178,7 @@ public class PropertiesFilter implements IFilter {
 			open(input.getInputStream());
 		}
 		else {
-			throw new BadFilterInputException("InputResource has no input defined.");
+			throw new BadFilterInputException("RawDocument has no input defined.");
 		}
 	}
 	
