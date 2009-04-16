2ce5ffa344fd6da85670910f6fac0966d8e851e0
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-04-16 23:43:51 +0000

    rename InputResource to RawDocument
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1643 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.rtf/src/net/sf/okapi/filters/rtf/RTFFilter.java
+++ filters/net.sf.okapi.filters.rtf/src/net/sf/okapi/filters/rtf/RTFFilter.java
@@ -48,7 +48,7 @@ import net.sf.okapi.common.filters.IFilter;
 import net.sf.okapi.common.filterwriter.GenericFilterWriter;
 import net.sf.okapi.common.filterwriter.IFilterWriter;
 import net.sf.okapi.common.resource.Ending;
-import net.sf.okapi.common.resource.InputResource;
+import net.sf.okapi.common.resource.RawDocument;
 import net.sf.okapi.common.resource.StartDocument;
 import net.sf.okapi.common.resource.TextFragment;
 import net.sf.okapi.common.resource.TextUnit;
@@ -336,11 +336,11 @@ public class RTFFilter implements IFilter {
 		}
 	}
 
-	public void open (InputResource input) {
+	public void open (RawDocument input) {
 		open(input, true);
 	}
 	
-	public void open (InputResource input,
+	public void open (RawDocument input,
 		boolean generateSkeleton)
 	{
 		setOptions(input.getSourceLanguage(), input.getTargetLanguage(),
@@ -355,7 +355,7 @@ public class RTFFilter implements IFilter {
 			open(input.getInputStream());
 		}
 		else {
-			throw new BadFilterInputException("InputResource has no input defined.");
+			throw new BadFilterInputException("RawDocument has no input defined.");
 		}
 	}
 	
