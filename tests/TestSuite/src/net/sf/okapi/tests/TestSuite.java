20e186c03bca6606e1b70325f8fd30eb9dcd33fe
asgeirf <asgeirf@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-04-28 01:37:45 +0000

    adding cross-project test suite
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1769 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ tests/TestSuite/src/net/sf/okapi/tests/TestSuite.java
@@ -0,0 +1,32 @@
+package net.sf.okapi.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	net.sf.okapi.common.filters.tests.AllTests.class,
+	net.sf.okapi.common.tests.AllCommonFiltersTests.class,
+	net.sf.okapi.filters.html.tests.AllHtmlUnitTests.class,
+	net.sf.okapi.filters.html.tests.integration.AllHtmlIntegrationTests.class,
+	net.sf.okapi.filters.idml.tests.IDMLContentFilterTest.class,
+	net.sf.okapi.filters.idml.tests.IDMLFilterTest.class,
+	net.sf.okapi.filters.mif.tests.MIFFilterTest.class,
+	net.sf.okapi.filters.openoffice.tests.ODFFilterTest.class,
+	net.sf.okapi.filters.openoffice.tests.OpenOfficeFilterTest.class,
+	net.sf.okapi.filters.openxml.tests.AllOpenXMLTests.class,
+	net.sf.okapi.filters.po.tests.POFilterTest.class,
+	net.sf.okapi.filters.properties.tests.AllTests.class,
+	net.sf.okapi.filters.regex.tests.RegexFilterTest.class,
+	net.sf.okapi.filters.rtf.tests.RTFFilterTest.class,
+	//net.sf.okapi.filters.tests, ??
+	net.sf.okapi.filters.tmx.tests.TmxFilterTest.class,
+	net.sf.okapi.filters.xliff.tests.XLIFFFilterTest.class,
+	net.sf.okapi.filters.xml.tests.XMLFilterTest.class,
+	net.sf.okapi.lib.jython.tests.JythonBasedPipelineTest.class,
+	net.sf.okapi.lib.segmentation.tests.AllTests.class,
+	net.sf.okapi.common.pipeline.tests.AllPipelineTests.class
+})
+public class TestSuite {
+}
