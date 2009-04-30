45d2c4acf168111782d703324cf0d4652b0e91b0
engrean <engrean@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-04-30 16:53:24 +0000

    fixed new lines
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1851 0cd2bb99-014b-0410-b875-5d0485b745ed

--- deployment/rainbow/data/carbon-macosx/rainbow.sh
+++ deployment/rainbow/data/carbon-macosx/rainbow.sh
@@ -1,1 +1,7 @@
-#!/bin/bashjava -XstartOnFirstThread -jar lib/rainbow.jar# You can also use the following options:# org.eclipse.swt.internal.carbon.smallFonts# org.eclipse.swt.internal.carbon.noFocusRing# to possibly get better display.# java -XstartOnFirstThread -Dorg.eclipse.swt.internal.carbon.smallFonts -Dorg.eclipse.swt.internal.carbon.noFocusRing -jar lib/rainbow.jar
+#!/bin/bash
+java -XstartOnFirstThread -jar lib/rainbow.jar
+# You can also use the following options:
+# org.eclipse.swt.internal.carbon.smallFonts
+# org.eclipse.swt.internal.carbon.noFocusRing
+# to possibly get better display.
+# java -XstartOnFirstThread -Dorg.eclipse.swt.internal.carbon.smallFonts -Dorg.eclipse.swt.internal.carbon.noFocusRing -jar lib/rainbow.jar
