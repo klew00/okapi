c016045b5a6bf1a0e47d0d6199d9c194e5a004a6
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-05-01 17:25:33 +0000

    create test templates for RTF filter - will fill these out later
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1859 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ filters/net.sf.okapi.filters.rtf.tests/src/net/sf/okapi/filters/rtf/tests/RtfEventTest.java
@@ -0,0 +1,74 @@
+/* Copyright (C) 2008 Jim Hargrave
+/*---------------------------------------------------------------------------*/
+/* This library is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU Lesser General Public License as published by  */
+/* the Free Software Foundation; either version 2.1 of the License, or (at   */
+/* your option) any later version.                                           */
+/*                                                                           */
+/* This library is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser   */
+/* General Public License for more details.                                  */
+/*                                                                           */
+/* You should have received a copy of the GNU Lesser General Public License  */
+/* along with this library; if not, write to the Free Software Foundation,   */
+/* Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA              */
+/*                                                                           */
+/* See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html */
+/*===========================================================================*/
+
+package net.sf.okapi.filters.rtf.tests;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.EventType;
+import net.sf.okapi.common.resource.Code;
+import net.sf.okapi.common.resource.DocumentPart;
+import net.sf.okapi.common.resource.Ending;
+import net.sf.okapi.common.resource.RawDocument;
+import net.sf.okapi.common.resource.Property;
+import net.sf.okapi.common.resource.StartGroup;
+import net.sf.okapi.common.resource.TextFragment;
+import net.sf.okapi.common.resource.TextUnit;
+import net.sf.okapi.common.resource.TextFragment.TagType;
+import net.sf.okapi.common.skeleton.GenericSkeleton;
+import net.sf.okapi.filters.rtf.RTFFilter;
+import net.sf.okapi.filters.tests.FilterTestDriver;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RtfEventTest {
+	private RTFFilter filter;
+	private URL parameters;
+	
+	@Before
+	public void setUp() throws Exception {
+		filter = new RTFFilter();
+		//parameters = RtfEventTest.class.getResource("testConfiguration1.yml");
+	}
+	
+	private ArrayList<Event> getEvents(String snippet) {
+		ArrayList<Event> list = new ArrayList<Event>();
+		//filter.setParametersFromURL(parameters);
+		filter.open(new RawDocument(snippet, "en"));
+		while (filter.hasNext()) {
+			Event event = filter.next();
+			list.add(event);
+		}
+		filter.close();
+		return list;
+	}
+
+	private void addStartEvents(ArrayList<Event> events) {		
+		events.add(new Event(EventType.START_DOCUMENT));
+	}
+
+	private void addEndEvents(ArrayList<Event> events) {
+		events.add(new Event(EventType.END_DOCUMENT));
+	}
+}
