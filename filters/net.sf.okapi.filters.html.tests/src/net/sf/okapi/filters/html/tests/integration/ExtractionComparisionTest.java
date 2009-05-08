66d776ac6d9df2c6b78cf649a369ed0601deb368
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-05-08 18:06:59 +0000

    updated round trip testing to match Yves common filter test code
    git-svn-id: https://okapi.googlecode.com/svn/trunk@1899 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/net.sf.okapi.filters.html.tests/src/net/sf/okapi/filters/html/tests/integration/ExtractionComparisionTest.java
+++ filters/net.sf.okapi.filters.html.tests/src/net/sf/okapi/filters/html/tests/integration/ExtractionComparisionTest.java
@@ -1,16 +1,16 @@
 package net.sf.okapi.filters.html.tests.integration;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import net.sf.okapi.common.Event;
-import net.sf.okapi.common.filterwriter.IFilterWriter;
-import net.sf.okapi.common.resource.RawDocument;
+import net.sf.okapi.common.Util;
 import net.sf.okapi.filters.html.HtmlFilter;
-import net.sf.okapi.filters.html.tests.HtmlFullFileTest;
 import net.sf.okapi.filters.html.tests.TestUtils;
+import net.sf.okapi.filters.tests.InputDocument;
+import net.sf.okapi.filters.tests.RoundTripComparison;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,56 +18,30 @@ import org.junit.Test;
 
 public class ExtractionComparisionTest {
 	private HtmlFilter htmlFilter;
-	private String[] testFileList;
-	ArrayList<Event> originalEvents;
-	ArrayList<Event> postExtractionEvents;
-	IFilterWriter writer;
-	ByteArrayOutputStream writerBuffer;
+	private String[] testFileList;	
+	private String root;
 
 	@Before
 	public void setUp() throws Exception {
 		htmlFilter = new HtmlFilter();		
-		writer = htmlFilter.createFilterWriter();
-		writer.setOptions("en", "UTF-8");
-		writerBuffer = new ByteArrayOutputStream(1024);
-		writer.setOutput(writerBuffer);
-
 		testFileList = TestUtils.getTestFiles();
-		originalEvents = new ArrayList<Event>();
-		postExtractionEvents = new ArrayList<Event>();
+		
+		URL url = ExtractionComparisionTest.class.getResource("/324.html");
+		root = Util.getDirectoryName(url.getPath());
+		root = Util.getDirectoryName(root) + "/html/";
 	}
 
 	@After
 	public void tearDown() throws Exception {		
 	}
-
+	
 	@Test
-	public void roundTripCompare() throws UnsupportedEncodingException {
+	public void testDoubleExtraction () throws URISyntaxException {		
+		RoundTripComparison rtc = new RoundTripComparison();
+		ArrayList<InputDocument> list = new ArrayList<InputDocument>();
 		for (String f : testFileList) {
-			originalEvents.clear();
-			postExtractionEvents.clear();		
-			roundTripEvents(f);			
-			// TODO: This test still fails - comment out for now till we understand why
-			//assertTrue(FilterTestDriver.compareEventTypesOnly(originalEvents, postExtractionEvents));
+			list.add(new InputDocument(root + f, null));
 		}
-	}
-
-	private void roundTripEvents(String file) throws UnsupportedEncodingException {
-		InputStream htmlStream = HtmlFullFileTest.class.getResourceAsStream("/" + file);
-		htmlFilter.open(new RawDocument(htmlStream, "UTF-8", "en"));
-		while (htmlFilter.hasNext()) {
-			Event event = htmlFilter.next();
-			originalEvents.add(event);
-			writer.handleEvent(event);
-		}
-		htmlFilter.close();
-		writer.close();
-
-		htmlFilter.open(new RawDocument(new String(writerBuffer.toByteArray(), "UTF-8"), "UTF-8", "en"));
-		while (htmlFilter.hasNext()) {
-			Event event = htmlFilter.next();
-			postExtractionEvents.add(event);
-		}
-		htmlFilter.close();
+		assertTrue(rtc.executeCompare(htmlFilter, list, "UTF-8", "en", "en"));
 	}
 }
