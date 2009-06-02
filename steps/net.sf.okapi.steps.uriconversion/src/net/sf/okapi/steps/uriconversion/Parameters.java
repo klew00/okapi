89cf6eeddb0dbdff01d6246acb3c350846aa3731
fliden@translate.com <fliden@translate.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-06-02 19:52:02 +0000

    URI Conversion Step
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2020 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ steps/net.sf.okapi.steps.uriconversion/src/net/sf/okapi/steps/uriconversion/Parameters.java
@@ -0,0 +1,62 @@
+/*===========================================================================
+  Copyright (C) 2008-2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.uriconversion;
+
+import net.sf.okapi.common.BaseParameters;
+
+public class Parameters extends BaseParameters {
+	
+	protected int conversionType;
+	protected boolean updateAll;
+	protected String escapeList;
+	
+	public Parameters () {
+		reset();
+	}
+
+	public void reset () {
+		conversionType = 0;
+		updateAll = false;
+		escapeList = "%{}[]()&";
+	}
+
+	public void fromString (String data) {
+
+		reset();
+		
+		// Read the file content as a set of fields
+		buffer.fromString(data);
+		
+		conversionType = buffer.getInteger("conversionType", conversionType);
+		updateAll = buffer.getBoolean("updateAll", updateAll);
+		escapeList = buffer.getString("escapeList", escapeList);
+	}
+	
+	public String toString() {
+
+		buffer.reset();
+		buffer.setInteger("conversionType", conversionType);
+		buffer.setBoolean("updateAll", updateAll);		
+		buffer.setString("escapeList", escapeList);
+
+		return buffer.toString();
+	}
+}
---
+++ steps/net.sf.okapi.steps.uriconversion/src/net/sf/okapi/steps/uriconversion/UriConversionStep.java
@@ -0,0 +1,171 @@
+/*===========================================================================
+  Copyright (C) 2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.uriconversion;
+
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.IResource;
+import net.sf.okapi.common.pipeline.BasePipelineStep;
+import net.sf.okapi.common.resource.TextContainer;
+import net.sf.okapi.common.resource.TextFragment;
+import net.sf.okapi.common.resource.TextUnit;
+
+public class UriConversionStep extends BasePipelineStep {
+
+	static final int UNESCAPE  = 0;
+	static final int ESCAPE    = 1;
+
+	private final Logger logger = Logger.getLogger(getClass().getName());
+	private Parameters params;
+	private String trgLang;
+	
+	public UriConversionStep () {
+		params = new Parameters();
+	}
+	
+	public String getDescription () {
+		return "Encode/Decode URI content.";
+	}
+
+	public String getName () {
+		return "URI Conversion";
+	}
+
+	@Override
+	public IParameters getParameters () {
+		return params;
+	}
+
+	@Override
+	public void setParameters (IParameters params) {
+		params = (Parameters)params;
+	}
+	
+	@Override
+	protected void handleStartBatchItem (Event event) {
+		trgLang = getContext().getTargetLanguage(0);
+	}	
+ 
+	@Override
+	protected void handleTextUnit (Event event) {
+
+		String forceEscape = " * -,.";
+		
+		String tmp = null;
+		
+		StringBuilder sb = new StringBuilder();
+		StringBuilder sbTemp = new StringBuilder();
+
+		TextUnit tu = (TextUnit)event.getResource();
+		
+		// Skip non-translatable
+		if ( !tu.isTranslatable()) 
+			return;
+
+		// Else: do the requested modifications
+		// Make sure we have a target where to set data
+		tu.createTarget(trgLang, false, IResource.COPY_ALL);
+
+		try {
+			String result = tu.getTarget(trgLang).getCodedText();
+			
+			if ( params.conversionType == UNESCAPE ){
+				//--do unescape
+				
+				//--loop all characters--
+				for ( int i=0; i<result.length(); i++ ) {
+					switch ( result.charAt(i) ) {
+					case TextFragment.MARKER_OPENING:
+					case TextFragment.MARKER_CLOSING:
+					case TextFragment.MARKER_ISOLATED:
+					case TextFragment.MARKER_SEGMENT:
+						sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+						sb.append(result.charAt(i));
+						i++;
+						sb.append(result.charAt(i));
+						sbTemp = new StringBuilder();
+						break;
+
+					case '+':
+						sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+						sb.append(result.charAt(i));
+						sbTemp = new StringBuilder();
+						break;
+						
+					default:
+						sbTemp.append(result.charAt(i));
+						break;
+					}
+				}				
+				sb.append(URLDecoder.decode(sbTemp.toString(),"UTF-8"));
+
+			}else{
+				//--do escape
+			
+				//--loop all characters--
+				for ( int i=0; i<result.length(); i++ ) {
+			
+					String subStr = result.substring(i, i+1);
+					
+					//--process if extended character if selected--
+					if (( result.charAt(i) > '\u007f' ) && (params.updateAll )){
+					
+						switch ( result.charAt(i) ) {
+						case TextFragment.MARKER_OPENING:
+						case TextFragment.MARKER_CLOSING:
+						case TextFragment.MARKER_ISOLATED:
+						case TextFragment.MARKER_SEGMENT:
+							sb.append(result.charAt(i));
+							i++;
+							sb.append(result.charAt(i));
+							break;
+						default:
+							sb.append(URLEncoder.encode(subStr,"UTF-8"));
+							break;
+						}
+					}else if (params.escapeList.contains(subStr)){
+						
+						if(forceEscape.contains(subStr)){
+							byte bytes[] = subStr.getBytes();
+							sb.append("%"+Integer.toHexString(bytes[0]));
+						}else{
+							sb.append(URLEncoder.encode(subStr,"UTF-8"));							
+						}
+
+					}else{
+						sb.append(result.charAt(i));
+					}
+				}	
+			}
+			
+			TextContainer cnt = tu.getTarget(trgLang);
+			cnt.setCodedText(sb.toString(), tu.getSourceContent().getCodes(), false);
+		}
+		catch ( Exception e ) {
+			logger.log(Level.WARNING, "Error when updating content: '"+tmp+"'", e);
+		}
+	}		
+}
