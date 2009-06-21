a81001a90fff6c4e2a4f6bb86261f2bebdc15cae
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-06-21 03:49:50 +0000

    Remove BlindPipeline from temporary sandbox in the trunk.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2179 0cd2bb99-014b-0410-b875-5d0485b745ed

--- sandbox/BlindPipeline/src/net/sf/okapi/common/Okapi.java
+++
@@ -1,7 +0,0 @@
-package net.sf.okapi.common;
-
-public class Okapi extends BaseContext {
-
-	static public OkapiContext context = new OkapiContext();
-	
-}
--- sandbox/BlindPipeline/src/net/sf/okapi/common/OkapiContext.java
+++
@@ -1,7 +0,0 @@
-package net.sf.okapi.common;
-
-public class OkapiContext extends BaseContext {
-
-	//TODO: Add extra variables as needed
-
-}
