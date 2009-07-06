2b64d03d3a349ff6319e43094e10d0ffd9f4f25d
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-07-06 23:04:57 +0000

    If there is only one target language in the TextUnit then use it as the default target to copy source to. In this case there is no need to configure this parameter
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2366 0cd2bb99-014b-0410-b875-5d0485b745ed

--- steps/net.sf.okapi.steps.copysourcetotarget/src/net/sf/okapi/steps/copysourcetotarget/CopySourceToTargetStep.java
+++ steps/net.sf.okapi.steps.copysourcetotarget/src/net/sf/okapi/steps/copysourcetotarget/CopySourceToTargetStep.java
@@ -1,39 +1,53 @@
 package net.sf.okapi.steps.copysourcetotarget;
 
+import java.util.Set;
+
 import net.sf.okapi.common.Event;
 import net.sf.okapi.common.IParameters;
 import net.sf.okapi.common.IResource;
 import net.sf.okapi.common.pipeline.BasePipelineStep;
+import net.sf.okapi.common.resource.StartDocument;
 import net.sf.okapi.common.resource.TextUnit;
 
 public class CopySourceToTargetStep extends BasePipelineStep {
 	private Parameters params;
-	
+
 	public CopySourceToTargetStep() {
 		params = new Parameters();
 	}
-	
-	public String getDescription() {		
+
+	public String getDescription() {
 		return "Copy the source segments to the specified target. Create the target if needed.";
 	}
 
 	public String getName() {
 		return "Copy Source To Target";
 	}
-	
+
 	@Override
 	public void setParameters(IParameters params) {
-		this.params = (Parameters)params;
+		this.params = (Parameters) params;
 	}
-	
+
 	@Override
 	public Parameters getParameters() {
 		return params;
 	}
-	
+
 	@Override
-	public void handleTextUnit (Event event) {
-		TextUnit tu = (TextUnit)event.getResource();
+	public void handleStartDocument(Event event) {
+		StartDocument sd = (StartDocument) event.getResource();
+		Set<String> tls = sd.getTargetLanguages();
+		// if there is only one target language then use it as the default
+		// target to copy source to
+		if (tls.size() == 1) {
+			getParameters().targetLanguage = tls.iterator().next();
+		}
+	}
+
+	@Override
+	public void handleTextUnit(Event event) {
+		TextUnit tu = (TextUnit) event.getResource();
 		tu.createTarget(getParameters().targetLanguage, false, IResource.COPY_ALL);
-	}	
+	}
 }
