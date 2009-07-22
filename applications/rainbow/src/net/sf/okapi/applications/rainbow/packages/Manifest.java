29c7ae28c30e707413d5408b81f0b0602dbb4ba3
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-07-22 11:03:21 +0000

    Updated test files, change log, downloads.html info for snapshots, cleaned up one warning.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2483 0cd2bb99-014b-0410-b875-5d0485b745ed

--- applications/rainbow/src/net/sf/okapi/applications/rainbow/packages/Manifest.java
+++ applications/rainbow/src/net/sf/okapi/applications/rainbow/packages/Manifest.java
@@ -66,7 +66,7 @@ public class Manifest {
 		targetDir = "";
 		originalDir = "";
 		doneDir = "";
-		useApprovedOnly = true;
+		useApprovedOnly = false;
 		updateApprovedFlag = true;
 	}
 
--- filters/net.sf.okapi.ui.filters.regex/src/net/sf/okapi/filters/ui/regex/Editor.java
+++ filters/net.sf.okapi.ui.filters.regex/src/net/sf/okapi/filters/ui/regex/Editor.java
@@ -42,7 +42,6 @@ import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
