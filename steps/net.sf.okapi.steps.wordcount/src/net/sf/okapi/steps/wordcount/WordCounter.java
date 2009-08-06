685aac793dfbce5a6135f6d05283e3269a257f0c
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-06 14:05:30 +0000

    Adding back other files deleted from src in rev 2567/2568.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2570 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ steps/net.sf.okapi.steps.wordcount/src/net/sf/okapi/steps/wordcount/WordCounter.java
@@ -0,0 +1,60 @@
+/*===========================================================================
+  Copyright (C) 2008-2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.wordcount;
+
+import net.sf.okapi.steps.tokenization.Tokenizer;
+import net.sf.okapi.steps.tokenization.tokens.Token;
+import net.sf.okapi.steps.tokenization.tokens.Tokens;
+import net.sf.okapi.steps.wordcount.common.BaseCounter;
+
+/**
+ * Word Count engine. Contains static methods to calculate number of words in a given text fragment. 
+ * 
+ * @version 0.1 07.07.2009
+ */
+
+public class WordCounter extends BaseCounter {
+
+	@Override
+	protected long doGetCount(String text, String language) {
+		
+		Tokens tokens = Tokenizer.tokenize(text, language, new String[]{Token.WORD});		
+		if (tokens == null) return 0;
+		
+		return tokens.size();
+	}
+	
+	/**
+	 * 
+	 * @param text any of the following:
+	 * <li> TextUnit
+	 * <li> TextContainer
+	 * <li> TextFragment
+	 * <li> String
+	 * @param language RFC 4646, 4647 language tag (like "en-US")
+	 * @return word count in the current text
+	 */
+	static public long getCount(Object text, String language) {
+		
+		return getCount(WordCounter.class, text, language);		
+	}
+
+}
