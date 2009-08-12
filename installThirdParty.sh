74ad3292b2cc9f5f6ac2e1c1363ca5a492534bfb
engrean <engrean@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-12 22:14:08 +0000

    removed jerhico as it finally made it to the maven repo
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2685 0cd2bb99-014b-0410-b875-5d0485b745ed

--- installThirdParty.sh
+++ installThirdParty.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mvn install:install-file -DgroupId=net.htmlparser.jericho -DartifactId=jericho-html -Dversion=3.1 -Dfile=third-party/jericho-html-3.1.jar -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-win-32 -Dversion=3.5 -Dfile=third-party/win32-x86/org.eclipse.swt.win32.win32.x86_3.5.0.v3550b.jar -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-cocoa-macosx -Dversion=3.5 -Dfile=third-party/cocoa-macosx/org.eclipse.swt.cocoa.macosx_3.5.0.v3550b.jar -Dpackaging=jar -DgeneratePom=true
