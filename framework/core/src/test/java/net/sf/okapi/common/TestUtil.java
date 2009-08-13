9cac9ccaa6854755356219a7daf06573280ebcf5
engrean <engrean@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-13 14:38:25 +0000

    nice docs ..... oooh
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2731 0cd2bb99-014b-0410-b875-5d0485b745ed

--- framework/core/src/test/java/net/sf/okapi/common/TestUtil.java
+++ framework/core/src/test/java/net/sf/okapi/common/TestUtil.java
@@ -5,13 +5,22 @@ import java.io.File;
 import java.net.URL;
 
 /**
- * User: Christian Hargraves
- * Date: Aug 13, 2009
- * Time: 8:11:05 AM
+ * Class to hold test utility methods
+ * @author Christian Hargraves
+ *
  */
 public class TestUtil {
 
-
+    /**
+     * Takes a class and a file path and returns you the parent directory name of that file. This is used
+     * for getting the directory from a file which is in the classpath.
+     * @param clazz - The class to use for classpath loading. Don't forget the resource might be a jar file where this
+     * class exists.
+     * @param filepath - the location of the file. For example &quot;/testFile.txt&quot; would be loaded from the root
+     * of the classpath.
+     * @return The path of directory which contains the file
+     * @throws FileNotFoundException when the file is not found in the classpath
+     */
     public static String getParentDir(Class clazz, String filepath) throws FileNotFoundException {
         URL url = clazz.getResource(filepath);
         if (url == null) {
