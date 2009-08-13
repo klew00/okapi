7a26b2be578285f57f76c6feaa414bdcda3e3c98
asgeirf <asgeirf@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-13 00:13:51 +0000

    add linux swt profiles
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2696 0cd2bb99-014b-0410-b875-5d0485b745ed

--- installThirdParty.sh
+++ installThirdParty.sh
@@ -2,3 +2,7 @@
 
 mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-win-32 -Dversion=3.5 -Dfile=third-party/win32-x86/org.eclipse.swt.win32.win32.x86_3.5.0.v3550b.jar -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-cocoa-macosx -Dversion=3.5 -Dfile=third-party/cocoa-macosx/org.eclipse.swt.cocoa.macosx_3.5.0.v3550b.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-gtk2-linux-x86 -Dversion=3.5 -Dfile=third-party/gtk2-linux-x86/swt.jar -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.eclipse.swt -DartifactId=swt-gtk2-linux-x86_64 -Dversion=3.5 -Dfile=third-party/gtk2-linux-x86_64/swt.jar -Dpackaging=jar -DgeneratePom=true
+
+
