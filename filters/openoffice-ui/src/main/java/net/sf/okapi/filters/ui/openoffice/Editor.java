050c65c7172fb9b095af49c8bb2735edab11705a
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-13 17:32:44 +0000

    rename openoffice filter ui module
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2760 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ filters/openoffice-ui/src/main/java/net/sf/okapi/filters/ui/openoffice/Editor.java
@@ -0,0 +1,168 @@
+/*===========================================================================
+  Copyright (C) 2008-2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.filters.ui.openoffice;
+
+import net.sf.okapi.common.IContext;
+import net.sf.okapi.common.IHelp;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.IParametersEditor;
+import net.sf.okapi.common.ui.Dialogs;
+import net.sf.okapi.common.ui.OKCancelPanel;
+import net.sf.okapi.common.ui.UIUtil;
+import net.sf.okapi.filters.openoffice.Parameters;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+
+public class Editor implements IParametersEditor {
+	
+	private Shell shell;
+	private boolean result = false;
+	private Button chkExtractNotes;
+	private Button chkExtractReferences;
+	private Parameters params;
+	private IHelp help;
+
+	public boolean edit (IParameters options,
+		boolean readOnly,
+		IContext context)
+	{
+		help = (IHelp)context.getObject("help");
+		boolean bRes = false;
+		shell = null;
+		params = (Parameters)options;
+		try {
+			shell = new Shell((Shell)context.getObject("shell"), SWT.CLOSE | SWT.TITLE | SWT.RESIZE | SWT.APPLICATION_MODAL);
+			create((Shell)context.getObject("shell"), readOnly);
+			return showDialog();
+		}
+		catch ( Exception E ) {
+			Dialogs.showError(shell, E.getLocalizedMessage(), null);
+			bRes = false;
+		}
+		finally {
+			// Dispose of the shell, but not of the display
+			if ( shell != null ) shell.dispose();
+		}
+		return bRes;
+	}
+	
+	public IParameters createParameters () {
+		return new Parameters();
+	}
+	
+	private void create (Shell parent,
+		boolean readOnly)
+	{
+		shell.setText("OpenDocument Filter Parameters");
+		if ( parent != null ) UIUtil.inheritIcon(shell, parent);
+		GridLayout layTmp = new GridLayout();
+		layTmp.marginBottom = 0;
+		layTmp.verticalSpacing = 0;
+		shell.setLayout(layTmp);
+
+		TabFolder tfTmp = new TabFolder(shell, SWT.NONE);
+		GridData gdTmp = new GridData(GridData.FILL_BOTH);
+		tfTmp.setLayoutData(gdTmp);
+
+		//--- Options tab
+		
+		Composite cmpTmp = new Composite(tfTmp, SWT.NONE);
+		layTmp = new GridLayout();
+		cmpTmp.setLayout(layTmp);
+		
+		chkExtractNotes = new Button(cmpTmp, SWT.CHECK);
+		chkExtractNotes.setText("Extract notes (office:annotation)");
+
+		chkExtractReferences = new Button(cmpTmp, SWT.CHECK);
+		chkExtractReferences.setText("Extract references (text:bookmark-ref)");
+
+		TabItem tiTmp = new TabItem(tfTmp, SWT.NONE);
+		tiTmp.setText("Options");
+		tiTmp.setControl(cmpTmp);
+		
+		
+		//--- Dialog-level buttons
+
+		SelectionAdapter OKCancelActions = new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				result = false;
+				if ( e.widget.getData().equals("h") ) {
+					if ( help != null ) help.showTopic(this, "index");
+					return;
+				}
+				if ( e.widget.getData().equals("o") ) {
+					if ( !saveData() ) return;
+					result = true;
+				}
+				shell.close();
+			};
+		};
+		OKCancelPanel pnlActions = new OKCancelPanel(shell, SWT.NONE, OKCancelActions, true);
+		gdTmp = new GridData(GridData.FILL_HORIZONTAL);
+		pnlActions.setLayoutData(gdTmp);
+		pnlActions.btOK.setEnabled(!readOnly);
+		if ( !readOnly ) {
+			shell.setDefaultButton(pnlActions.btOK);
+		}
+
+		shell.pack();
+		Rectangle Rect = shell.getBounds();
+		shell.setMinimumSize(Rect.width, Rect.height);
+		Point startSize = shell.getMinimumSize();
+		if ( startSize.x < 300 ) startSize.x = 300; 
+		if ( startSize.y < 200 ) startSize.y = 200; 
+		shell.setSize(startSize);
+		Dialogs.centerWindow(shell, parent);
+		setData();
+	}
+	
+	private boolean showDialog () {
+		shell.open();
+		while ( !shell.isDisposed() ) {
+			if ( !shell.getDisplay().readAndDispatch() )
+				shell.getDisplay().sleep();
+		}
+		return result;
+	}
+	
+	private void setData () {
+		chkExtractNotes.setSelection(params.extractNotes);
+		chkExtractReferences.setSelection(params.extractReferences);
+	}
+	
+	private boolean saveData () {
+		params.extractNotes = chkExtractNotes.getSelection();
+		params.extractReferences = chkExtractReferences.getSelection();
+		return true;
+	}
+	
+}
