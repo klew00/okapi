12ba298a320d0cf90a4169b4e2e6ce858147e784
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-14 11:56:43 +0000

    Updating xml filter.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2838 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/xml/src/main/java/org/w3c/its/Main.java
+++ filters/xml/src/main/java/org/w3c/its/Main.java
@@ -19,7 +19,7 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class Test {
+public class Main {
 
 	public static void main (String[] args) {
 		try {
