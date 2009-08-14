b8207d70336f875c7cdc4a7f045ad2eaa5198ab3
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-14 18:15:10 +0000

    Continuing lib-segmentation update.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2861 0cd2bb99-014b-0410-b875-5d0485b745ed

--- libraries/lib-segmentation/src/test/java/net/sf/okapi/lib/segmentation/SRXSegmenterTest.java
+++ libraries/lib-segmentation/src/test/java/net/sf/okapi/lib/segmentation/SRXSegmenterTest.java
@@ -18,7 +18,7 @@
   See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
 ===========================================================================*/
 
-package net.sf.okapi.lib.segmentation.tests;
+package net.sf.okapi.lib.segmentation;
 
 import java.util.ArrayList;
 
