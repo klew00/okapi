ff8c72b21aababd893d8d939b642740a96c50ccb
asgeirf <asgeirf@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-14 00:41:02 +0000

    really set svn:ignore on eclipse-stuff
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2825 0cd2bb99-014b-0410-b875-5d0485b745ed

--- cleanEcplisePaths.sh
+++ cleanEcplisePaths.sh
@@ -3,21 +3,16 @@
 for i in `find . | grep ".pom.xml$"`; do 
   d=${i%"/pom.xml"};
   echo "Processing $d"
-
-  echo ".. .settings"
   svn rm "$d/.settings"; 
-  svn propset svn:ignore .settings $d;
-
-  echo ".. .classpath"
   svn rm "$d/.classpath";
-  svn propset svn:ignore .classpath $d;
-
-  echo ".. .project"
   svn rm "$d/.project";
-  svn propset svn:ignore .project $d;
-
-  echo ".. target"
   svn rm "$d/target";
-  svn propset svn:ignore target $d;
+
+  svn propset svn:ignore '
+.project
+.classpath
+target
+.settings
+' $d;
 
 done;
