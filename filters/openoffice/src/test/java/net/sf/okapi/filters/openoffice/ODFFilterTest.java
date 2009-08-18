7c75c1ea047f39ef7d4c004dd3fe9c8da336afa6
engrean <engrean@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-18 13:36:18 +0000

    removed tests package
    git-svn-id: https://okapi.googlecode.com/svn/trunk@2979 0cd2bb99-014b-0410-b875-5d0485b745ed

--- filters/openoffice/src/test/java/net/sf/okapi/filters/openoffice/ODFFilterTest.java
+++ filters/openoffice/src/test/java/net/sf/okapi/filters/openoffice/ODFFilterTest.java
@@ -18,7 +18,7 @@
   See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
 ===========================================================================*/
 
-package net.sf.okapi.filters.openoffice.tests;
+package net.sf.okapi.filters.openoffice;
 
 import static org.junit.Assert.*;
 
