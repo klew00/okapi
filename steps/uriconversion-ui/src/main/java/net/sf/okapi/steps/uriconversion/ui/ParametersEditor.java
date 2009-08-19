d0bc85fc1c60e3da6d5ed98b78c4a48ad0bf8576
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-19 00:23:01 +0000

    rename packages to *.ui
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3145 0cd2bb99-014b-0410-b875-5d0485b745ed

--- steps/uriconversion-ui/src/main/java/net/sf/okapi/steps/uriconversion/ui/ParametersEditor.java
+++ steps/uriconversion-ui/src/main/java/net/sf/okapi/steps/uriconversion/ui/ParametersEditor.java
@@ -18,7 +18,7 @@
   See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
 ===========================================================================*/
 
-package net.sf.okapi.steps.ui.uriconversion;
+package net.sf.okapi.steps.uriconversion.ui;
 
 import net.sf.okapi.common.IContext;
 import net.sf.okapi.common.IHelp;
