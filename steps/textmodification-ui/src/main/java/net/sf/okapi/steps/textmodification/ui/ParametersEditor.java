f91a2a4759343253500cb8c623932d0a25d66cd6
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-19 16:19:12 +0000

    Renamed steps.ui.textmodification package to steps.textmodification.ui.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3166 0cd2bb99-014b-0410-b875-5d0485b745ed

--- steps/textmodification-ui/src/main/java/net/sf/okapi/steps/textmodification/ui/ParametersEditor.java
+++ steps/textmodification-ui/src/main/java/net/sf/okapi/steps/textmodification/ui/ParametersEditor.java
@@ -18,7 +18,7 @@
   See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
 ===========================================================================*/
 
-package net.sf.okapi.steps.ui.textmodification;
+package net.sf.okapi.steps.textmodification.ui;
 
 import net.sf.okapi.common.IContext;
 import net.sf.okapi.common.IHelp;
