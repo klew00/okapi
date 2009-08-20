7adf7e8aac8a4d4fd9a23502b6b8a91dcd1c513d
twbgazelem <twbgazelem@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-20 15:53:50 +0000

    U and I go to the end -- fullwidthconversion-ui
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3201 0cd2bb99-014b-0410-b875-5d0485b745ed

--- steps/fullwidthconversion-ui/src/main/java/net/sf/okapi/steps/ui/fullwidthconversion/ParametersEditor.java
+++ steps/fullwidthconversion-ui/src/main/java/net/sf/okapi/steps/ui/fullwidthconversion/ParametersEditor.java
@@ -18,7 +18,7 @@
   See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
 ===========================================================================*/
 
-package net.sf.okapi.steps.ui.fullwidthconversion;
+package net.sf.okapi.steps.fullwidthconversion.ui;
 
 import net.sf.okapi.common.IContext;
 import net.sf.okapi.common.IHelp;
