312d2772ddaa1cc8467233ebb1a5d4d6f4619c64
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-20 20:33:07 +0000

    Continuing porting java examples.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3242 0cd2bb99-014b-0410-b875-5d0485b745ed

--- examples/java/run_examples.bat
+++ examples/java/run_examples.bat
@@ -5,11 +5,11 @@ java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example01/target/okapi-example-01-0
 java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example01/target/okapi-example-01-0.3-SNAPSHOT.jar Main myFile.properties -s pseudo
 java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example01/target/okapi-example-01-0.3-SNAPSHOT.jar Main myFile.xml -s pseudo
 
-rem java -cp .;../../lib/okapi-lib.jar;example02.jar example02.Main myFile.odt
+java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example02/target/okapi-example-02-0.3-SNAPSHOT.jar Main myFile.odt
 
-rem java -cp .;../../lib/okapi-lib.jar;example03.jar example03.Main
+java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example03/target/okapi-example-03-0.3-SNAPSHOT.jar Main
 
-rem java -cp .;../../lib/okapi-lib.jar;example04.jar example04.Main
+rem java -cp .;../lib/okapi-lib-0.3-SNAPSHOT.jar;example04/target/okapi-example-04-0.3-SNAPSHOT.jar Main
 
 rem java -cp .;../../lib/okapi-lib.jar;example05.jar;../../lib/axis.jar;../../lib/wsdl4j-1.5.1.jar;../../lib/jaxrpc.jar;../../lib/commons-logging-1.0.4.jar;../../lib/commons-discovery-0.2.jar;../../lib/saaj.jar;../../lib/xmlrpc-client-3.1.jar;../../lib/xmlrpc-common-3.1.jar;../../lib/ws-commons-util-1.0.2.jar example05.Main
 
