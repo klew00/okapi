1c2255e2a7fb0dabbd1a7f2ce4a32c017e0a8fda
daximus <daximus@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-08-24 19:17:02 +0000

    Adding pensieve projects and tweaked TM Pom to include maven-jar-plugin
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3281 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ tm/pensieve/indexer/src/main/java/net/sf/okapi/tm/pensieve/indexer/Indexer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sf.okapi.tm.pensieve.indexer;
+
+/**
+ *
+ * @author HaslamJD
+ */
+public interface Indexer {
+
+}
---
+++ tm/pensieve/searcher/src/main/java/net/sf/okapi/tm/pensieve/searcher/Searcher.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sf.okapi.tm.pensieve.searcher;
+
+/**
+ *
+ * @author HaslamJD
+ */
+public interface Searcher {
+
+}
