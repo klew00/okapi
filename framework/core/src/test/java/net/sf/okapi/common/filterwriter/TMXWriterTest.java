2a014e0af86cddcac20c9ef05cedb568bba7ef46
daximus <daximus@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-09-15 20:47:21 +0000

    More unit tests for TMXWriter
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3501 0cd2bb99-014b-0410-b875-5d0485b745ed

--- framework/core/src/test/java/net/sf/okapi/common/filterwriter/TMXWriterTest.java
+++ framework/core/src/test/java/net/sf/okapi/common/filterwriter/TMXWriterTest.java
@@ -29,6 +29,9 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import net.sf.okapi.common.resource.Property;
+import net.sf.okapi.common.resource.TextFragment;
+import net.sf.okapi.common.resource.TextUnit;
 
 /**
  *
@@ -38,44 +41,133 @@ public class TMXWriterTest {
 
     final static File TMX_File = new File("target/test-classes/tmxwritertest_tmxfile.tmx");
     TMXWriter tmxWriter;
+    StringWriter strWriter;
 
-    @SuppressWarnings({"ResultOfMethodCallIgnored"})
     @Before
     public void setUp() {
-        TMX_File.delete();
-        tmxWriter = new TMXWriter(TMX_File.getPath());
+        strWriter = new StringWriter();
+        XMLWriter xmlWriter = new XMLWriter(strWriter);
+        tmxWriter = new TMXWriter(xmlWriter);
+        createTmxHeader();
     }
 
     @Test
     public void constructorStringPath() {
+        TMX_File.delete();
+        tmxWriter = new TMXWriter(TMX_File.getPath());
         assertTrue("tmx file should have been created", TMX_File.exists());
     }
 
     @Test
-    public void writeStartDocument() throws IOException {
+    public void writeStartDocumentWithFile() throws IOException {
+        TMX_File.delete();
+        tmxWriter = new TMXWriter(TMX_File.getPath());
         createTmxHeader();
+        tmxWriter.close();
         String tmx = getFileAsString(TMX_File);
         testHeader(tmx);
     }
 
     @Test
     public void constructorWithXmlWriter() {
-        StringWriter strWriter = new StringWriter();
-        XMLWriter xmlWriter = new XMLWriter(strWriter);
-        tmxWriter = new TMXWriter(xmlWriter);
-        createTmxHeader();
         String tmx = strWriter.toString();
         testHeader(tmx);
     }
 
-    private void createTmxHeader() {
-        tmxWriter.writeStartDocument("EN", "FR", "pensieve", "0.0.1", "sentence", "pensieve_format", "unknown");
-        tmxWriter.close();
+    @Test
+    public void emptyDocument() {
+        tmxWriter.writeEndDocument();
+        String tmx = stripNewLinesAndReturns(strWriter.toString());
+        assertEquals("Header and Footer Only", expectedHeaderTmx + expectedFooterTmx, tmx);
     }
 
-    private void testHeader(String tmx) {
-        tmx = tmx.replaceAll("[\\n\\r]+", "");
-        String expectedTMX = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><tmx version=\"1.4\"><header creationtool=\"pensieve\" creationtoolversion=\"0.0.1\" segtype=\"sentence\" o-tmf=\"pensieve_format\" adminlang=\"en\" srclang=\"EN\" datatype=\"unknown\"></header><body>";
-        assertEquals("TMX Header", expectedTMX, tmx);
+    @Test
+    public void testTmxTuNoAttributes() {
+        TextUnit tu = createTextUnit("id", "SourceContent", "TargetContent", null);
+        tmxWriter.writeTUFull(tu);
+        testTu(tu, strWriter.toString());
+    }
+
+    @Test
+    public void testTmxTuSingleProp() {
+        TextUnit tu = createTextUnit("id", "SourceContent", "TargetContent", new String[][]{{"prop1", "value1"}});
+        tmxWriter.writeTUFull(tu);
+        testTu(tu, strWriter.toString());
+    }
+
+    @Test
+    public void testTmxTuMultiProp() {
+        TextUnit tu = createTextUnit("id", "SourceContent", "TargetContent", new String[][]{{"prop1", "value1"}, {"prop2", "value2"}});
+        tmxWriter.writeTUFull(tu);
+        testTu(tu, strWriter.toString());
+    }
+
+    @Test
+    public void testTmxTuMultiLang() {
+        TextUnit tu = createTextUnit("id", "SourceContent", "TargetContent", new String[][]{{"prop1", "value1"}, {"prop2", "value2"}});
+        tu.setTargetContent("KR", new TextFragment("KoreanTarget"));
+        tmxWriter.writeTUFull(tu);
+        testTu(tu, strWriter.toString());
+    }
+
+     private void testHeader(String tmx) {
+        tmx = stripNewLinesAndReturns(tmx);
+        assertEquals("TMX Header", expectedHeaderTmx, tmx);
+    }
+     
+    private final static String expectedHeaderTmx = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><tmx version=\"1.4\"><header creationtool=\"pensieve\" creationtoolversion=\"0.0.1\" segtype=\"sentence\" o-tmf=\"pensieve_format\" adminlang=\"en\" srclang=\"EN\" datatype=\"unknown\"></header><body>";
+    private final static String expectedFooterTmx = "</body></tmx>";
+
+    private void testTu(TextUnit tu, String tmx) {
+        tmx = stripNewLinesAndReturns(tmx);
+        String properties = getProps(tu);
+        String targetTuvs = getTargetTuvs(tu);
+
+        String expectedTMX = 
+                "<tu tuid=\"" + tu.getName() + "\">" +
+                properties +
+                "<tuv xml:lang=\"EN\">" +
+                "<seg>" + tu.getSourceContent().toString() + "</seg>" +
+                "</tuv>" +
+                targetTuvs +
+                "</tu>";
+        assertEquals("TU Element", expectedHeaderTmx + expectedTMX, tmx);
+    }
+
+    private String getProps(TextUnit tu) {
+        String properties = "";
+        for (String propName : tu.getPropertyNames()) {
+            properties += "<prop type=\"" + propName + "\">" + tu.getProperty(propName) + "</prop>";
+        }
+        return properties;
+    }
+
+    private String getTargetTuvs(TextUnit tu) {
+        String targetTuvs = "";
+        for (String langName : tu.getTargetLanguages()) {
+            targetTuvs += "<tuv xml:lang=\"" + langName + "\">" + "<seg>" + tu.getTargetContent(langName).toString() + "</seg>" + "</tuv>";
+        }
+        return targetTuvs;
+    }
+
+    private TextUnit createTextUnit(String id, String sourceContent, String targetContent, String[][] attributes) {
+        TextUnit tu = new TextUnit(id);
+        tu.setName(id);
+        tu.setSourceContent(new TextFragment(sourceContent));
+        tu.setTargetContent("FR", new TextFragment(targetContent));
+        if (attributes != null) {
+            for (String[] kvp : attributes) {
+                tu.setProperty(new Property(kvp[0], kvp[1]));
+            }
+        }
+        return tu;
+    }
+
+    private void createTmxHeader() {
+        tmxWriter.writeStartDocument("EN", "FR", "pensieve", "0.0.1", "sentence", "pensieve_format", "unknown");
+    }
+
+    private String stripNewLinesAndReturns(String tmx) {
+        return tmx.replaceAll("[\\n\\r]+", "");
     }
 }
