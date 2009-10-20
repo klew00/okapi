7d7236226d007810f97484e372548c88b87a675d
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-10-20 01:45:35 +0000

    Updated PensieveTMConnectorTest to create the reference TM from the test.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3702 0cd2bb99-014b-0410-b875-5d0485b745ed

--- connectors/pensieve/src/test/java/net/sf/okapi/connectors/pensieve/PensieveTMConnectorTest.java
+++ connectors/pensieve/src/test/java/net/sf/okapi/connectors/pensieve/PensieveTMConnectorTest.java
@@ -24,6 +24,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 
 import net.sf.okapi.common.Util;
@@ -32,6 +34,10 @@ import net.sf.okapi.common.resource.TextFragment;
 import net.sf.okapi.common.resource.TextFragment.TagType;
 import net.sf.okapi.lib.translation.ITMQuery;
 import net.sf.okapi.lib.translation.QueryResult;
+import net.sf.okapi.tm.pensieve.common.TranslationUnit;
+import net.sf.okapi.tm.pensieve.common.TranslationUnitVariant;
+import net.sf.okapi.tm.pensieve.writer.ITmWriter;
+import net.sf.okapi.tm.pensieve.writer.TmWriterFactory;
 
 import org.junit.After;
 import org.junit.Before;
@@ -46,6 +52,8 @@ public class PensieveTMConnectorTest {
 	@Before
 	public void setUp() {
 		URL url = PensieveTMConnectorTest.class.getResource("/testtm/segments.gen");
+		connector = null;
+		if ( url == null ) return; // Creation case
 		connector = new PensieveTMConnector();
 		Parameters params = new Parameters();
 		params.setDbDirectory(Util.getDirectoryName(url.getPath()));
@@ -60,6 +68,65 @@ public class PensieveTMConnectorTest {
 			connector.close();
 			connector = null;
 		}
+	}
+
+	@Test
+	public void testCreateTM () throws IOException {
+		if ( connector != null ) {
+			connector.close(); // Close the connector created by setUp()
+		}
+		URL url = PensieveTMConnectorTest.class.getResource("/mytm.tmx");
+		String dir =  Util.getDirectoryName(url.getPath())+File.separator+"testtm"+File.separator;
+		Util.createDirectories(dir);
+		ITmWriter writer = TmWriterFactory.createFileBasedTmWriter(dir, true);
+		
+		// TU 1
+		TranslationUnit tu = new TranslationUnit();
+		TranslationUnitVariant tuv = new TranslationUnitVariant(locENUS,
+			new TextFragment("Elephants cannot fly."));
+		tu.setSource(tuv);
+		tuv = new TranslationUnitVariant(locFRFR,
+			new TextFragment("Les \u00e9l\u00e9phants ne peuvent pas voler."));
+		tu.setTarget(tuv);
+		writer.indexTranslationUnit(tu);
+
+		// TU 2
+		tu = new TranslationUnit();
+		TextFragment tf = new TextFragment("Elephants ");
+		tf.append(TagType.OPENING, "b", "<b>");
+		tf.append("cannot");
+		tf.append(TagType.CLOSING, "b", "</b>");
+		tf.append(" fly.");
+		tuv = new TranslationUnitVariant(locENUS, tf);
+		tu.setSource(tuv);
+		tf = new TextFragment("Les \u00e9l\u00e9phants ");
+		tf.append(TagType.OPENING, "b", "<b>");
+		tf.append("ne peuvent pas");
+		tf.append(TagType.CLOSING, "b", "</b>");
+		tf.append(" voler.");
+		tuv = new TranslationUnitVariant(locFRFR, tf);
+		tu.setTarget(tuv);
+		writer.indexTranslationUnit(tu);
+
+		// TU 3
+		tu = new TranslationUnit();
+		tf = new TextFragment("Elephants ");
+		tf.append(TagType.OPENING, "g0", "<g0>");
+		tf.append("cannot");
+		tf.append(TagType.CLOSING, "g0", "</g0>");
+		tf.append(" fly.");
+		tuv = new TranslationUnitVariant(locENUS, tf);
+		tu.setSource(tuv);
+		tf = new TextFragment("Les \u00e9l\u00e9phants ");
+		tf.append(TagType.OPENING, "g0", "<g0>");
+		tf.append("ne peuvent pas");
+		tf.append(TagType.CLOSING, "g0", "</g0>");
+		tf.append(" voler.");
+		tuv = new TranslationUnitVariant(locFRFR, tf);
+		tu.setTarget(tuv);
+		writer.indexTranslationUnit(tu);
+
+		writer.endIndex();
 	}
 
 	@Test
@@ -117,7 +184,7 @@ public class PensieveTMConnectorTest {
 	public void testGetAlmostExactMatchWithCodes () {
 		TextFragment tf = createElephantsFragment();
 		connector.setThreshold(99);
-		assertTrue(connector.query(tf) > 1);
+		assertTrue(connector.query(tf) == 2);
 		QueryResult qr = connector.next();
 		assertEquals(tf.toString(), qr.source.toString());
 		assertEquals("Les \u00e9l\u00e9phants <b>ne peuvent pas</b> voler.", qr.target.toString());
