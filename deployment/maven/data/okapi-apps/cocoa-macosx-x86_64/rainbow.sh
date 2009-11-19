0545ac4e487ef07e34f11bb27261b5058d32831a
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-11-19 13:01:03 +0000

    Adding missing build files for cocoa-64 (Thanks Fredrik)
    git-svn-id: https://okapi.googlecode.com/svn/trunk@3903 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ deployment/maven/data/okapi-apps/cocoa-macosx-x86_64/rainbow.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -d64 -XstartOnFirstThread -jar lib/rainbow.jar
+# You can also use the following options:
---
+++ deployment/maven/data/okapi-apps/cocoa-macosx-x86_64/ratel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -d64 -XstartOnFirstThread -jar lib/ratel.jar
