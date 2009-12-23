f2354af0163259a89d9b4ceed4e13d94435ad892
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2009-12-23 14:23:31 +0000

    Added missing file.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4050 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ framework/core-ui/src/main/java/net/sf/okapi/common/ui/genericeditor/GenericEmbeddableEditor.java
@@ -0,0 +1,61 @@
+/*===========================================================================
+  Copyright (C) 2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.common.ui.genericeditor;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import net.sf.okapi.common.IContext;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.ui.IEmbeddableParametersEditor;
+import net.sf.okapi.common.uidescription.IEditorDescriptionProvider;
+
+public class GenericEmbeddableEditor extends GenericEditor implements IEmbeddableParametersEditor {
+
+	private IEditorDescriptionProvider descProvider;
+	
+	public GenericEmbeddableEditor (IEditorDescriptionProvider descProvider) {
+		this.descProvider = descProvider;
+	}
+
+	@Override
+	public void initializeEmbeddableEditor (Composite parent,
+		IParameters paramsObject,
+		IContext context)
+	{
+		params = paramsObject; 
+		shell = (Shell)context.getObject("shell");
+		createComposite(parent, descProvider);
+		setData();
+	}
+
+	@Override
+	public Composite getComposite () {
+		return mainComposite;
+	}
+	
+	@Override
+	public String validateAndSaveParameters () {
+		if ( !saveData() ) return null;
+		return params.toString();
+	}
+
+}
