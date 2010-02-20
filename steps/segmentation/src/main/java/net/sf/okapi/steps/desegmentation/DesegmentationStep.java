4213e04c7f99b3b7e3f769ffb46fc99dede7aed6
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-02-20 14:19:07 +0000

    Added the Desegmentation step.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4220 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ steps/segmentation/src/main/java/net/sf/okapi/steps/desegmentation/DesegmentationStep.java
@@ -0,0 +1,94 @@
+/*===========================================================================
+  Copyright (C) 2010 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.desegmentation;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.LocaleId;
+import net.sf.okapi.common.UsingParameters;
+import net.sf.okapi.common.pipeline.BasePipelineStep;
+import net.sf.okapi.common.pipeline.annotations.StepParameterMapping;
+import net.sf.okapi.common.pipeline.annotations.StepParameterType;
+import net.sf.okapi.common.resource.TextContainer;
+import net.sf.okapi.common.resource.TextUnit;
+
+@UsingParameters(Parameters.class)
+public class DesegmentationStep extends BasePipelineStep {
+
+	private Parameters params;
+	private LocaleId targetLocale;
+
+	public DesegmentationStep () {
+		params = new Parameters();
+	}
+	
+	@StepParameterMapping(parameterType = StepParameterType.TARGET_LOCALE)
+	public void setTargetLocale (LocaleId targetLocale) {
+		this.targetLocale = targetLocale;
+	}
+	
+	@Override
+	public String getName () {
+		return "Desegmentation";
+	}
+
+	@Override
+	public String getDescription () {
+		return "Joins all segments into a single content. "
+			+ "Expects: filter events. Sends back: filter events.";
+	}
+
+	@Override
+	public IParameters getParameters () {
+		return params;
+	}
+
+	@Override
+	public void setParameters (IParameters params) {
+		this.params = (Parameters)params;
+	}
+
+	@Override
+	protected Event handleTextUnit (Event event) {
+		TextUnit tu = (TextUnit)event.getResource();
+		
+		// Skip non-translatable
+		if ( !tu.isTranslatable() ) return event;
+		
+		// Desegment source if needed
+		if ( params.getDesegmentSource() && tu.getSource().isSegmented() ) {
+			tu.getSource().mergeAllSegments();
+		}
+		
+		// Desegment target if needed
+		if ( params.getDesegmentTarget() ) {
+			TextContainer cont = tu.getTarget(targetLocale);
+			if ( cont != null ) {
+				if ( cont.isSegmented() ) {
+					cont.mergeAllSegments();
+				}
+			}
+		}
+		
+		return event;
+	}
+
+}
---
+++ steps/segmentation/src/main/java/net/sf/okapi/steps/desegmentation/Parameters.java
@@ -0,0 +1,96 @@
+/*===========================================================================
+  Copyright (C) 2010 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.desegmentation;
+
+import net.sf.okapi.common.BaseParameters;
+import net.sf.okapi.common.EditorFor;
+import net.sf.okapi.common.ParametersDescription;
+import net.sf.okapi.common.uidescription.EditorDescription;
+import net.sf.okapi.common.uidescription.IEditorDescriptionProvider;
+
+@EditorFor(Parameters.class)
+public class Parameters extends BaseParameters implements IEditorDescriptionProvider {
+	
+	private static final String DESEGMENTSOURCE = "desegmentSource";
+	private static final String DESEGMENTTARGET = "desegmentTarget";
+	
+	private boolean desegmentSource;
+	private boolean desegmentTarget;
+	
+	public Parameters () {
+		reset();
+	}
+	
+	public boolean getDesegmentSource () {
+		return desegmentSource;
+	}
+
+	public void setDesegmentSource (boolean desegmentSource) {
+		this.desegmentSource = desegmentSource;
+	}
+
+	public boolean getDesegmentTarget () {
+		return desegmentTarget;
+	}
+
+	public void setDesegmentTarget (boolean desegmentTarget) {
+		this.desegmentTarget = desegmentTarget;
+	}
+
+	@Override
+	public void reset () {
+		desegmentSource = true;
+		desegmentTarget = true;
+	}
+
+	@Override
+	public void fromString (String data) {
+		reset();
+		buffer.fromString(data);
+		desegmentSource = buffer.getBoolean(DESEGMENTSOURCE, desegmentSource);
+		desegmentTarget = buffer.getBoolean(DESEGMENTTARGET, desegmentTarget);
+	}
+
+	@Override
+	public String toString() {
+		buffer.reset();
+		buffer.setBoolean(DESEGMENTSOURCE, desegmentSource);
+		buffer.setBoolean(DESEGMENTTARGET, desegmentTarget);
+		return buffer.toString();
+	}
+	
+	@Override
+	public ParametersDescription getParametersDescription () {
+		ParametersDescription desc = new ParametersDescription(this);
+		desc.add(DESEGMENTSOURCE, "Join all segments of the source text", null);
+		desc.add(DESEGMENTTARGET, "Join all segments of the target text", null);
+		return desc;
+	}
+	
+	@Override
+	public EditorDescription createEditorDescription (ParametersDescription paramDesc) {
+		EditorDescription desc = new EditorDescription("Desegmentation");
+		desc.addCheckboxPart(paramDesc.get(DESEGMENTSOURCE));
+		desc.addCheckboxPart(paramDesc.get(DESEGMENTTARGET));
+		return desc;
+	}
+
+}
