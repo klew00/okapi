df7d57f81a8a576919998cc4ee7ee01e997cfdb3
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-06-04 21:13:26 +0000

    add PrintEvents for manual testing of HTML filter events
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4725 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/filters/html/src/test/java/net/sf/okapi/filters/html/PrintEvents.java
@@ -0,0 +1,27 @@
+package net.sf.okapi.filters.html;
+
+import java.io.InputStream;
+import java.net.URISyntaxException;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.LocaleId;
+import net.sf.okapi.common.resource.RawDocument;
+
+public class PrintEvents {
+
+	/**
+	 * @param args
+	 * @throws URISyntaxException 
+	 */
+	public static void main(final String[] args) throws URISyntaxException {
+		String[] files = HtmlUtils.getHtmlTestFiles();
+		
+		HtmlFilter htmlFilter = new HtmlFilter();
+		InputStream htmlStream = HtmlFullFileTest.class.getResourceAsStream("/" + files[2]);
+		htmlFilter.open(new RawDocument(htmlStream, "UTF-8", LocaleId.fromString("en")));
+			while (htmlFilter.hasNext()) {
+				Event event = htmlFilter.next();
+				System.out.println(event.getEventType());				
+			}
+	}
+}
