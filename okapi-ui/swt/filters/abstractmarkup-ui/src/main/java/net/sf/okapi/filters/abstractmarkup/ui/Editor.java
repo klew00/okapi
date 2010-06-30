2ce8ccd36190c1f2fb2dc4e2bdaed2323baec525
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-06-30 17:52:46 +0000

    Continued TaggedFilterConfiguration editor.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4823 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi-ui/swt/filters/abstractmarkup-ui/src/main/java/net/sf/okapi/filters/abstractmarkup/ui/Editor.java
+++ okapi-ui/swt/filters/abstractmarkup-ui/src/main/java/net/sf/okapi/filters/abstractmarkup/ui/Editor.java
@@ -28,6 +28,7 @@ import net.sf.okapi.common.IHelp;
 import net.sf.okapi.common.IParameters;
 import net.sf.okapi.common.IParametersEditor;
 import net.sf.okapi.common.ui.Dialogs;
+import net.sf.okapi.common.ui.InputDialog;
 import net.sf.okapi.common.ui.OKCancelPanel;
 import net.sf.okapi.common.ui.UIUtil;
 import net.sf.okapi.filters.abstractmarkup.AbstractMarkupParameters;
@@ -66,6 +67,7 @@ public class Editor implements IParametersEditor {
 	private Shell shell;
 	private boolean result = false;
 	private AbstractMarkupParameters params;
+	private TabFolder tabs;
 	private IHelp help;
 	private List lbAtt;
 	private Table tblAttRules;
@@ -74,6 +76,7 @@ public class Editor implements IParametersEditor {
 	private Button rdAttExceptThese;
 	private Text edAttScopeElements;
 	private Attribute currentAtt;
+	private Button btRemoveAtt;
 
 	public boolean edit (IParameters options,
 		boolean readOnly,
@@ -113,37 +116,37 @@ public class Editor implements IParametersEditor {
 		layTmp.verticalSpacing = 0;
 		shell.setLayout(layTmp);
 
-		TabFolder tfTmp = new TabFolder(shell, SWT.NONE);
+		tabs = new TabFolder(shell, SWT.NONE);
 		GridData gdTmp = new GridData(GridData.FILL_BOTH);
-		tfTmp.setLayoutData(gdTmp);
+		tabs.setLayoutData(gdTmp);
 
-		//--- General tab
+		//=== General tab
 		
-		Composite cmpTmp = new Composite(tfTmp, SWT.NONE);
+		Composite cmpTmp = new Composite(tabs, SWT.NONE);
 		layTmp = new GridLayout();
 		cmpTmp.setLayout(layTmp);
 		
 
-		TabItem tiTmp = new TabItem(tfTmp, SWT.NONE);
+		TabItem tiTmp = new TabItem(tabs, SWT.NONE);
 		tiTmp.setText("General");
 		tiTmp.setControl(cmpTmp);
 		
 		
-		//--- Elements tab
+		//=== Elements tab
 		
-		cmpTmp = new Composite(tfTmp, SWT.NONE);
+		cmpTmp = new Composite(tabs, SWT.NONE);
 		layTmp = new GridLayout();
 		cmpTmp.setLayout(layTmp);
 		
 
-		tiTmp = new TabItem(tfTmp, SWT.NONE);
+		tiTmp = new TabItem(tabs, SWT.NONE);
 		tiTmp.setText("Elements");
 		tiTmp.setControl(cmpTmp);
 		
 		
-		//--- Attributes tab
+		//=== Attributes tab
 		
-		cmpTmp = new Composite(tfTmp, SWT.NONE);
+		cmpTmp = new Composite(tabs, SWT.NONE);
 		layTmp = new GridLayout(2, false);
 		cmpTmp.setLayout(layTmp);
 		
@@ -156,7 +159,6 @@ public class Editor implements IParametersEditor {
 		lbAtt = new List(cmpTmp, SWT.BORDER | SWT.V_SCROLL);
 		gdTmp = new GridData(GridData.FILL_BOTH);
 		gdTmp.verticalSpan = 2;
-		gdTmp.widthHint = 130;
 		lbAtt.setLayoutData(gdTmp);
 		lbAtt.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
@@ -169,21 +171,38 @@ public class Editor implements IParametersEditor {
 		TableColumn col1 = new TableColumn(tblAttRules, SWT.NONE);
 		TableColumn col2 = new TableColumn(tblAttRules, SWT.NONE);
 		TableItem item = new TableItem(tblAttRules, SWT.NONE);
+		item.setData(ATTRULES_TRANS);
 		item.setText(0, "Translatable text"); item.setText(1, RULE_TYPE.ATTRIBUTE_TRANS.toString());
 		item = new TableItem(tblAttRules, SWT.NONE);
+		item.setData(ATTRULES_WRITABLE);
 		item.setText(0, "Modifiable property"); item.setText(1, RULE_TYPE.ATTRIBUTE_WRITABLE.toString());
 		item = new TableItem(tblAttRules, SWT.NONE);
+		item.setData(ATTRULES_READONLY);
 		item.setText(0, "Read-only property"); item.setText(1, RULE_TYPE.ATTRIBUTE_READONLY.toString());
 		item = new TableItem(tblAttRules, SWT.NONE);
+		item.setData(ATTRULES_ID);
 		item.setText(0, "Identifier"); item.setText(1, RULE_TYPE.ATTRIBUTE_ID.toString());
 		item = new TableItem(tblAttRules, SWT.NONE);
+		item.setData(ATTRULES_PRESERVE_WHITESPACE);
 		item.setText(0, "Preserve element's whitespaces"); item.setText(1, RULE_TYPE.ATTRIBUTE_PRESERVE_WHITESPACE.toString());
 		col1.pack();
 		col2.pack();
+		tblAttRules.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				if ( event.detail == SWT.CHECK ) {
+					updateAttributeRules((TableItem)event.item);
+				}
+            }
+		});
 
+		
+		//--- Scope group
+		
 		Group grpScope = new Group(cmpTmp, SWT.NONE);
 		grpScope.setLayout(new GridLayout());
-		grpScope.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.FILL_HORIZONTAL));
+		gdTmp = new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.FILL_HORIZONTAL);
+		gdTmp.verticalSpan = 2;
+		grpScope.setLayoutData(gdTmp);
 		grpScope.setText("Scope:");
 		
 		rdAttrAllElements = new Button(grpScope, SWT.RADIO); 
@@ -213,7 +232,31 @@ public class Editor implements IParametersEditor {
 		edAttScopeElements = new Text(grpScope, SWT.BORDER);
 		edAttScopeElements.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
-		tiTmp = new TabItem(tfTmp, SWT.NONE);
+		
+		//--- Add/Remove buttons for the list of attributes
+		
+		Composite cmpButtons = new Composite(cmpTmp, SWT.NONE);
+		GridLayout layout = new GridLayout(2, false);
+		layout.marginHeight = layout.marginWidth = 0;
+		cmpButtons.setLayout(layout);
+		cmpButtons.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_END));
+		
+		Button btAdd = UIUtil.createGridButton(cmpButtons, SWT.PUSH, "Add...", UIUtil.BUTTON_DEFAULT_WIDTH, 1);
+		btAdd.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				addAttribute();
+            }
+		});
+		
+		btRemoveAtt = UIUtil.createGridButton(cmpButtons, SWT.PUSH, "Remove", UIUtil.BUTTON_DEFAULT_WIDTH, 1);
+		btRemoveAtt.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				removeAttribute();
+            }
+		});
+		
+		
+		tiTmp = new TabItem(tabs, SWT.NONE);
 		tiTmp.setText("Attributes");
 		tiTmp.setControl(cmpTmp);
 		
@@ -253,6 +296,68 @@ public class Editor implements IParametersEditor {
 		setData();
 	}
 	
+	private void removeAttribute () {
+		int n = lbAtt.getSelectionIndex();
+		if ( n < 0 ) return;
+		lbAtt.remove(n);
+		if ( n >= lbAtt.getItemCount() ) n = lbAtt.getItemCount()-1;
+		if ( n > -1 ) lbAtt.setSelection(n);
+		updateAttributesButtons();
+		updateAttribute();
+	}
+	
+	private void addAttribute () {
+		try {
+			InputDialog dlg = new InputDialog(shell, "Add Attribute", "Name of the attribute to add:", null, null, 0, -1, -1);
+			String name = dlg.showDialog();
+			if ( name == null ) return;
+			name = ensureValidName(name);
+			if ( name.isEmpty() ) return;
+			
+			for ( String tmp : lbAtt.getItems() ) {
+				if ( tmp.equals(name) ) {
+					Dialogs.showError(shell,
+						String.format("The attribute \"%s\" is already listed.", name), null);
+					return;
+				}
+			}
+			// Else: add the attribute
+			Attribute att = new Attribute();
+			att.name = name;
+			lbAtt.add(name);
+			lbAtt.setData(name, att);
+			lbAtt.select(lbAtt.getItemCount()-1);
+			updateAttributesButtons();
+			updateAttribute();
+		}
+		catch ( Throwable e ) {
+			Dialogs.showError(shell, "Error when adding an attribute.\n"+e.getMessage(), null);
+		}
+	}
+	
+	private void updateAttributesButtons () {
+		btRemoveAtt.setEnabled(lbAtt.getItemCount()>0);
+	}
+	
+	// Determine which rules to enable/disable based on the one just being set
+	private void updateAttributeRules (TableItem item) {
+		if ( !item.getChecked() ) return; // Un-checking is fine for all
+		switch ( (Integer)item.getData() ) {
+		case ATTRULES_TRANS:
+			tblAttRules.getItem(ATTRULES_WRITABLE).setChecked(false);
+			tblAttRules.getItem(ATTRULES_READONLY).setChecked(false);
+			break;
+		case ATTRULES_WRITABLE:
+			tblAttRules.getItem(ATTRULES_TRANS).setChecked(false);
+			tblAttRules.getItem(ATTRULES_READONLY).setChecked(false);
+			break;
+		case ATTRULES_READONLY:
+			tblAttRules.getItem(ATTRULES_WRITABLE).setChecked(false);
+			tblAttRules.getItem(ATTRULES_TRANS).setChecked(false);
+			break;
+		}
+	}
+	
 	private void updateScopeElements () {
 		edAttScopeElements.setEnabled(!rdAttrAllElements.getSelection());
 	}
@@ -269,7 +374,7 @@ public class Editor implements IParametersEditor {
 		if ( rdAttExceptThese.getSelection() ) currentAtt.scope = Attribute.SCOPE_ALLEXCEPT;
 		else if ( rdAttOnlyThese.getSelection() ) currentAtt.scope = Attribute.SCOPE_ONLY;
 		else currentAtt.scope = Attribute.SCOPE_ALL;
-		currentAtt.scopeElements = edAttScopeElements.getText();
+		currentAtt.scopeElements = edAttScopeElements.getText().trim();
 		return true;
 	}
 	
@@ -283,6 +388,10 @@ public class Editor implements IParametersEditor {
 			for ( TableItem item : tblAttRules.getItems() ) {
 				item.setChecked(false);
 			}
+			rdAttrAllElements.setSelection(true);
+			rdAttExceptThese.setSelection(false);
+			rdAttOnlyThese.setSelection(false);
+			edAttScopeElements.setText("");
 			currentAtt = null;
 		}
 		else {
@@ -309,6 +418,35 @@ public class Editor implements IParametersEditor {
 				shell.getDisplay().sleep();
 		}
 		return result;
+	}
+	
+	private boolean validate () {
+		saveAttribute();
+		for ( int i=0; i<lbAtt.getItemCount(); i++ ) {
+			String name = lbAtt.getItem(i);
+			if ( !name.equals(ensureValidName(name)) ) {
+				Dialogs.showError(shell,
+					String.format("The attribute name \"%s\" is invalid.", lbAtt.getItem(i)), null);
+				tabs.setSelection(2);
+				return false;
+			}
+			Attribute att = (Attribute)lbAtt.getData(name);
+			// We need at least one rule
+			if ( att.rules.isEmpty() ) {
+				Dialogs.showError(shell,
+					String.format("The attribute \"%s\" has no rule defined.", name), null);
+				tabs.setSelection(2);
+				return false;
+			}
+			// Check the scope
+			if (( att.scope != Attribute.SCOPE_ALL ) && att.scopeElements.isEmpty() ) {
+				Dialogs.showError(shell,
+					String.format("The attribute \"%s\" has no elements defined for its scope.", name), null);
+				tabs.setSelection(2);
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private void setData () {
@@ -349,11 +487,25 @@ public class Editor implements IParametersEditor {
 		
 			lbAtt.add(attName);
 			lbAtt.setData(attName, att);
-
 		}
+		
+		if ( lbAtt.getItemCount() > 0 ) {
+			lbAtt.setSelection(0);
+		}
+		
+		updateAttribute();
+		updateAttributesButtons();
+	}
+
+	private String ensureValidName (String name) {
+		name = name.toLowerCase().trim();
+		name = name.replaceAll("\\s", "");
+		return name;
 	}
 	
 	private boolean saveData () {
+		if ( !validate() ) return false;
+		
 		return true;
 	}
 	
