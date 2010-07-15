6e97a8f6daf3984deeb6cbf64bb9baa1a6d43253
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-07-15 22:29:40 +0000

    remove unused import
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4919 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/filters/abstractmarkup/src/main/java/net/sf/okapi/filters/abstractmarkup/AbstractMarkupFilter.java
+++ okapi/filters/abstractmarkup/src/main/java/net/sf/okapi/filters/abstractmarkup/AbstractMarkupFilter.java
@@ -24,7 +24,6 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.util.EmptyStackException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
