0cfd057320bcd22954b22f51de8420bf045efd94
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-07-27 00:03:46 +0000

    correct DITA config long description
    git-svn-id: https://okapi.googlecode.com/svn/trunk@4968 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/filters/xmlstream/src/main/java/net/sf/okapi/filters/xmlstream/XmlStreamFilter.java
+++ okapi/filters/xmlstream/src/main/java/net/sf/okapi/filters/xmlstream/XmlStreamFilter.java
@@ -54,7 +54,7 @@ public class XmlStreamFilter extends AbstractMarkupFilter {
 				getClass().getName(), "XML Stream", "Large XML Documents", //$NON-NLS-1$
 				Parameters.DEFAULT_PARAMETERS));
 		addConfiguration(new FilterConfiguration(getName(), MimeTypeMapper.XML_MIME_TYPE,
-				getClass().getName(), "DITA", "DITA Filter", //$NON-NLS-1$
+				getClass().getName(), "DITA", "DITA XML", //$NON-NLS-1$
 				Parameters.DITA_PARAMETERS));
 		addConfiguration(new FilterConfiguration(getName(), MimeTypeMapper.XML_MIME_TYPE,
 				getClass().getName(), "Java Properties XML", "Java Properties XML with Embedded HTML", //$NON-NLS-1$
