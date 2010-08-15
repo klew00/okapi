59811f6e509202e717d1173ea19607a07ef8f5bd
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-08-15 13:40:57 +0000

    Added tests for opentran connector.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5019 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/connectors/opentran/src/test/java/net/sf/okapi/connectors/opentran/ManualTry.java
@@ -0,0 +1,56 @@
+package net.sf.okapi.connectors.opentran;
+
+import net.sf.okapi.common.LocaleId;
+import net.sf.okapi.common.resource.TextFragment;
+import net.sf.okapi.common.resource.TextFragment.TagType;
+import net.sf.okapi.lib.translation.QueryResult;
+
+public class ManualTry {
+
+	static public void main (String[] args ) {
+		OpenTranTMConnector conn = null;
+		try {
+			conn = new OpenTranTMConnector();
+			conn.open();
+			conn.setThreshold(60);
+			conn.setMaximumHits(20);
+			conn.setLanguages(LocaleId.ENGLISH, LocaleId.FRENCH);
+			
+			conn.query("Open the application");
+			showResults(conn, "No codes, not exact");
+			
+			conn.query("Open the application.");
+			showResults(conn, "No codes, exact");
+
+			TextFragment tf = new TextFragment("Open the ");
+			tf.append(TagType.OPENING, "b", "<b>");
+			tf.append("application");
+			tf.append(TagType.CLOSING, "b", "</b>");
+			conn.query(tf);
+			showResults(conn, "Codes, not exact");
+			
+			tf.append('.');
+			conn.query(tf);
+			showResults(conn, "Codes, exact");
+		}
+		finally {
+			if ( conn != null ) conn.close();
+		}
+	}
+	
+	static void showResults (OpenTranTMConnector conn,
+		String caption)
+	{
+		QueryResult qr;
+		System.out.println("--- Results for "+ caption + ":");
+		while ( conn.hasNext() ) {
+			qr = conn.next();
+			System.out.println("-- S="+qr.source.toString());
+			System.out.println("   T="+qr.target.toString());
+			System.out.println("   O="+qr.origin);
+			System.out.println("   score="+qr.score);
+		}
+		System.out.println("--- end.");
+	}
+
+}
