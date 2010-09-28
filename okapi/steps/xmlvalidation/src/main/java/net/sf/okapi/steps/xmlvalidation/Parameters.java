64aba6e463f102c4ac8acfe4410bdf9eec8c69cf
fliden@translate.com <fliden@translate.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-09-28 20:19:31 +0000

    xml validation step
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5131 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/steps/xmlvalidation/src/main/java/net/sf/okapi/steps/xmlvalidation/Parameters.java
@@ -0,0 +1,133 @@
+/*===========================================================================
+  Copyright (C) 2008 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+============================================================================*/
+
+package net.sf.okapi.steps.xmlvalidation;
+
+import net.sf.okapi.common.BaseParameters;
+import net.sf.okapi.common.EditorFor;
+import net.sf.okapi.common.ParametersDescription;
+import net.sf.okapi.common.uidescription.EditorDescription;
+import net.sf.okapi.common.uidescription.IEditorDescriptionProvider;
+import net.sf.okapi.common.uidescription.ListSelectionPart;
+import net.sf.okapi.common.uidescription.PathInputPart;
+
+
+@EditorFor(Parameters.class)
+public class Parameters extends BaseParameters implements IEditorDescriptionProvider{
+
+	public static final int VALIDATIONTYPE_DTD = 0;
+	public static final int VALIDATIONTYPE_SCHEMA = 1;
+	public static final int VALIDATIONTYPE_RELAXNG = 2;
+	
+	private static String VALIDATE = "validate";
+	private static String SCHEMAPATH = "schemaPath";
+	private static final String VALIDATIONTYPE = "validationType";
+
+	private boolean validate;	
+	private String schemaPath;
+	private int validationType;
+	
+	public Parameters () {
+		reset();
+	}
+	
+	public boolean isValidate () {
+		return validate;
+	}
+
+	public void setValidate (boolean validate) {
+		this.validate = validate;
+	}
+	
+	public String getSchemaPath () {
+		return schemaPath;
+	}
+
+	public void setSchemaPath (String schemaPath) {
+		this.schemaPath = schemaPath;
+	}
+	
+	public int getValidationType () {
+		return this.validationType;
+	}
+	
+	public void setValidationType (int validationType) {
+		this.validationType = validationType;
+	}
+	
+	public void reset () {
+		schemaPath = "";
+		validate = false;
+		validationType = 0;
+	}
+
+	public void fromString (String data) {
+		reset();
+		buffer.fromString(data);
+		validate = buffer.getBoolean(VALIDATE, validate);
+		schemaPath = buffer.getString(SCHEMAPATH, schemaPath);
+		validationType = buffer.getInteger(VALIDATIONTYPE, validationType);
+	}
+
+	@Override
+	public String toString () {
+		buffer.reset();
+		buffer.setParameter(VALIDATE, validate);
+		buffer.setParameter(SCHEMAPATH, schemaPath);
+		buffer.setInteger(VALIDATIONTYPE, validationType);
+		return buffer.toString();
+	}
+
+	@Override
+	public ParametersDescription getParametersDescription () {
+		ParametersDescription desc = new ParametersDescription(this);
+		desc.add(VALIDATE,
+				"Validate the files [Well-formedness is checked regardless]", null);
+		
+		desc.add(VALIDATIONTYPE, "Type of validation", "Indicates which validation to use");
+
+		desc.add(SCHEMAPATH,
+				"Path of the XML Schema", "Full path of the XML Schema.");
+	
+		return desc;
+	}
+
+	
+	@Override
+	public EditorDescription createEditorDescription(ParametersDescription paramDesc) {
+		EditorDescription desc = new EditorDescription("XML Validation", true, false);
+
+		desc.addCheckboxPart(paramDesc.get(VALIDATE));
+
+		String[] values = {
+				"DTD",
+				"XML Schema",
+				"RelaxNG"
+			};
+		ListSelectionPart lsp = desc.addListSelectionPart(paramDesc.get(VALIDATIONTYPE), values);
+		
+		PathInputPart part = desc.addPathInputPart(paramDesc.get(SCHEMAPATH), "Schema", false);
+		part.setBrowseFilters("Schema Files (*.xsd)\tAll Files (*.*)", "*.xsd\t*.*");
+		part.setAllowEmpty(true);
+	
+		return desc;
+	}
+
+}
---
+++ okapi/steps/xmlvalidation/src/main/java/net/sf/okapi/steps/xmlvalidation/XMLValidationStep.java
@@ -0,0 +1,235 @@
+/*===========================================================================
+  Copyright (C) 2009 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.xmlvalidation;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.Source;
+import javax.xml.validation.Schema;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.UsingParameters;
+import net.sf.okapi.common.Util;
+import net.sf.okapi.common.pipeline.BasePipelineStep;
+import net.sf.okapi.common.resource.RawDocument;
+
+@UsingParameters(Parameters.class)
+public class XMLValidationStep extends BasePipelineStep {
+
+	private final Logger logger = Logger.getLogger(getClass().getName());
+
+	XMLInputFactory xmlInputFact;
+
+    Schema schema;
+    Validator validator;
+    InputSource source;
+    String currentFileDir;
+	
+	private Parameters params;
+	
+	public XMLValidationStep () {
+		params = new Parameters();
+	}
+	
+	@Override
+	public void destroy () {
+		// Make available to GC
+	}
+	
+	public String getDescription () {
+		return "Validate XML file."
+			+ " Expects: raw XML document. Sends back: raw document.";
+	}
+
+	public String getName () {
+		return "XML Validation";
+	}
+
+	@Override
+	public IParameters getParameters () {
+		return params;
+	}
+
+	@Override
+	public void setParameters (IParameters params) {
+		this.params = (Parameters)params;
+	}
+ 
+	@Override
+	protected Event handleStartBatch (Event event) {
+		
+		xmlInputFact = XMLInputFactory.newInstance();
+		//xmlInputFact.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+		
+		if(params.isValidate()){
+			logger.info("Validating using XML Schema: "+params.getSchemaPath());
+		}
+		
+		return event;
+	}
+	
+	
+	@Override
+	protected Event handleRawDocument (Event event) {
+		
+		RawDocument rawDoc = (RawDocument)event.getResource();
+		
+		//--this is used to locate relative dtds--
+		currentFileDir = Util.getDirectoryName(rawDoc.getInputURI().getPath());
+		
+		// Create the input source
+		// Use the stream, so the encoding auto-detection can be done
+		Source xmlInput = new javax.xml.transform.stream.StreamSource(rawDoc.getStream());
+		
+		//--Checking for well-formedness--
+		try {
+			XMLStreamReader reader  = xmlInputFact.createXMLStreamReader(xmlInput);
+			while(reader.hasNext()) {
+				reader.next();
+			}
+			reader.close();
+
+		} catch (XMLStreamException e) {
+			logger.severe("Well-Formedness Error" +
+					"\nLine: "+ e.getLocation().getLineNumber() +
+					"\nColumn: "+ e.getLocation().getColumnNumber() +
+					"\nOffset: "+ e.getLocation().getCharacterOffset() +
+					"\n"+ e.getMessage().substring(e.getMessage().indexOf("Message:")));
+			return event;
+		}
+		
+
+		if(params.isValidate()){
+			
+			xmlInput = new javax.xml.transform.stream.StreamSource(rawDoc.getStream());
+			
+			//--validating against schema--
+
+			
+			try {
+				  SAXParserFactory factory = SAXParserFactory.newInstance();
+				  factory.setNamespaceAware( true);
+				  factory.setValidating( true);
+
+				  SAXParser parser = factory.newSAXParser();
+
+				  if(params.getValidationType() == Parameters.VALIDATIONTYPE_DTD){
+					  
+				  }else if(params.getValidationType() == Parameters.VALIDATIONTYPE_SCHEMA){
+					  
+					  parser.setProperty( "http://java.sun.com/xml/jaxp/properties/schemaLanguage", 
+					                      "http://www.w3.org/2001/XMLSchema");
+					  if(params.getSchemaPath().length() > 0){
+						  parser.setProperty( "http://java.sun.com/xml/jaxp/properties/schemaSource", new File(params.getSchemaPath()));						  
+					  }
+				  }
+
+				  XMLReader reader = parser.getXMLReader();
+
+				  reader.setErrorHandler(new ErrorHandler(){
+
+					public void error(SAXParseException e) throws SAXException {
+						logger.severe("Validation Error" +
+								"\nLine: "+ e.getLineNumber() +
+								"\nColumn: "+ e.getColumnNumber() +
+								"\n"+ e.getMessage()+"\n");
+						throw new SAXException("Error encountered");
+					}
+
+					public void fatalError(SAXParseException e) throws SAXException {
+						logger.severe("Validation Fatal Error" +
+								"\nLine: "+ e.getLineNumber() +
+								"\nColumn: "+ e.getColumnNumber() +
+								"\n"+ e.getMessage()+"\n");
+						throw new SAXException("Fatal Error encountered");
+					}
+
+					public void warning(SAXParseException e) throws SAXException {
+						logger.severe("Validation Warning" +
+								"\nLine: "+ e.getLineNumber() +
+								"\nColumn: "+ e.getColumnNumber() +
+								"\n"+ e.getMessage()+"\n");
+						throw new SAXException("Warning encountered");
+					}
+					  
+				  });
+				  reader.setEntityResolver(new DTDResolver(currentFileDir));
+				  reader.parse( new InputSource(rawDoc.getStream()));
+
+				} catch ( ParserConfigurationException e) {
+					logger.severe(e.getMessage());
+					return event;
+				} catch ( SAXException e) {
+					logger.severe(e.getMessage());
+					return event;
+				} catch ( IOException e) {
+				  logger.severe(e.getMessage());
+				  return event;
+				}
+		}
+		
+		return event;
+	}
+}
+
+class DTDResolver implements EntityResolver {
+	
+	String currentFileDir;
+	
+    public DTDResolver(String currentFileDir) {
+    	super();
+    	this.currentFileDir = currentFileDir;
+	}
+
+	public InputSource resolveEntity(String publicID, String systemID) throws SAXException {
+
+		//--check if the default dtd-location file is resolved--
+    	File file = new File(systemID);
+    	if (file.exists()){
+	    	return null;
+	    }
+	    
+    	//--check for dtd relative to the xml file--
+	    file = new File(currentFileDir+systemID.substring(systemID.lastIndexOf("/")));
+	    if (file.exists()){
+	    	return new InputSource(currentFileDir+systemID.substring(systemID.lastIndexOf("/")));
+	    }
+        
+        return null;
+    }
+}
