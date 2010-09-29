12a734cc18a7e2aa71d19084026dcb35fd7cc794
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-09-29 18:37:30 +0000

    add sort method to sort list of AltTranslation (safe for M9)
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5142 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/core/src/main/java/net/sf/okapi/common/annotation/AltTranslationsAnnotation.java
+++ okapi/core/src/main/java/net/sf/okapi/common/annotation/AltTranslationsAnnotation.java
@@ -21,6 +21,7 @@
 package net.sf.okapi.common.annotation;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -30,118 +31,135 @@ import net.sf.okapi.common.resource.TextFragment;
 
 /**
  * Annotation for storing one or more alternate translations for a target content.
- * <p>When used, this annotation is designed to be attached to the segments or the container of the targets.
+ * <p>
+ * When used, this annotation is designed to be attached to the segments or the container of the targets.
  */
 public class AltTranslationsAnnotation implements IAnnotation, Iterable<AltTranslation> {
-	
+
 	private ArrayList<AltTranslation> list;
 
 	/**
 	 * Creates a new empty AltTranslationsAnnotation object.
 	 */
-	public AltTranslationsAnnotation () {
+	public AltTranslationsAnnotation() {
 		list = new ArrayList<AltTranslation>(2);
 	}
-	
+
 	/**
 	 * Adds an existing {@link AltTranslation} object to this annotation.
-	 * @param alt the {@link AltTranslation} object to add.
+	 * 
+	 * @param alt
+	 *            the {@link AltTranslation} object to add.
 	 */
-	public void add (AltTranslation alt) {
+	public void add(AltTranslation alt) {
 		list.add(alt);
 	}
-	
+
 	/**
 	 * Adds a new entry to the list of alternate translations.
-	 * @param sourceLocId the locale of the source.
-	 * @param targetLocId the locale of the target.
-	 * @param originalSource the original source content.
-	 * @param alternateSource the source content corresponding to the alternate translation.
-	 * @param alternateTarget the content of alternate translation. 
-	 * @param type the type of alternate translation.
-	 * @param score the score for this alternate translation (must be between 0 and 100).
-	 * @param origin an option identifier for the origin of this alternate translation.
+	 * 
+	 * @param sourceLocId
+	 *            the locale of the source.
+	 * @param targetLocId
+	 *            the locale of the target.
+	 * @param originalSource
+	 *            the original source content.
+	 * @param alternateSource
+	 *            the source content corresponding to the alternate translation.
+	 * @param alternateTarget
+	 *            the content of alternate translation.
+	 * @param type
+	 *            the type of alternate translation.
+	 * @param score
+	 *            the score for this alternate translation (must be between 0 and 100).
+	 * @param origin
+	 *            an option identifier for the origin of this alternate translation.
 	 * @return the {@link AltTranslation} object created and added to this annotation.
 	 */
-	public AltTranslation add (LocaleId sourceLocId,
-		LocaleId targetLocId,
-		TextFragment originalSource,
-		TextFragment alternateSource,
-		TextFragment alternateTarget,
-		MatchType type,
-		int score,
-		String origin)
-	{
-		list.add(new AltTranslation(sourceLocId, targetLocId, originalSource,
-			alternateSource, alternateTarget, type, score, origin));
-		return list.get(list.size()-1);
+	public AltTranslation add(LocaleId sourceLocId, LocaleId targetLocId,
+			TextFragment originalSource, TextFragment alternateSource,
+			TextFragment alternateTarget, MatchType type, int score, String origin) {
+		list.add(new AltTranslation(sourceLocId, targetLocId, originalSource, alternateSource,
+				alternateTarget, type, score, origin));
+		return list.get(list.size() - 1);
 	}
 
 	/**
 	 * Creates a new iterator for the entries in this annotations.
+	 * 
 	 * @return a new iterator for the entries in this annotations.
 	 */
 	@Override
-	public Iterator<AltTranslation> iterator () {
+	public Iterator<AltTranslation> iterator() {
 		return new Iterator<AltTranslation>() {
 			int current = 0;
-			
+
 			@Override
-			public void remove () {
+			public void remove() {
 				throw new UnsupportedOperationException("The method remove() not supported.");
 			}
-			
+
 			@Override
-			public AltTranslation next () {
-				if ( current >= list.size() ) {
+			public AltTranslation next() {
+				if (current >= list.size()) {
 					throw new NoSuchElementException("No more content parts.");
 				}
 				return list.get(current++);
 			}
-			
+
 			@Override
-			public boolean hasNext () {
-				return (current<list.size());
+			public boolean hasNext() {
+				return (current < list.size());
 			}
 		};
 	}
 
 	/**
 	 * Gets the first entry in the list of alternate translations.
+	 * 
 	 * @return the first alternate translation entry or null if the list is empty.
 	 */
-	public AltTranslation getFirst () {
-		if ( list.isEmpty() ) {
+	public AltTranslation getFirst() {
+		if (list.isEmpty()) {
 			return null;
 		}
 		return list.get(0);
 	}
-	
+
 	/**
 	 * Gets the last entry in the list of alternate translations.
+	 * 
 	 * @return the last alternate translation entry or null if the list is empty.
 	 */
-	public AltTranslation getLast () {
-		if ( list.isEmpty() ) {
+	public AltTranslation getLast() {
+		if (list.isEmpty()) {
 			return null;
 		}
-		return list.get(list.size()-1);
+		return list.get(list.size() - 1);
 	}
 
 	/**
-	 * Indicates if the list of alternate translation is empty. 
+	 * Indicates if the list of alternate translation is empty.
+	 * 
 	 * @return true if the list is empty.
 	 */
-	public boolean isEmpty () {
+	public boolean isEmpty() {
 		return list.isEmpty();
 	}
-	
+
 	/**
 	 * Gets the number of entries in this annotation.
+	 * 
 	 * @return the number of alternate translations available.
 	 */
-	public int size () {
+	public int size() {
 		return list.size();
 	}
 
+	/**
+	 * Sort the list of {@link AltTranslation}s in the order defined by {@link AltTranslation#compareTo(AltTranslation)}
+	 */
+	public void sort() {
+		Collections.sort(list);
+	}
 }
