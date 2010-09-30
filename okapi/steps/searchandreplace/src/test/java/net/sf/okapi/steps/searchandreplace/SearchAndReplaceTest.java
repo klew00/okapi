d66700538f11db0eafd87055693afc6cd6d6d3be
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-09-30 20:47:35 +0000

    add unit test for multiple patterns
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5157 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/searchandreplace/src/test/java/net/sf/okapi/steps/searchandreplace/SearchAndReplaceTest.java
+++ okapi/steps/searchandreplace/src/test/java/net/sf/okapi/steps/searchandreplace/SearchAndReplaceTest.java
@@ -84,6 +84,46 @@ public class SearchAndReplaceTest {
 	}
 
 	@Test
+	public void replaceSourceCharacterMultiplePatterns() {		
+		Parameters p = (Parameters)searchAndReplace.getParameters();
+		p.reset();
+		p.regEx = true;
+		String pattern[] = new String[3];
+		pattern[0] = Boolean.toString(true);
+		pattern[1] = "\\{nb\\}|\\{tab\\}|\\{em\\}";
+		pattern[2] = "";
+		p.addRule(pattern);
+		
+		String pattern2[] = new String[3];
+		pattern2[0] = Boolean.toString(true);
+		pattern2[1] = "\\{en\\}|\\{emsp\\}|\\{ensp\\}";
+		pattern2[2] = "";
+		p.addRule(pattern2);
+		
+		p.target = false;
+		p.source = true;
+		
+		pipeline.startBatch();
+		TextUnit tu = new TextUnit("1", "{nb}{tab}{em}{en}{emsp}{ensp}");
+		tu.createTarget(LocaleId.SPANISH, true, IResource.COPY_ALL);
+		Event e = new Event(EventType.TEXT_UNIT, tu);
+		pipeline.process(e);
+		pipeline.endBatch();
+
+		// test we observed the correct events
+		List<Event> el = eventObserver.getResult();
+		assertEquals(EventType.START_BATCH, el.remove(0).getEventType());
+		assertEquals(EventType.START_BATCH_ITEM, el.remove(0).getEventType());
+		tu = el.get(0).getTextUnit();
+		assertTrue(tu.getSource().getFirstContent().isEmpty());
+		assertEquals("{nb}{tab}{em}{en}{emsp}{ensp}", tu.getTarget(LocaleId.SPANISH).getFirstContent().toString());
+		assertEquals(EventType.TEXT_UNIT, el.remove(0).getEventType());
+		assertEquals(EventType.END_BATCH_ITEM, el.remove(0).getEventType());
+		assertEquals(EventType.END_BATCH, el.remove(0).getEventType());
+		searchAndReplace.setTargetLocale(LocaleId.SPANISH);
+	}
+
+	@Test
 	public void replaceSourceCharacterWithUnicodeChar() {		
 		Parameters p = (Parameters)searchAndReplace.getParameters();
 		p.reset();
@@ -146,4 +186,4 @@ public class SearchAndReplaceTest {
 		assertEquals(EventType.END_BATCH, el.remove(0).getEventType());
 	}
 	
-	}
+}
