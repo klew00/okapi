fb2c9f612664ec2b67fda8020068e98c0cc8d280
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-11-01 00:38:34 +0000

    Integrated scoping report step in Rainbow.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5305 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/scopingreport/src/main/java/net/sf/okapi/steps/scopingreport/ScopingReportStep.java
+++ okapi/steps/scopingreport/src/main/java/net/sf/okapi/steps/scopingreport/ScopingReportStep.java
@@ -60,7 +60,7 @@ public class ScopingReportStep extends CompoundStep {
 		super();
 		params = new Parameters();
 		setParameters(params);
-		setName("Scoping Report Step");
+		setName("Scoping Report");
 		setDescription("Create a template-based scoping report based on word count and leverage annotations."
 			+" Expects: filter events. Sends back: filter events.");
 		gen = new ReportGenerator(getTemplateStream());
