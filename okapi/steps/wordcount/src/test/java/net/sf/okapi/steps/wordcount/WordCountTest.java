7e2ba5198cd58becf0c7283af1b8281c7f27f2d4
vasilievsi@gmail.com <vasilievsi@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2010-12-24 22:28:58 +0000

    1. Word counter, tokenizer: handling "L'objectif" in French: 2 words 2. Updated ICU dependency to version 4.6
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5474 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/wordcount/src/test/java/net/sf/okapi/steps/wordcount/WordCountTest.java
+++ okapi/steps/wordcount/src/test/java/net/sf/okapi/steps/wordcount/WordCountTest.java
@@ -26,12 +26,12 @@ public class WordCountTest {
 
 	@Test
 	public void testCountApostrophe () {
-		//TODO: Should be 4 per http://www.lisa.org/fileadmin/standards/GMX-V.html#Words "L'objectif" is 2 words in FR
-		assertEquals(3, WordCounter.count("L'objectif est defini.", locFR));
-		assertEquals(3, WordCounter.count("L\u2019objectif est defini.", locFR));
+		//Should be 4 per http://www.lisa.org/fileadmin/standards/GMX-V.html#Words "L'objectif" is 2 words in FR
+		assertEquals(4, WordCounter.count("L'objectif est defini.", locFR));
+		assertEquals(4, WordCounter.count("L\u2019objectif est defini.", locFR));
 
-		assertEquals(5, WordCounter.count("He can't eat that fast.", locFR));
-		assertEquals(5, WordCounter.count("He can\u2019t eat that fast.", locFR));
+		assertEquals(5, WordCounter.count("He can't eat that fast.", locEN));
+		assertEquals(5, WordCounter.count("He can\u2019t eat that fast.", locEN));
 	}
 	
 	@Test
