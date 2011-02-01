830b0339225b9f16ceb6284fecef7ac32ff24d00
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-02-01 17:48:25 +0000

    Added FileAnalysisStep stubs for Naoko work (not attached to main build yet)
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5548 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/steps/fileanalysis/src/main/java/net/sf/okapi/steps/fileanalysis/FileAnalysisStep.java
@@ -0,0 +1,76 @@
+/*===========================================================================
+  Copyright (C) 2011 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.fileanalysis;
+
+import net.sf.okapi.common.Event;
+import net.sf.okapi.common.IParameters;
+import net.sf.okapi.common.UsingParameters;
+import net.sf.okapi.common.pipeline.BasePipelineStep;
+import net.sf.okapi.common.resource.RawDocument;
+
+@UsingParameters(Parameters.class)
+public class FileAnalysisStep extends BasePipelineStep {
+
+	private Parameters params;
+
+	public FileAnalysisStep () {
+		params = new Parameters();
+	}
+
+	public String getDescription () {
+		return "Analyzes a file properties such as BOM, line-break, encoding, etc."
+			+ " Expects: raw document. Sends back: raw document.";
+	}
+
+	public String getName () {
+		return "File Analysis";
+	}
+
+	@Override
+	public IParameters getParameters () {
+		return params;
+	}
+
+	@Override
+	public void setParameters (IParameters params) {
+		this.params = (Parameters)params;
+	}
+
+	@Override
+	protected Event handleStartBatch (Event event) {
+		//TODO: any task before the first file of the batch
+		return event;
+	}
+	
+	@Override
+	protected Event handleEndBatch (Event event) {
+		//TODO: any tasks at the end of the batch
+		return event;
+	}
+	
+	@Override
+	protected Event handleRawDocument (Event event) {
+		RawDocument rawDoc = event.getRawDocument();
+		//TODO: analyze the file
+		return event;
+	}
+
+}
---
+++ okapi/steps/fileanalysis/src/test/java/net/sf/okapi/steps/fileanalysis/FileAnalysisStepTest.java
@@ -0,0 +1,69 @@
+/*===========================================================================
+  Copyright (C) 2011 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.steps.fileanalysis;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import net.sf.okapi.common.LocaleId;
+import net.sf.okapi.common.TestUtil;
+import net.sf.okapi.common.Util;
+import net.sf.okapi.common.pipelinedriver.BatchItemContext;
+import net.sf.okapi.common.pipelinedriver.IPipelineDriver;
+import net.sf.okapi.common.pipelinedriver.PipelineDriver;
+import net.sf.okapi.common.resource.RawDocument;
+import net.sf.okapi.steps.common.RawDocumentToFilterEventsStep;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileAnalysisStepTest {
+	
+	private String root;
+	private LocaleId locEN = LocaleId.fromString("en");
+	
+	@Before
+	public void setUp() {
+		root = TestUtil.getParentDir(this.getClass(), "/test01.properties");
+	}
+
+	@Test
+	public void testSimpleStep ()
+		throws URISyntaxException
+	{
+		IPipelineDriver pdriver = new PipelineDriver();
+		pdriver.setRootDirectory(Util.deleteLastChar(root)); // Don't include final separator
+		pdriver.addStep(new RawDocumentToFilterEventsStep());
+		pdriver.addStep(new FileAnalysisStep());
+	
+		URI inputURI = new File(root+"/test01.properties").toURI();
+		RawDocument rd = new RawDocument(inputURI, "UTF-8", locEN);
+		pdriver.addBatchItem(new BatchItemContext(rd, null, null));
+		
+		pdriver.processBatch();
+
+		//TODO: test results
+	}
+
+}
