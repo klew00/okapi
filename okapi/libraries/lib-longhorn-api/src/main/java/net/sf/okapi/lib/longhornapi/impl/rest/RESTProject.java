36084c65c9100c42768e17bbab16958c7735dab4
stefan.pries@gmail.com <stefan.pries@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-08 17:10:09 +0000

    Added REST implementation for Longhorn API
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5660 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/libraries/lib-longhorn-api/src/main/java/net/sf/okapi/lib/longhornapi/impl/rest/RESTProject.java
@@ -0,0 +1,148 @@
+/*===========================================================================
+  Copyright (C) 2011 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
+package net.sf.okapi.lib.longhornapi.impl.rest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import net.sf.okapi.lib.longhornapi.LonghornFile;
+import net.sf.okapi.lib.longhornapi.LonghornProject;
+import net.sf.okapi.lib.longhornapi.impl.rest.RESTFile.Filetype;
+
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.Part;
+
+/**
+ * Implementation of {@link LonghornProject} for Longhorn's RESTful interface.
+ */
+public class RESTProject implements LonghornProject {
+	private URI projUri;
+
+	protected RESTProject() {
+	}
+	
+	protected RESTProject(URI projUri) {
+		this.projUri = projUri;
+	}
+	
+	protected RESTProject(URI serviceUri, String projId) {
+		String newProjUri = serviceUri.toString();
+		if (!newProjUri.endsWith("/"))
+			newProjUri += "/";
+		newProjUri += projId;
+		
+		try {
+			this.projUri = new URI(newProjUri);
+		}
+		catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	protected URI getProjectURI() {
+		return projUri;
+	}
+
+	@Override
+	public void addBatchConfiguration(File bconf) throws FileNotFoundException {
+		Part[] parts = {
+				new FilePart("batchConfiguration", bconf.getName(), bconf)};
+		try {
+			Util.post(projUri + "/batchConfiguration", parts);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void addInputFile(File inputFile, String relativePath) throws FileNotFoundException {
+		String uri = projUri + "/inputFiles/" + inputFile.getName();
+		Part[] inputParts = {
+				new FilePart("inputFile", inputFile.getName(), inputFile)};
+		try {
+			//TODO implement sub-directories
+			Util.put(uri, inputParts);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void delete() {
+		try {
+			Util.delete(projUri.toString());
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void executePipeline() {
+		try {
+			Util.post(projUri + "/tasks/execute", null);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public ArrayList<LonghornFile> getInputFiles() {
+		try {
+			ArrayList<String> filenames = Util.getList(projUri + "/inputFiles");
+			ArrayList<LonghornFile> files = new ArrayList<LonghornFile>();
+			
+			for (String filename : filenames) {
+				files.add(new RESTFile(this, Filetype.input, filename));
+			}
+			
+			return files;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public ArrayList<LonghornFile> getOutputFiles() {
+		try {
+			ArrayList<String> filenames = Util.getList(projUri + "/outputFiles");
+			ArrayList<LonghornFile> files = new ArrayList<LonghornFile>();
+			
+			for (String filename : filenames) {
+				files.add(new RESTFile(this, Filetype.output, filename));
+			}
+			
+			return files;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
---
+++ okapi/libraries/lib-longhorn-api/src/main/java/net/sf/okapi/lib/longhornapi/impl/rest/RESTService.java
@@ -0,0 +1,56 @@
+package net.sf.okapi.lib.longhornapi.impl.rest;
+
+import java.net.URI;
+import java.util.ArrayList;
+
+import net.sf.okapi.lib.longhornapi.LonghornProject;
+import net.sf.okapi.lib.longhornapi.LonghornService;
+
+/**
+ * Implementation of {@link LonghornService} for the communication with Longhorn's RESTful interface.
+ */
+public class RESTService implements LonghornService {
+	private URI baseUri;
+	
+	protected RESTService() {
+	}
+	
+	public RESTService(URI baseUri) {
+		this.baseUri = baseUri;
+	}
+	
+	public URI getBaseUri() {
+		return baseUri;
+	}
+
+	@Override
+	public LonghornProject createProject() {
+
+		try {
+			URI projUri = Util.createProject(baseUri);
+			return new RESTProject(projUri);
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public ArrayList<LonghornProject> getProjects() {
+		
+		try {
+			ArrayList<String> projIds = Util.getList(baseUri.toString() + "/projects");
+			ArrayList<LonghornProject> projects = new ArrayList<LonghornProject>();
+			
+			for (String projId : projIds) {
+				projects.add(new RESTProject(baseUri, projId));
+			}
+			
+			return projects;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
