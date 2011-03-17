3b485c0a3ba9003e6af48b7e841807af223b30ef
yves@opentag.com <yves@opentag.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-17 17:04:09 +0000

    Added rebuild-all batch
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5690 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ deployment/maven/rebuild-all.bat
@@ -0,0 +1,16 @@
+cd ../../
+call mvn clean
+if ERRORLEVEL 1 goto end
+
+call mvn install
+if ERRORLEVEL 1 goto end
+
+cd deployment/maven
+call ant
+if ERRORLEVEL 1 goto end
+
+cd ../../applications/integration-tests
+call mvn integration-test -Dmaven.test.skip=true
+
+:end
+pause
