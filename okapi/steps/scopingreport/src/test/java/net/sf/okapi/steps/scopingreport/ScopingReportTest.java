21eae92e3950bb8a15a6b88e6793b25c28d6f3d7
vasilievsi@gmail.com <vasilievsi@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-18 23:16:15 +0000

    Scoping report, Word count steps. Intermediate.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5701 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/scopingreport/src/test/java/net/sf/okapi/steps/scopingreport/ScopingReportTest.java
+++ okapi/steps/scopingreport/src/test/java/net/sf/okapi/steps/scopingreport/ScopingReportTest.java
@@ -114,7 +114,7 @@ public class ScopingReportTest {
 		ScopingReportStep srs = new ScopingReportStep();
 		Parameters params = (Parameters) srs.getParameters();
 		params.setProjectName("Test scoping report");
-		//params.setOutputPath(pathBase + "out/test_scoping_report2.html"); 
+		params.setOutputPath(pathBase + "out/test_scoping_report2.html"); 
 		
 		srs.setSourceLocale(EN); 
 		srs.setTargetLocale(ES);
