3274518f81f4422c21fabdaf8d578c3a31326a54
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-22 21:19:53 +0000

    add 64 bit linux build script
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5714 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ deployment/maven/update-and-rebuild-linux64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+cd ../..
+svn update
+mvn clean
+mvn install
+
+cd deployment/maven
+ant -f build_okapi-lib.xml
+
+ant -f build_okapi-apps.xml -Dplatform=gtk2-linux-x86_64
+
+ant -f build_okapi-plugins.xml
+
+ant -f build_omegat-plugins.xml
+
+chmod a+x dist_gtk2-linux-x86_64/tikal.sh
+chmod a+x dist_gtk2-linux-x86_64/rainbow.sh
+chmod a+x dist_gtk2-linux-x86_64/ratel.sh
+chmod a+x dist_gtk2-linux-x86_64/checkmate.sh
+
+cd ../../applications/integration-tests
+mvn clean integration-test
