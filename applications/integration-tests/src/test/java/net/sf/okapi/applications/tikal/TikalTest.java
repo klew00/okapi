17cfe3a8a0e056d37b4d7aae04a13d56b9a49393
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-31 21:46:20 +0000

    1. fix several serious maven warnings 2. fix 64-but detection in integration tests
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5771 0cd2bb99-014b-0410-b875-5d0485b745ed

--- applications/integration-tests/src/test/java/net/sf/okapi/applications/tikal/TikalTest.java
+++ applications/integration-tests/src/test/java/net/sf/okapi/applications/tikal/TikalTest.java
@@ -37,7 +37,7 @@ public class TikalTest {
 			distDir = "dist_win32-x86";
 		}
 		else { // Assumes Unix or Linux
-			if ( System.getProperty("os.arch").equals("x86_64") ) {
+			if ( System.getProperty("os.arch").equals("x86_64") || System.getProperty("os.arch").equals("amd64") ) {
 				distDir = "dist_gtk2-linux-x86_64";
 			}
 			else {
