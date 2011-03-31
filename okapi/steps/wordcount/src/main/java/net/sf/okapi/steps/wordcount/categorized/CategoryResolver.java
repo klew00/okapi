f72c5c0ab1404f58b2c454566631e7885f6ac81a
vasilievsi@gmail.com <vasilievsi@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-31 20:58:18 +0000

    Scoping Report Step, tests.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5770 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/wordcount/src/main/java/net/sf/okapi/steps/wordcount/categorized/CategoryResolver.java
+++ okapi/steps/wordcount/src/main/java/net/sf/okapi/steps/wordcount/categorized/CategoryResolver.java
@@ -2,7 +2,13 @@ package net.sf.okapi.steps.wordcount.categorized;
 
 import java.util.LinkedList;
 
+import net.sf.okapi.common.LocaleId;
 import net.sf.okapi.common.pipeline.IPipelineStep;
+import net.sf.okapi.common.resource.Segment;
+import net.sf.okapi.common.resource.TextContainer;
+import net.sf.okapi.common.resource.TextUnit;
+import net.sf.okapi.steps.wordcount.common.Metrics;
+import net.sf.okapi.steps.wordcount.common.MetricsAnnotation;
 
 public class CategoryResolver {
 
@@ -24,6 +30,57 @@ public class CategoryResolver {
 					okapiCategories.add(handler.getMetric());
 					break;
 				}
+			}
+		}
+	}
+
+	public void resolve(Metrics metrics) {
+		for (String metric : metrics) {
+			// Remove all metrics lower ranked
+			int index = gmxCategories.indexOf(metric);			
+			if (index > -1) {
+				for (int i = index + 1; i < gmxCategories.size(); i++) {
+					metrics.unregisterMetric(gmxCategories.get(i));
+				}
+				return;
+			}
+			
+			// Remove all metrics lower ranked
+			index = okapiCategories.indexOf(metric);			
+			if (index > -1) {
+				for (int i = index + 1; i < okapiCategories.size(); i++) {
+					metrics.unregisterMetric(okapiCategories.get(i));
+				}
+			}
+		}
+	}
+	
+	public void resolve(TextUnit tu) {		
+		MetricsAnnotation ma = tu.getAnnotation(MetricsAnnotation.class);
+		if (ma != null)	
+			resolve(ma.getMetrics());
+		
+		TextContainer tc = tu.getSource();
+		ma = tc.getAnnotation(MetricsAnnotation.class);
+		if (ma != null)	
+			resolve(ma.getMetrics());
+		
+		for (Segment seg : tc.getSegments()) {
+			ma = seg.getAnnotation(MetricsAnnotation.class);
+			if (ma != null)	
+				resolve(ma.getMetrics());
+		}
+		
+		for (LocaleId trgLoc : tu.getTargetLocales()) {
+			tc = tu.getTarget(trgLoc);
+			ma = tc.getAnnotation(MetricsAnnotation.class);
+			if (ma != null)	
+				resolve(ma.getMetrics());
+			
+			for (Segment seg : tc.getSegments()) {
+				ma = seg.getAnnotation(MetricsAnnotation.class);
+				if (ma != null)	
+					resolve(ma.getMetrics());
 			}
 		}
 	}
