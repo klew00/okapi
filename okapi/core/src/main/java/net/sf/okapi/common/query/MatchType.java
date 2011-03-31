b222bd878c0e7aa506a1285b0c9d1a34dfa84a9e
vasilievsi@gmail.com <vasilievsi@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-03-31 18:05:25 +0000

    1. MatchType reordering (with Jim's consent) 2. Scoping Report Step
    git-svn-id: https://okapi.googlecode.com/svn/trunk@5763 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/core/src/main/java/net/sf/okapi/common/query/MatchType.java
+++ okapi/core/src/main/java/net/sf/okapi/common/query/MatchType.java
@@ -60,14 +60,14 @@ public enum MatchType {
 	EXACT,
 
 	/**
-	 * EXACT_TEXT_ONLY and comes from a previous version of the same document
-	 */
-	EXACT_TEXT_ONLY_PREVIOUS_VERSION,
-
-	/**
 	 * EXACT_TEXT_ONLY and matches with a unique id
 	 */
 	EXACT_TEXT_ONLY_UNIQUE_ID,
+	
+	/**
+	 * EXACT_TEXT_ONLY and comes from a previous version of the same document
+	 */
+	EXACT_TEXT_ONLY_PREVIOUS_VERSION,
 	
 	/**
 	 * Matches text exactly, but there is a difference in one or more codes and/or whitespace
@@ -82,14 +82,14 @@ public enum MatchType {
 	EXACT_REPAIRED,
 
 	/**
-	 * FUZZY and comes from a previous version of the same document
-	 */
-	FUZZY_PREVIOUS_VERSION,
-	
-	/**
 	 * Matches FUZZY with a unique id
 	 */
 	FUZZY_UNIQUE_ID,
+	
+	/**
+	 * FUZZY and comes from a previous version of the same document
+	 */
+	FUZZY_PREVIOUS_VERSION,
 	
 	/**
 	 * Matches both text and/or codes partially.
