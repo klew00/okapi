c1e5c1b4894c85e592404027e16cdc47c089da3b
jhargraveiii@gmail.com <jhargraveiii@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-06-10 19:33:29 +0000

    add javadoc
    git-svn-id: https://okapi.googlecode.com/svn/trunk@6003 0cd2bb99-014b-0410-b875-5d0485b745ed

--- okapi/steps/common/src/main/java/net/sf/okapi/steps/common/codesimplifier/CodeRemoverStep.java
+++ okapi/steps/common/src/main/java/net/sf/okapi/steps/common/codesimplifier/CodeRemoverStep.java
@@ -3,8 +3,13 @@ package net.sf.okapi.steps.common.codesimplifier;
 import net.sf.okapi.common.Event;
 import net.sf.okapi.common.pipeline.BasePipelineStep;
 import net.sf.okapi.common.resource.ITextUnit;
+import net.sf.okapi.common.resource.TextUnit;
 import net.sf.okapi.common.resource.TextUnitUtil;
 
+/**
+ * Removes all inline codes from the source and all target {@link TextUnit}s.
+ * @author hargraveje
+ */
 public class CodeRemoverStep extends BasePipelineStep {
 	@Override
 	public String getName() {		
