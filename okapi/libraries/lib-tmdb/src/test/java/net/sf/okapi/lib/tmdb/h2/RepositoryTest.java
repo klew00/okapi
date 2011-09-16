79cd0ff95f287a22cd31a3aa4ef06a92557e34b7
yves.savourel@gmail.com <yves.savourel@gmail.com@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-09-16 23:54:04 +0000

    Added API-level tests for tmdb.
    git-svn-id: https://okapi.googlecode.com/svn/trunk@6220 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ okapi/libraries/lib-tmdb/src/test/java/net/sf/okapi/lib/tmdb/h2/RepositoryTest.java
@@ -0,0 +1,39 @@
+package net.sf.okapi.lib.tmdb.h2;
+
+import java.sql.SQLException;
+
+import net.sf.okapi.common.Util;
+import net.sf.okapi.lib.tmdb.IRepository;
+import net.sf.okapi.lib.tmdb.ProcesswithAPI;
+
+import org.junit.Test;
+
+public class RepositoryTest {
+	
+	@Test
+	public void testSimpleTest ()
+		throws SQLException
+	{
+		String repoPath = Util.ensureSeparator(Util.getTempDirectory(), true) + "testTMRepository";
+		// Make sure the repository is not there
+		Repository.delete(repoPath);
+
+		// Create repository (step 1)
+		IRepository repo = new Repository(repoPath);
+		ProcesswithAPI.runMultipleTestsStep1(repo);
+
+		// Run the second step with the existing repository (step 2)
+		repo = new Repository(repoPath);
+		ProcesswithAPI.runMultipleTestsStep2(repo);
+	}
+
+	@Test
+	public void testMultiFieldsAdd ()
+		throws SQLException
+	{
+		// Create memory repository
+		IRepository repo = new Repository(null);
+		ProcesswithAPI.runMultipleTestsStep3(repo);
+	}
+	
+}
