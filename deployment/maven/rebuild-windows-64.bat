1c4432490f583dfc5809601254538ccbefbaac34
jhargraveiii <jhargraveiii@0cd2bb99-014b-0410-b875-5d0485b745ed>
2011-09-22 17:13:35 +0000

    add windows 64 bit build script
    git-svn-id: https://okapi.googlecode.com/svn/trunk@6238 0cd2bb99-014b-0410-b875-5d0485b745ed

---
+++ deployment/maven/rebuild-windows-64.bat
@@ -0,0 +1,24 @@
+cd ../../
+call mvn clean
+if ERRORLEVEL 1 goto end
+call mvn install
+if ERRORLEVEL 1 goto end
+
+cd deployment/maven
+call ant -f build_okapi-lib.xml
+if ERRORLEVEL 1 goto end
+
+call ant -f build_okapi-apps.xml -Dplatform=win32-x86_64
+if ERRORLEVEL 1 goto end
+
+call ant -f build_okapi-plugins.xml
+if ERRORLEVEL 1 goto end
+
+call ant -f build_omegat-plugins.xml
+if ERRORLEVEL 1 goto end
+
+cd ../../applications/integration-tests
+call mvn clean integration-test
+
+:end
+pause
