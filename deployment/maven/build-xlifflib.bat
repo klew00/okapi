279e1558e798f9a376a175f12d9de6ed75ffb03e
Yves <yves.savourel@gmail.com>
2012-02-26 05:56:04 -0700

    Fixed xliff2 build.

--- deployment/maven/build-xlifflib.bat
+++ deployment/maven/build-xlifflib.bat
@@ -2,11 +2,11 @@ cd ../../okapi/libraries/lib-xliff/
 call mvn install
 if ERRORLEVEL 1 goto end
 
-cd ../../applications/lynx
+cd ../../../applications/lynx
 call mvn install
 if ERRORLEVEL 1 goto end
 
-cd ../../../deployment/maven
+cd ../../deployment/maven
 call ant -f build_xliff-lib.xml
 if ERRORLEVEL 1 goto end
 
