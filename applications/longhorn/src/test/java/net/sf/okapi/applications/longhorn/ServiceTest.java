1a2ff1be163c567071f62c941b48b4d2b8467593
Stefan Pries <stefan.pries@gmail.com>
2012-07-11 10:51:48 +0200

    Refactored Longhorn Test to not repeat the setup so many times. Test run time decreased significantly.

--- applications/longhorn/src/test/java/net/sf/okapi/applications/longhorn/ServiceTest.java
+++ applications/longhorn/src/test/java/net/sf/okapi/applications/longhorn/ServiceTest.java
@@ -20,10 +20,10 @@
 
 package net.sf.okapi.applications.longhorn;
 
-import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -41,36 +41,53 @@ import net.sf.okapi.lib.longhornapi.LonghornService;
 import net.sf.okapi.lib.longhornapi.impl.rest.RESTService;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ServiceTest {
 	private static final String SERVICE_BASE_URL = "http://localhost:9095/okapi-longhorn";
-	private LonghornService ws;
-	private LonghornProject proj;
-	private File inputFile;
-	private File inputZip;
-	private File bconf;
+	private static LonghornService ws;
+	private static LonghornProject emptyProj;
+	private static LonghornProject preparedProj;
+	private static File inputFile;
+	private static File inputZip;
+	
+	@BeforeClass
+	public static void setup() throws Exception {
+		ws = new RESTService(new URI(SERVICE_BASE_URL));
+		
+		URL input1 = ServiceTest.class.getResource("/rawdocumenttofiltereventsstep.html");
+		inputFile = new File(input1.toURI());
+		
+		URL input2 = ServiceTest.class.getResource("/more_files.zip");
+		inputZip = new File(input2.toURI());
+		
+		URL bconfUrl = ServiceTest.class.getResource("/html_segment_and_text_mod.bconf");
+		File bconf = new File(bconfUrl.toURI());
+		
+		preparedProj = ws.createProject();
+		preparedProj.addBatchConfiguration(bconf);
+		preparedProj.addInputFile(inputFile, inputFile.getName());
+		preparedProj.addInputFile(inputFile, "samefile/" + inputFile.getName());
+	}
 	
 	@Before
 	public void prep() throws Exception {
-		ws = new RESTService(new URI(SERVICE_BASE_URL));
-		proj = ws.createProject();
-		
-		URL input1 = this.getClass().getResource("/rawdocumenttofiltereventsstep.html");
-		inputFile = new File(input1.toURI());
-		
-		URL input2 = this.getClass().getResource("/more_files.zip");
-		inputZip = new File(input2.toURI());
-		
-		URL bconfUrl = this.getClass().getResource("/html_segment_and_text_mod.bconf");
-		bconf = new File(bconfUrl.toURI());
+		emptyProj = ws.createProject();
 	}
 	
 	@After
 	public void cleanup() {
-		if (proj != null)
-			proj.delete();
+		if (emptyProj != null)
+			emptyProj.delete();
+	}
+	
+	@AfterClass
+	public static void cleanupFinal() {
+		if (preparedProj != null)
+			preparedProj.delete();
 	}
 	
 	@Test
@@ -96,35 +113,35 @@ public class ServiceTest {
 	
 	@Test
 	public void newProjectIsEmpty() {
-		assertEquals(0, proj.getInputFiles().size());
-		assertEquals(0, proj.getOutputFiles().size());
+		assertEquals(0, emptyProj.getInputFiles().size());
+		assertEquals(0, emptyProj.getOutputFiles().size());
 	}
 	
 	@Test
 	public void addingInputFile() throws FileNotFoundException {
-		proj.addInputFile(inputFile, inputFile.getName());
-		assertEquals(1, proj.getInputFiles().size());
-		assertEquals(0, proj.getOutputFiles().size());
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
+		emptyProj.addInputFile(inputFile, inputFile.getName());
+		assertEquals(1, emptyProj.getInputFiles().size());
+		assertEquals(0, emptyProj.getOutputFiles().size());
+		ArrayList<LonghornFile> inputFiles = emptyProj.getInputFiles();
 		assertEquals("rawdocumenttofiltereventsstep.html", inputFiles.get(0).getRelativePath());
 	}
 	
 	@Test
 	public void addingInputFileInSubdir() throws FileNotFoundException {
-		proj.addInputFile(inputFile, "samefile/" + inputFile.getName());
-		assertEquals(1, proj.getInputFiles().size());
-		assertEquals(0, proj.getOutputFiles().size());
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
+		emptyProj.addInputFile(inputFile, "samefile/" + inputFile.getName());
+		assertEquals(1, emptyProj.getInputFiles().size());
+		assertEquals(0, emptyProj.getOutputFiles().size());
+		ArrayList<LonghornFile> inputFiles = emptyProj.getInputFiles();
 		assertEquals("samefile/rawdocumenttofiltereventsstep.html", inputFiles.get(0).getRelativePath());
 	}
 	
 	@Test
 	public void addingInputFilesFromZip() throws FileNotFoundException {
-		proj.addInputFilesFromZip(inputZip);
+		emptyProj.addInputFilesFromZip(inputZip);
 		
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
+		ArrayList<LonghornFile> inputFiles = emptyProj.getInputFiles();
 		assertEquals(2, inputFiles.size());
-		assertEquals(0, proj.getOutputFiles().size());
+		assertEquals(0, emptyProj.getOutputFiles().size());
 
 		// Are the input file names as expected (with 1 file in a sub-directory)?
 		ArrayList<String> relFilePaths = new ArrayList<String>();
@@ -136,18 +153,11 @@ public class ServiceTest {
 	}
 	
 	@Test
-	public void addBconf() throws FileNotFoundException {
-		proj.addBatchConfiguration(bconf);
-	}
-	
-	@Test
 	public void executePipelineCreatesOutputFiles() throws FileNotFoundException {
-		prepProject();
-		
-		proj.executePipeline();
+		preparedProj.executePipeline();
 
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
-		ArrayList<LonghornFile> outputFiles = proj.getOutputFiles();
+		ArrayList<LonghornFile> inputFiles = preparedProj.getInputFiles();
+		ArrayList<LonghornFile> outputFiles = preparedProj.getOutputFiles();
 		
 		// Should be the same number of files with this config
 		assertEquals(inputFiles.size(), outputFiles.size());
@@ -163,12 +173,10 @@ public class ServiceTest {
 	
 	@Test
 	public void executePipelineWithLangParametersCreatesOutputFiles() throws FileNotFoundException {
-		prepProject();
-		
-		proj.executePipeline("en", "de");
+		preparedProj.executePipeline("en", "de");
 
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
-		ArrayList<LonghornFile> outputFiles = proj.getOutputFiles();
+		ArrayList<LonghornFile> inputFiles = preparedProj.getInputFiles();
+		ArrayList<LonghornFile> outputFiles = preparedProj.getOutputFiles();
 		
 		// Should be the same number of files with this config
 		assertEquals(inputFiles.size(), outputFiles.size());
@@ -184,12 +192,10 @@ public class ServiceTest {
 	
 	@Test
 	public void executePipelineWithMultipleTargetLangsCreatesOutputFiles() throws FileNotFoundException {
-		prepProject();
-		
-		proj.executePipeline("en", Arrays.asList("de", "it", "fr"));
+		preparedProj.executePipeline("en", Arrays.asList("de", "it", "fr"));
 
-		ArrayList<LonghornFile> inputFiles = proj.getInputFiles();
-		ArrayList<LonghornFile> outputFiles = proj.getOutputFiles();
+		ArrayList<LonghornFile> inputFiles = preparedProj.getInputFiles();
+		ArrayList<LonghornFile> outputFiles = preparedProj.getOutputFiles();
 		
 		// Should be the same number of files with this config
 		assertEquals(inputFiles.size(), outputFiles.size());
@@ -205,10 +211,9 @@ public class ServiceTest {
 	
 	@Test
 	public void fetchSingleOutputFile() throws IOException {
-		prepProject();
-		proj.executePipeline();
+		preparedProj.executePipeline();
 
-		ArrayList<LonghornFile> outputFiles = proj.getOutputFiles();
+		ArrayList<LonghornFile> outputFiles = preparedProj.getOutputFiles();
 		assertEquals(2, outputFiles.size());
 		
 		for (LonghornFile of : outputFiles) {
@@ -223,17 +228,16 @@ public class ServiceTest {
 	
 	@Test
 	public void fetchOutputFilesAsZip() throws FileNotFoundException {
-		prepProject();
-		proj.executePipeline();
+		preparedProj.executePipeline();
 
-		InputStream zippedOutputFiles = proj.getOutputFilesAsZip();
+		InputStream zippedOutputFiles = preparedProj.getOutputFilesAsZip();
 		assertNotNull(zippedOutputFiles);
 	}
 	
 	@Test
 	public void fetchOutputFilesAsZipThrowsExceptionForNoFiles() throws FileNotFoundException {
 		try {
-			proj.getOutputFilesAsZip();
+			emptyProj.getOutputFilesAsZip();
 			fail("No output files should cause an exception.");
 		}
 		catch (IllegalStateException e) {
@@ -242,19 +246,12 @@ public class ServiceTest {
 	
 	@Test
 	public void fetchOutputFileAsZip() throws FileNotFoundException {
-		prepProject();
-		proj.executePipeline();
+		preparedProj.executePipeline();
 
-		ArrayList<LonghornFile> outputFiles = proj.getOutputFiles();
+		ArrayList<LonghornFile> outputFiles = preparedProj.getOutputFiles();
 		LonghornFile firstOutputFile = outputFiles.get(0);
 		InputStream zippedFile = firstOutputFile.openStreamToZip();
 		assertNotNull(zippedFile);
-	}
-	
-	private void prepProject() throws FileNotFoundException {
-		proj.addBatchConfiguration(bconf);
-		proj.addInputFile(inputFile, inputFile.getName());
-		proj.addInputFile(inputFile, "samefile/" + inputFile.getName());
 	}
 
 	private File downloadFileToTemp(InputStream remoteFile) throws IOException {
