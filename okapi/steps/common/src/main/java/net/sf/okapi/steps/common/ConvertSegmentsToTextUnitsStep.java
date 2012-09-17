7e029f0122ab1fc04958630a5e2844618a91bf2a
Yves <yves.savourel@gmail.com>
2012-09-17 05:45:23 -0600

    Integrated segments to Text Units Converter step in Rainbow.

--- okapi/steps/common/src/main/java/net/sf/okapi/steps/common/ConvertSegmentsToTextUnitsStep.java
+++ okapi/steps/common/src/main/java/net/sf/okapi/steps/common/ConvertSegmentsToTextUnitsStep.java
@@ -1,3 +1,23 @@
+/*===========================================================================
+  Copyright (C) 2012 by the Okapi Framework contributors
+-----------------------------------------------------------------------------
+  This library is free software; you can redistribute it and/or modify it 
+  under the terms of the GNU Lesser General Public License as published by 
+  the Free Software Foundation; either version 2.1 of the License, or (at 
+  your option) any later version.
+
+  This library is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License 
+  along with this library; if not, write to the Free Software Foundation, 
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+  See also the full LGPL text here: http://www.gnu.org/copyleft/lesser.html
+===========================================================================*/
+
 package net.sf.okapi.steps.common;
 
 import java.util.Iterator;
@@ -26,12 +46,13 @@ public class ConvertSegmentsToTextUnitsStep extends BasePipelineStep {
 
 	@Override
 	public String getName() {
-		return "Segments to TextUnits Converter";
+		return "Segments to Text Units Converter";
 	}
 
 	@Override
 	public String getDescription() {
-		return "Convert each aligned segment pair (for all target locales) to its own complete TextUnit";
+		return "Convert each aligned segment pair (for all target locales) to its own complete text unit"
+			+ " Expects: filter events. Sends back: filter events.";
 	}
 
 	@Override
