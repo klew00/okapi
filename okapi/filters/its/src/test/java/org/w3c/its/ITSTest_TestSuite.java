6a197b68eea9a0c5ba967af6c0cfb794458fb31b
Yves <yves.savourel@gmail.com>
2012-12-13 07:28:59 -0700

    ITS: Added test case for external LQI standoff

--- okapi/filters/its/src/test/java/org/w3c/its/ITSTest_TestSuite.java
+++ okapi/filters/its/src/test/java/org/w3c/its/ITSTest_TestSuite.java
@@ -41,23 +41,23 @@ public class ITSTest_TestSuite {
 
 	@Test
 	public void process () throws URISyntaxException {
-	//	processBatches(root+"/translate", Main.DC_TRANSLATE);
+//		processBatches(root+"/translate", Main.DC_TRANSLATE);
 //		processBatches(root+"/localizationnote", Main.DC_LOCALIZATIONNOTE);
 //		processBatches(root+"/terminology", Main.DC_TERMINOLOGY);
-//		processBatches(root+"/directionality", Main.DC_DIRECTIONALITY);
+//		//processBatches(root+"/directionality", Main.DC_DIRECTIONALITY);
 //		processBatches(root+"/languageinformation", Main.DC_LANGUAGEINFORMATION);
 //		processBatches(root+"/elementswithintext", Main.DC_WITHINTEXT);
 //		processBatches(root+"/domain", Main.DC_DOMAIN);
 //		processBatches(root+"/disambiguation", Main.DC_DISAMBIGUATION);
 //		processBatches(root+"/localefilter", Main.DC_LOCALEFILTER);
-	//	processBatches(root+"/externalresource", Main.DC_EXTERNALRESOURCE);
+//		processBatches(root+"/externalresource", Main.DC_EXTERNALRESOURCE);
 //		processBatches(root+"/targetpointer", Main.DC_TARGETPOINTER);
-	//	processBatches(root+"/idvalue", Main.DC_IDVALUE);
-	//	processBatches(root+"/preservespace", Main.DC_PRESERVESPACE);
-	//	processBatches(root+"/locqualityissue", Main.DC_LOCQUALITYISSUE);
-		processBatches(root+"/locqualityrating", Main.DC_LOCQUALITYRATING);
+//		processBatches(root+"/idvalue", Main.DC_IDVALUE);
+//		processBatches(root+"/preservespace", Main.DC_PRESERVESPACE);
+		processBatches(root+"/locqualityissue", Main.DC_LOCQUALITYISSUE);
+//		processBatches(root+"/locqualityrating", Main.DC_LOCQUALITYRATING);
 //		processBatches(root+"/storagesize", Main.DC_STORAGESIZE);
-	//	processBatches(root+"/mtconfidence", Main.DC_MTCONFIDENCE);
+//		processBatches(root+"/mtconfidence", Main.DC_MTCONFIDENCE);
 //		processBatches(root+"/allowedcharacters", Main.DC_ALLOWEDCHARACTERS);
 	}
 	
@@ -78,19 +78,13 @@ public class ITSTest_TestSuite {
 	 * @param category
 	 * @throws URISyntaxException
 	 */
-	public void processBatch(String base, String category) throws URISyntaxException {
-		
+	public void processBatch (String base, String category) throws URISyntaxException {
 		removeOutput(base);
-		
 		File f = new File(base);
-		if (! f.exists())
-			return;
-		
+		if ( ! f.exists() ) return;
 		String[] files = Util.getFilteredFiles(base, "");
-		
-		for (String file : files) {
-			if (file.contains("rules")) 
-				continue;
+		for ( String file : files ) {
+			if ( file.contains("rules") || file.contains("standoff") ) continue;
 			process(base + "/" + file, category);
 		}
 	}
