b366c71e1c8b31d575242189baefe5e49fddfe06
Yves <yves.savourel@gmail.com>
2013-03-19 08:50:47 -0600

    Updated build for LanguageToolStep.

--- deployment/maven/build_okapi-plugins.bat
+++ deployment/maven/build_okapi-plugins.bat
@@ -1,6 +1,10 @@
 call ant -f build_getVerProp.xml
 if ERRORLEVEL 1 goto end
 
+cd ../../okapi/libraries/lib-languagetool
+call mvn clean package
+
+cd ../../../deployment/maven
 call ant -f build_okapi-plugins.xml
 if ERRORLEVEL 1 goto end
 
